d48312112f42fe05ce72437ace15015ff747ca12
trif0lium <gh@fleeingsunlight.dev>
2021-08-09 13:17:56 +0700

    feat(apiserver): add GET /project/:projectID

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -1,1 +1,15 @@
 package apiserver
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/cast"
+)
+
+func APIServer(port int) {
+	app := fiber.New()
+
+	app.Get("/project/:projectID", func(c *fiber.Ctx) error {
+		projectID := cast.ToString(c.Params("projectID"))
+		return c.SendString(projectID)
+	})
+}
