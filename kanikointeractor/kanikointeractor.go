e59bc2d46029d74f8ee59a825e17e8366415bb8d
trif0lium <gh@fleeingsunlight.dev>
2021-08-14 20:19:26 +0700

    feat(kanikointeractor): add BuildContainerImage()

--- kanikointeractor/kanikointeractor.go
+++ kanikointeractor/kanikointeractor.go
@@ -1,1 +1,16 @@
 package kanikointeractor
+
+import (
+	"github.com/thecodeisalreadydeployed/containerregistry"
+	"github.com/thecodeisalreadydeployed/kubernetesinteractor"
+	v1 "k8s.io/api/apps/v1"
+)
+
+type KanikoInteractor struct {
+	Registry containerregistry.ContainerRegistryType
+}
+
+func (it KanikoInteractor) BuildContainerImage() {
+	k8s := kubernetesinteractor.NewKubernetesInteractor()
+	k8s.CreateDeployment(&v1.Deployment{})
+}
