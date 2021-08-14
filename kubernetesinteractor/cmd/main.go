5269a761c57bbf9e3a933a24067a7dfe988e786b
trif0lium <gh@fleeingsunlight.dev>
2021-08-14 20:02:39 +0700

    feat(kubernetesinteractor): NewKubernetesInteractor()

--- kubernetesinteractor/cmd/main.go
+++ kubernetesinteractor/cmd/main.go
@@ -1,52 +1,9 @@
 package main
 
-import (
-	"path/filepath"
-	"strings"
-
-	"github.com/mitchellh/go-homedir"
-	"github.com/spf13/pflag"
-	"github.com/thecodeisalreadydeployed/kubernetesinteractor"
-	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-)
-
-func absolutePath(path string) string {
-	if filepath.IsAbs(path) {
-		return path
-	}
-
-	if strings.HasPrefix(path, string(0x7E)) {
-		home, err := homedir.Dir()
-		if err != nil {
-			panic(err)
-		}
-
-		return filepath.Join(home, strings.TrimPrefix(path, string(0x7E)))
-	}
-
-	return path
-}
+import "github.com/thecodeisalreadydeployed/kubernetesinteractor"
 
 func main() {
-	var kubeconfig string
-	pflag.StringVar(&kubeconfig, "kubeconfig", "", "")
-	pflag.Parse()
-	kubeconfig = absolutePath(kubeconfig)
-
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		panic(err)
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err)
-	}
-
-	kubernetesinteractor.CreateDeployment(clientset, &appsv1.Deployment{})
-	kubernetesinteractor.ListDeployments(clientset)
-
+	it := kubernetesinteractor.NewKubernetesInteractor()
+	it.ListDeployments()
 	return
 }
--- kubernetesinteractor/kubernetesinteractor.go
+++ kubernetesinteractor/kubernetesinteractor.go
@@ -4,15 +4,43 @@ import (
 	"context"
 	"fmt"
 
+	"path/filepath"
+	"strings"
+
 	"github.com/davecgh/go-spew/spew"
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/pflag"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 type KubernetesInteractor struct {
 	Clientset *kubernetes.Clientset
+}
+
+func NewKubernetesInteractor() KubernetesInteractor {
+	var kubeconfig string
+	pflag.StringVar(&kubeconfig, "kubeconfig", "", "")
+	pflag.Parse()
+	kubeconfig = absolutePath(kubeconfig)
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	return KubernetesInteractor{
+		Clientset: clientset,
+	}
 }
 
 func (it *KubernetesInteractor) ListDeployments() {
@@ -31,4 +59,21 @@ func (it *KubernetesInteractor) CreateDeployment(deployment *appsv1.Deployment) 
 		fmt.Printf("err: %v\n", err)
 	}
 	spew.Dump(result.GetObjectMeta())
+}
+
+func absolutePath(path string) string {
+	if filepath.IsAbs(path) {
+		return path
+	}
+
+	if strings.HasPrefix(path, string(0x7E)) {
+		home, err := homedir.Dir()
+		if err != nil {
+			panic(err)
+		}
+
+		return filepath.Join(home, strings.TrimPrefix(path, string(0x7E)))
+	}
+
+	return path
 }
