ef8fbddf813ff62a0905ace5c37dc5880b491e96
trif0lium <gh@fleeingsunlight.dev>
2021-08-14 19:21:55 +0700

    feat(kubernetesinteractor): CreateDeployment()

--- kubernetesinteractor/kubernetesinteractor.go
+++ kubernetesinteractor/kubernetesinteractor.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/davecgh/go-spew/spew"
+	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -17,4 +18,13 @@ func ListDeployments(clientset *kubernetes.Clientset) {
 		fmt.Printf("err: %v\n", err)
 	}
 	spew.Dump(deployments)
+}
+
+func CreateDeployment(clientset *kubernetes.Clientset, deployment *appsv1.Deployment) {
+	deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
+	result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+	spew.Dump(result.GetObjectMeta())
 }
