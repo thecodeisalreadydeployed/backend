18a2255a8f890aa1856eaf5e83e8f3366e372e6e
trif0lium <gh@fleeingsunlight.dev>
2021-08-14 16:44:49 +0700

    feat(kubernetesinteractor/cmd): call ListDeployments()

--- kubernetesinteractor/cmd/main.go
+++ kubernetesinteractor/cmd/main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"path/filepath"
+	"strings"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/pflag"
+	"github.com/thecodeisalreadydeployed/kubernetesinteractor"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func absolutePath(path string) string {
+	if filepath.IsAbs(path) {
+		return path
+	}
+
+	if strings.HasPrefix(path, string(0x7E)) {
+		home, err := homedir.Dir()
+		if err != nil {
+			panic(err)
+		}
+
+		return filepath.Join(home, strings.TrimPrefix(path, string(0x7E)))
+	}
+
+	return path
+}
+
 func main() {
+	var kubeconfig string
+	pflag.StringVar(&kubeconfig, "kubeconfig", "", "")
+	kubeconfig = absolutePath(kubeconfig)
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	kubernetesinteractor.ListDeployments()
+
 	return
 }
