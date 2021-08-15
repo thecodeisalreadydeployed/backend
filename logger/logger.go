fb2964a21c4669df1073c66542f5ea1eac425bbc
trif0lium <gh@fleeingsunlight.dev>
2021-08-15 18:24:12 +0700

    feat(logger): Init()

--- logger/logger.go
+++ logger/logger.go
@@ -1,1 +1,20 @@
 package logger
+
+import (
+	"sync"
+
+	"go.uber.org/zap"
+)
+
+var instance *zap.Logger
+var once sync.Once
+
+func Init() {
+	once.Do(func() {
+		logger, err := zap.NewProduction()
+		if err != nil {
+			panic(err)
+		}
+		instance = logger
+	})
+}
