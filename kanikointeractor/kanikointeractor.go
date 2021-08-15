59bee81364ea952733605ef5e886a88141ad6951
trif0lium <gh@fleeingsunlight.dev>
2021-08-15 10:40:26 +0700

    feat(kanikointeractor): add K8S Pod spec for `kaniko`

--- kanikointeractor/kanikointeractor.go
+++ kanikointeractor/kanikointeractor.go
@@ -4,6 +4,7 @@ import (
 	"github.com/thecodeisalreadydeployed/containerregistry"
 	"github.com/thecodeisalreadydeployed/kubernetesinteractor"
 	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type KanikoInteractor struct {
@@ -11,7 +12,20 @@ type KanikoInteractor struct {
 }
 
 func (it *KanikoInteractor) baseKanikoPodSpec() apiv1.Pod {
-	podSpec := apiv1.Pod{}
+	podSpec := apiv1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "kaniko",
+		},
+		Spec: apiv1.PodSpec{
+			RestartPolicy: apiv1.RestartPolicyNever,
+			Containers: []apiv1.Container{
+				{
+					Name:  "kaniko",
+					Image: "gcr.io/kaniko-project/executor:v1.6.0",
+				},
+			},
+		},
+	}
 	return podSpec
 }
 
