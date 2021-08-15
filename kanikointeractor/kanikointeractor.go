84bd9bc833e8d6a3fd0b9b4e7171fe01540baecc
trif0lium <gh@fleeingsunlight.dev>
2021-08-15 10:26:49 +0700

    feat(kanikointeractor): add baseKanikoPodSpec()

--- kanikointeractor/kanikointeractor.go
+++ kanikointeractor/kanikointeractor.go
@@ -10,7 +10,22 @@ type KanikoInteractor struct {
 	Registry containerregistry.ContainerRegistryType
 }
 
-func (it KanikoInteractor) BuildContainerImage() {
+func (it *KanikoInteractor) baseKanikoPodSpec() apiv1.Pod {
+	podSpec := apiv1.Pod{}
+	return podSpec
+}
+
+func (it *KanikoInteractor) gcrKanikoPodSpec() apiv1.Pod {
+	podSpec := it.baseKanikoPodSpec()
+	return podSpec
+}
+
+func (it *KanikoInteractor) ecrKanikoPodSpec() apiv1.Pod {
+	podSpec := it.baseKanikoPodSpec()
+	return podSpec
+}
+
+func (it *KanikoInteractor) BuildContainerImage() {
 	k8s := kubernetesinteractor.NewKubernetesInteractor()
 	k8s.CreatePod(&apiv1.Pod{})
 }
