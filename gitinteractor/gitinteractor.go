b936873b8cff2c3245fb95c53f25b478c42c5071
trif0lium <gh@fleeingsunlight.dev>
2021-08-15 16:03:40 +0700

    feat(gitinteractor): WriteFile()

--- gitinteractor/gitinteractor.go
+++ gitinteractor/gitinteractor.go
@@ -1,24 +1,31 @@
 package gitinteractor
 
 import (
+	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-billy/v5/util"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/storage"
 	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/thecodeisalreadydeployed/config"
 )
 
 type GitInteractor struct {
 	repository *git.Repository
+	storage    storage.Storer
+	fs         billy.Filesystem
 }
 
 func NewGitInteractor() GitInteractor {
 	it := GitInteractor{}
+	it.storage = memory.NewStorage()
+	it.fs = memfs.New()
 	it.Init()
 	return it
 }
 
 func (it *GitInteractor) Init() {
-	repo, err := git.Init(memory.NewStorage(), memfs.New())
+	repo, err := git.Init(it.storage, it.fs)
 	if err != nil {
 		panic(err)
 	}
@@ -39,4 +46,8 @@ func (it *GitInteractor) Commit(message string) {
 		panic(err)
 	}
 	w.Commit(message, &git.CommitOptions{Author: config.DefaultGitSignature()})
+}
+
+func (it *GitInteractor) WriteFile(name string, data []byte) {
+	util.WriteFile(it.fs, name, data, 0644)
 }
