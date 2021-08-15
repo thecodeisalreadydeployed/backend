2842c05d2b401a3ee067d80cb5fe75b677fb723c
trif0lium <gh@fleeingsunlight.dev>
2021-08-15 15:35:57 +0700

    feat(gitinteractor): NewGitInteractor() + Init()

--- gitinteractor/gitinteractor.go
+++ gitinteractor/gitinteractor.go
@@ -1,1 +1,25 @@
 package gitinteractor
+
+import (
+	"github.com/go-git/go-billy/v5/memfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/storage/memory"
+)
+
+type GitInteractor struct {
+	Repository *git.Repository
+}
+
+func NewGitInteractor() GitInteractor {
+	it := GitInteractor{}
+	it.Init()
+	return it
+}
+
+func (it *GitInteractor) Init() {
+	repo, err := git.Init(memory.NewStorage(), memfs.New())
+	if err != nil {
+		panic(err)
+	}
+	it.Repository = repo
+}
