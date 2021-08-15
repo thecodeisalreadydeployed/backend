04364ea15d41ad05df599b848989bdd2fce52762
trif0lium <gh@fleeingsunlight.dev>
2021-08-15 18:00:22 +0700

    feat(apiserver): GET /project/:projectID/app/:appID

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -12,4 +12,10 @@ func APIServer(port int) {
 		projectID := cast.ToString(c.Params("projectID"))
 		return c.SendString(projectID)
 	})
+
+	app.Get("/project/:projectID/app/:appID", func(c *fiber.Ctx) error {
+		projectID := cast.ToString(c.Params("projectID"))
+		appID := cast.ToString(c.Params("appID"))
+		return c.SendString(projectID)
+	})
 }
