55e96320b4ddd22ec2e1c3e08fc1ec7ee1dd359f
trif0lium <gh@fleeingsunlight.dev>
2021-08-16 11:58:47 +0700

    feat(gitopscontroller/logger): init

---
+++ gitopscontroller/logger.go
@@ -0,0 +1,10 @@
+package gitopscontroller
+
+import (
+	"github.com/thecodeisalreadydeployed/logger"
+	"go.uber.org/zap"
+)
+
+func Info(msg string) {
+	logger.Logger().Info(msg, zap.String("module", "gitopscontroller"))
+}
