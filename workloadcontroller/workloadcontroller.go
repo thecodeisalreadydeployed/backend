c7c44fb8b355dfa296ae6c91cdfc3cc5bdec994c
ChanyutJean <chanyut22@hotmail.com>
2021-08-18 15:09:49 +0700

    feat(workloadcontroller): mvp create workload

--- workloadcontroller/workloadcontroller.go
+++ workloadcontroller/workloadcontroller.go
@@ -1,1 +1,10 @@
 package workloadcontroller
+
+import (
+	"fmt"
+	"github.com/thecodeisalreadydeployed/model"
+)
+
+func CreateWorkload(w *model.Payload) {
+	fmt.Printf("Workload %s created.", w.Id)
+}
