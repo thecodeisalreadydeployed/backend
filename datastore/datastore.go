8a1bfc54917cbbc42a6f27b3976d7a16796947f3
ChanyutJean <chanyut22@hotmail.com>
2021-08-18 16:10:01 +0700

    feat(datastore): mvp datastore

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -1,1 +1,15 @@
 package datastore
+
+import "github.com/thecodeisalreadydeployed/model"
+
+func GetProject(p *model.Project) *model.Project {
+	return new(model.Project)
+}
+
+func GetApp(app *model.Project) *model.Project {
+	return new(model.Project)
+}
+
+func GetDeployment(dpl *model.Deployment) *model.Deployment {
+	return new(model.Deployment)
+}
