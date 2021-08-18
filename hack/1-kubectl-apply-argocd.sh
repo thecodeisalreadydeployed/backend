89abf103868454708eb794855a21635c454a5463
trif0lium <gh@fleeingsunlight.dev>
2021-08-18 11:37:56 +0700

    chore(hack): use thecodeisalreadydeployed/argo-cd fork

--- hack/1-kubectl-apply-argocd.sh
+++ hack/1-kubectl-apply-argocd.sh
@@ -1,3 +1,3 @@
 cd $(dirname $0)
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.5/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/thecodeisalreadydeployed/argo-cd/v2.0.5/manifests/install.yaml
