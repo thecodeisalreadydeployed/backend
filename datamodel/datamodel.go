2688ea5b4d3a85baeff526d45d87a03560431dd2
trif0lium <gh@fleeingsunlight.dev>
2021-08-18 15:08:01 +0700

    feat(datamodel): NewProjectFromModel()

--- datamodel/datamodel.go
+++ datamodel/datamodel.go
@@ -2,6 +2,10 @@ package datamodel
 
 import "github.com/thecodeisalreadydeployed/model"
 
+func NewProjectFromModel() Project {
+	return Project{}
+}
+
 func NewAppFromModel(app model.App) App {
 	return App{}
 }
