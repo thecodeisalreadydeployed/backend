a7f2984488e3b820c3e7e91042d8577677b97a19
ChanyutJean <chanyut22@hotmail.com>
2021-08-18 16:18:52 +0700

    feat(datastore): complete datastore mvp

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -6,10 +6,14 @@ func GetProject(p *model.Project) *model.Project {
 	return new(model.Project)
 }
 
-func GetApp(app *model.Project) *model.Project {
-	return new(model.Project)
+func GetApp(app *model.App) *model.App {
+	return new(model.App)
 }
 
 func GetDeployment(dpl *model.Deployment) *model.Deployment {
 	return new(model.Deployment)
+}
+
+func GetEvent(id string) string {
+	return "Dummy event."
 }
