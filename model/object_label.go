7944f82f10aeef6b8e6b13d03aee3b07682b3a06
trif0lium <gh@fleeingsunlight.dev>
2021-08-18 18:53:24 +0700

    feat(model/objectlabel): add PodLabel()

--- model/object_label.go
+++ model/object_label.go
@@ -5,3 +5,9 @@ func DeploymentLabel(deploymentID string) map[string]string {
 		"codedeploy/deployment-id": deploymentID,
 	}
 }
+
+func PodLabel(deploymentID string) map[string]string {
+	return map[string]string{
+		"codedeploy/deployment-id": deploymentID,
+	}
+}
