7afa412a57b4bcb693406a6dd26f173bc97d56f4
ChanyutJean <chanyut22@hotmail.com>
2021-08-18 15:09:05 +0700

    feat(apiserver): mvp create endpoint

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -1,8 +1,12 @@
 package apiserver
 
 import (
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/cast"
+	"github.com/thecodeisalreadydeployed/model"
+	"github.com/thecodeisalreadydeployed/workloadcontroller"
+	"log"
 )
 
 func APIServer(port int) {
@@ -27,4 +31,15 @@ func APIServer(port int) {
 		deploymentID := cast.ToString(c.Params("deploymentID"))
 		return c.SendString(deploymentID)
 	})
+
+	app.Post("/create", func(c *fiber.Ctx) error {
+		payload := model.Payload{}
+		if err := c.BodyParser(&payload); err != nil {
+			return c.SendStatus(500)
+		}
+		workloadcontroller.CreateWorkload(&payload)
+		return c.SendStatus(200)
+	})
+
+	log.Fatal(app.Listen(fmt.Sprintf(":%d", port)))
 }
