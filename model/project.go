dc5ad9a00ca880afaa93729ffe81f23b214004d3
trif0lium <gh@fleeingsunlight.dev>
2021-08-19 17:32:37 +0700

    feat(model/project): GenerateProjectID()

--- model/project.go
+++ model/project.go
@@ -1,10 +1,19 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/thecodeisalreadydeployed/util"
+)
 
 type Project struct {
 	ID        string
 	Name      string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func GenerateProjectID() string {
+	return fmt.Sprintf("prj_%s", util.RandomString(5))
 }
