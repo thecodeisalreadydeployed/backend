376efe32956a3766035c2648df19a1f5000185a7
trif0lium <gh@fleeingsunlight.dev>
2021-08-19 16:48:31 +0700

    feat(model/gitsource): add `json`

--- model/git_source.go
+++ model/git_source.go
@@ -1,9 +1,9 @@
 package model
 
 type GitSource struct {
-	Provider         string
-	Organization     string
-	CommitSHA        string
-	CommitMessage    string
-	CommitAuthorName string
+	Provider         string `json:"provider"`
+	Organization     string `json:"organization"`
+	CommitSHA        string `json:"commit_sha"`
+	CommitMessage    string `json:"commit_message"`
+	CommitAuthorName string `json:"commit_author_name"`
 }
