ed2570a27c8128134c0610b98b731518e4221e6f
trif0lium <gh@fleeingsunlight.dev>
2021-08-19 17:49:57 +0700

    feat(datamodel): NewAppFromModel()

--- datamodel/datamodel.go
+++ datamodel/datamodel.go
@@ -1,13 +1,34 @@
 package datamodel
 
-import "github.com/thecodeisalreadydeployed/model"
+import (
+	"encoding/json"
+
+	"github.com/spf13/cast"
+	"github.com/thecodeisalreadydeployed/model"
+)
 
 func NewProjectFromModel(prj model.Project) Project {
-	return Project{}
+	return Project{
+		ID:        prj.ID,
+		Name:      prj.Name,
+		CreatedAt: prj.CreatedAt,
+		UpdatedAt: prj.UpdatedAt,
+	}
 }
 
 func NewAppFromModel(app model.App) App {
-	return App{}
+	gitSource, err := json.Marshal(app.GitSource)
+	if err != nil {
+		panic(err)
+	}
+
+	return App{
+		ID:        app.ID,
+		Name:      app.Name,
+		GitSource: cast.ToString(gitSource),
+		CreatedAt: app.CreatedAt,
+		UpdatedAt: app.UpdatedAt,
+	}
 }
 
 func NewDeploymentFromModel(dpl model.Deployment) Deployment {
