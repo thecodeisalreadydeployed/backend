59ed324f1f6d733bf6408e58aedac85e5e380fd3
ChanyutJean <chanyut22@hotmail.com>
2021-08-20 15:40:01 +0700

    feat(manifestgenerator): add deployment builder (partial)

--- manifestgenerator/manifestgenerator.go
+++ manifestgenerator/manifestgenerator.go
@@ -1,1 +1,22 @@
 package manifestgenerator
+
+import (
+	"github.com/thecodeisalreadydeployed/manifestgenerator/dto"
+)
+
+func createDeploymentYAML(dpl *dto.Deployment) string {
+	var yb YAMLBuilder
+	yb.AppendApiVersion(dpl.ApiVersion)
+	yb.AppendKind("Deployment")
+	yb.AppendDeploymentMetadata(dpl.Name, dpl.Labels)
+	yb.EnterSpec()
+	yb.AppendReplicas(dpl.Replicas)
+	yb.Enter("selector:")
+	yb.Enter("matchLabels:")
+	yb.AppendLabels(dpl.Labels)
+	yb.Close(2)
+	yb.EnterTemplate()
+	yb.EnterLabels("metadata:", "labels:")
+	yb.EnterSpec()
+	yb.AppendContainers(dpl.ContainerSpec)
+}
