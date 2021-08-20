a0e26630fa38239a660afbfadcb3f62485365335
trif0lium <gh@fleeingsunlight.dev>
2021-08-20 21:52:56 +0700

    chore(dev/gitserver): init-repo

--- dev/gitserver/init-repo.sh
+++ dev/gitserver/init-repo.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-if [ "$#" != 2 ]
+if [ "$#" != 3 ]
 then
   exit 1
 fi
 
 USERNAME=$1
-REPO=$2.git
+PASSWORD=$2
+REPO=$3.git
 W=/__w
+
+mkdir -p $W/$USERNAME
 
 PATH=$W/$USERNAME/$REPO
 
