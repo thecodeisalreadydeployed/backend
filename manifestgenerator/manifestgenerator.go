c93359ec787740b973a1290241e7e230357e4682
ChanyutJean <chanyut22@hotmail.com>
2021-08-20 17:25:54 +0700

    feat(manifestgenerator): generate deployment

--- manifestgenerator/manifestgenerator.go
+++ manifestgenerator/manifestgenerator.go
@@ -2,21 +2,32 @@ package manifestgenerator
 
 import (
 	"github.com/thecodeisalreadydeployed/manifestgenerator/dto"
+	"github.com/thecodeisalreadydeployed/manifestgenerator/json"
+	"gopkg.in/yaml.v2"
 )
 
-func createDeploymentYAML(dpl *dto.Deployment) string {
-	var yb YAMLBuilder
-	yb.AppendApiVersion(dpl.ApiVersion)
-	yb.AppendKind("Deployment")
-	yb.AppendDeploymentMetadata(dpl.Name, dpl.Labels)
-	yb.EnterSpec()
-	yb.AppendReplicas(dpl.Replicas)
-	yb.Enter("selector:")
-	yb.Enter("matchLabels:")
-	yb.AppendLabels(dpl.Labels)
-	yb.Close(2)
-	yb.EnterTemplate()
-	yb.EnterLabels("metadata:", "labels:")
-	yb.EnterSpec()
-	yb.AppendContainers(dpl.ContainerSpec)
+func CreateDeploymentYAML(dpl *dto.Deployment) string {
+	var port json.Port
+	port.ContainerPort                        = []int{ dpl.ContainerSpec.Port }
+
+	var ctn json.Container
+	ctn.ContainerName                         = dpl.ContainerSpec.Name
+	ctn.ContainerImage                        = dpl.ContainerSpec.Image
+	ctn.ContainerPorts                        = []json.Port{ port }
+
+	var obj json.Deployment
+	obj.ApiVersion                            = dpl.ApiVersion
+	obj.Kind                                  = "Deployment"
+	obj.Metadata.Name                         = dpl.Name
+	obj.Metadata.Labels                       = dpl.Labels
+	obj.Spec.Replicas                         = dpl.Replicas
+	obj.Spec.Selector.Labels                  = dpl.Labels
+	obj.Spec.Template.TemplateMetadata.Labels = dpl.Labels
+	obj.Spec.Template.TemplateSpec.Containers = []json.Container{ ctn }
+
+	y, err := yaml.Marshal(&obj)
+	if err != nil {
+		panic(err)
+	}
+	return string(y)
 }
