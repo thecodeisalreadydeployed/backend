4d741017ecf9d455c276d82c349f524a88542329
ChanyutJean <chanyut22@hotmail.com>
2021-08-20 17:26:20 +0700

    refactor(manifestgenerator): fix struct layout

--- manifestgenerator/json/deployment.go
+++ manifestgenerator/json/deployment.go
@@ -1,47 +1,47 @@
 package json
 
 type Deployment struct {
-	ApiVersion string `json:"apiVersion"`
-	Kind string `json:"kind"`
-	Metadata Metadata `json:"metadata"`
-	Spec Spec `json:"spec"`
+	ApiVersion       string            `json:"apiVersion"`
+	Kind             string            `json:"kind"`
+	Metadata         Metadata          `json:"metadata"`
+	Spec             Spec              `json:"spec"`
 }
 
 type Metadata struct {
-	Name string `json:"name"`
-	Labels map[string]string `json:"labels"`
+	Name             string            `json:"name"`
+	Labels           map[string]string `json:"labels"`
 }
 
 type Spec struct {
-	Replicas int `json:"replicas"`
-	Selector Selector `json:"selector"`
-	Template Template `json:"template"`
+	Replicas         int               `json:"replicas"`
+	Selector         Selector          `json:"selector"`
+	Template         Template          `json:"template"`
 }
 
 type Selector struct {
-	MatchLabels map[string]string `json:"matchLabels"`
+	Labels           map[string]string `json:"matchLabels"`
 }
 
 type Template struct {
-	Metadata TemplateMetadata `json:"metadata"`
-	Spec TemplateSpec `json:"spec"`
+	TemplateMetadata TemplateMetadata  `json:"metadata"`
+	TemplateSpec     TemplateSpec      `json:"spec"`
 }
 
 type TemplateMetadata struct {
-	Labels map[string]string `json:"labels"`
+	Labels           map[string]string `json:"labels"`
 }
 
 type TemplateSpec struct {
-	Containers []Container
+	Containers       []Container       `json:"containers"`
 }
 
 type Container struct {
-	Name string
-	Image string
-	Ports []Port
+	ContainerName    string            `json:"name"`
+	ContainerImage   string            `json:"image"`
+	ContainerPorts   []Port            `json:"ports"`
 }
 
 type Port struct {
-	ContainerPort []int
+	ContainerPort    []int             `json:"containerPort"`
 }
 
