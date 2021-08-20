4b93f56a65f8c4782066128251eeff3cea7c025f
trif0lium <gh@fleeingsunlight.dev>
2021-08-20 21:49:06 +0700

    chore(dev/gitserver): remove `adduser`

--- dev/gitserver/init-user.sh
+++ dev/gitserver/init-user.sh
@@ -9,10 +9,4 @@ USERNAME=$1
 PASSWORD=$2
 W=/__w
 
-adduser -D -h "/home/$USERNAME" -s '/usr/bin/git-shell' $USERNAME
-echo "$USERNAME:$PASSWORD" | chpasswd
-
 mkdir -p $W/$USERNAME
-
-chown $USERNAME.$USERNAME -R $W/$USERNAME
-chmod 700 -R $W/$USERNAME
