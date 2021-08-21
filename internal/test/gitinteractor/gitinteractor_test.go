aa24bfd5051956bf27935ab6ade2335014508e2e
trif0lium <gh@fleeingsunlight.dev>
2021-08-21 14:33:17 +0700

    chore(test/gitinteractor): use ssh:// prefix

--- internal/test/gitinteractor/gitinteractor_test.go
+++ internal/test/gitinteractor/gitinteractor_test.go
@@ -11,7 +11,7 @@ import (
 var privateKey = flag.String("private-key", "", "")
 
 func TestGitInteractor_Clone(t *testing.T) {
-	git := it.NewGitInteractorSSH("git@localhost:2222/__w/repos/userspace", *privateKey)
+	git := it.NewGitInteractorSSH("ssh://codedeploy@localhost:2222/__w/repos/userspace", *privateKey)
 	log := git.Log()
 	assert.Equal(t, 1, len(log))
 }
