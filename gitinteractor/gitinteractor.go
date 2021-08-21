5627759035bb65d316f59cceade83075bb062cd7
trif0lium <gh@fleeingsunlight.dev>
2021-08-21 12:18:00 +0700

    feat(gitinteractor): NewGitInteractorSSH()

--- gitinteractor/gitinteractor.go
+++ gitinteractor/gitinteractor.go
@@ -1,10 +1,14 @@
 package gitinteractor
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-billy/v5/util"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/storage"
 	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/thecodeisalreadydeployed/config"
@@ -21,6 +25,24 @@ func NewGitInteractor() GitInteractor {
 	it.storage = memory.NewStorage()
 	it.fs = memfs.New()
 	it.Init()
+	return it
+}
+
+func NewGitInteractorSSH(url string) GitInteractor {
+	if strings.HasPrefix(url, "ssh://") {
+		panic("Repository URL has no ssh:// prefix.")
+	}
+
+	it := GitInteractor{}
+
+	r, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
+		URL: url,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	it.repository = r
 	return it
 }
 
@@ -62,4 +84,23 @@ func (it *GitInteractor) WriteFile(path string, name string, data []byte) {
 	}
 
 	util.WriteFile(fs, fs.Join(path, name), data, 0644)
+}
+
+func (it *GitInteractor) Log() []string {
+	messages := []string{}
+	r := it.repository
+	ref, err := r.Head()
+	if err != nil {
+		panic(err)
+	}
+	cIter, err := r.Log(&git.LogOptions{From: ref.Hash()})
+	err = cIter.ForEach(func(c *object.Commit) error {
+		fmt.Println(c)
+		messages = append(messages, c.Message)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return messages
 }
