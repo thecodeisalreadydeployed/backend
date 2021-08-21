9f10ff53a274ee95cecd3826b3a6d8e352e0513a
trif0lium <gh@fleeingsunlight.dev>
2021-08-21 19:59:21 +0700

    chore(test/gitinteractor): use `git` protocol

--- internal/test/gitinteractor/gitinteractor_test.go
+++ internal/test/gitinteractor/gitinteractor_test.go
@@ -11,7 +11,7 @@ import (
 var privateKey = flag.String("private-key", "", "")
 
 func TestGitInteractor_Clone(t *testing.T) {
-	git := it.NewGitInteractorSSH("ssh://codedeploy@thecodeisalreadydeployed.local/__w/repos/userspace", *privateKey)
+	git := it.NewGitInteractorSSH("git@thecodeisalreadydeployed.local:__w/repos/userspace", *privateKey)
 	log := git.Log()
 	assert.Equal(t, 1, len(log))
 }
