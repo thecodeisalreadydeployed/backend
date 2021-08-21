27895f26a5ef5d48fbb0fc46afb98f825a1de339
trif0lium <gh@fleeingsunlight.dev>
2021-08-21 14:30:41 +0700

    feat(test/gitinteractor): use Go `flag`

--- internal/test/gitinteractor/gitinteractor_test.go
+++ internal/test/gitinteractor/gitinteractor_test.go
@@ -1,17 +1,16 @@
 package gitinteractor
 
 import (
+	"flag"
 	"testing"
 
-	"github.com/spf13/pflag"
 	"github.com/stretchr/testify/assert"
 	it "github.com/thecodeisalreadydeployed/gitinteractor"
 )
 
-var privateKey = pflag.String("private-key", "", "")
+var privateKey = flag.String("private-key", "", "")
 
 func TestGitInteractor_Clone(t *testing.T) {
-	pflag.Parse()
 	git := it.NewGitInteractorSSH("git@localhost:2222/__w/repos/userspace", *privateKey)
 	log := git.Log()
 	assert.Equal(t, 1, len(log))
