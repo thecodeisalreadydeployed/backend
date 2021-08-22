92563f24fb1ebc35d93c7f475fc7659042476958
trif0lium <gh@fleeingsunlight.dev>
2021-08-22 18:17:43 +0700

    refactor(datastore): move GetAppDeployments() to datastore/deployment.go

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -2,28 +2,7 @@ package datastore
 
 import (
 	"fmt"
-
-	"github.com/thecodeisalreadydeployed/datamodel"
-	"github.com/thecodeisalreadydeployed/logger"
 )
-
-func GetAppDeployments(key string) []datamodel.BareDeployment {
-	var res []datamodel.BareDeployment
-	err := getDB().Table("deployments").Where(datamodel.Deployment{AppID: key}).Scan(&res).Error
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	return res
-}
-
-func GetDeploymentByID(dpl datamodel.Deployment) datamodel.BareDeployment {
-	var res datamodel.BareDeployment
-	err := getDB().Table("deployments").Where("ID = ?", dpl.ID).Scan(&res).Error
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	return res
-}
 
 func GetEvent(id string) string {
 	return fmt.Sprintf("Dummy event %s.", id)
