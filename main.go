a233bd3af6548a5447c00c5faa271e598cfa6a58
ChanyutJean <chanyut22@hotmail.com>
2021-08-22 15:38:44 +0700

    fix(main): restore main func

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"github.com/thecodeisalreadydeployed/apiserver"
-	"github.com/thecodeisalreadydeployed/datastore"
 )
 
 func main() {
-	db := datastore.GetDB()
-	datastore.InitDB(db)
-	//p := datastore.GetProject(db, &datamodel.Project{ID: "11"})
-	//fmt.Println(p)
 	apiserver.APIServer(3000)
 }
