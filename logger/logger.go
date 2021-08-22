34a4a58e7945f351380a1206781fce97f8dc242e
ChanyutJean <chanyut22@hotmail.com>
2021-08-22 15:38:27 +0700

    feat(logger): add log level

--- logger/logger.go
+++ logger/logger.go
@@ -27,6 +27,22 @@ func Logger() *zap.Logger {
 	return instance
 }
 
+func Debug(message string, fields ...zap.Field) {
+	Logger().Debug(message, fields...)
+}
+
 func Info(message string, fields ...zap.Field) {
 	Logger().Info(message, fields...)
+}
+
+func Warn(message string, fields ...zap.Field) {
+	Logger().Warn(message, fields...)
+}
+
+func Error(message string, fields ...zap.Field) {
+	Logger().Error(message, fields...)
+}
+
+func Fatal(message string, fields ...zap.Field) {
+	Logger().Fatal(message, fields...)
 }
