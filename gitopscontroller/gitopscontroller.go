78ddcf879b6ec597e5ea6ab8d81b07770e8b8a1d
trif0lium <gh@fleeingsunlight.dev>
2021-08-22 10:12:09 +0700

    feat(gitopscontroller): SetupUserspace()

--- gitopscontroller/gitopscontroller.go
+++ gitopscontroller/gitopscontroller.go
@@ -1,6 +1,9 @@
 package gitopscontroller
 
-import "github.com/thecodeisalreadydeployed/gitinteractor"
+import (
+	"github.com/thecodeisalreadydeployed/config"
+	"github.com/thecodeisalreadydeployed/gitinteractor"
+)
 
 type GitOpsController struct {
 	gitInteractor *gitinteractor.GitInteractor
@@ -9,4 +12,9 @@ type GitOpsController struct {
 func (c *GitOpsController) Init() {
 	git := gitinteractor.NewGitInteractor()
 	c.gitInteractor = &git
+}
+
+func SetupUserspace() error {
+	err := gitinteractor.InitRepository(config.DefaultUserspaceRepository)
+	return err
 }
