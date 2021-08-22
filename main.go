4737773e39a3e3c6dd26aebfc142ba2755a08e51
ChanyutJean <chanyut22@hotmail.com>
2021-08-22 16:41:39 +0700

    feat(main): init db every time server starts

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/thecodeisalreadydeployed/apiserver"
+	"github.com/thecodeisalreadydeployed/datastore"
 )
 
 func main() {
+	datastore.InitDB()
 	apiserver.APIServer(3000)
 }
