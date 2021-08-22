8f30af82b2c1b0ef08c588dade9669314075ae02
ChanyutJean <chanyut22@hotmail.com>
2021-08-22 14:09:27 +0700

    feat(datastore/seed): finish seed function

--- datastore/seed.go
+++ datastore/seed.go
@@ -5,35 +5,98 @@ import (
 	faker "github.com/bxcodec/faker/v3"
 	"github.com/thecodeisalreadydeployed/datamodel"
 	"gorm.io/gorm"
+	"math/rand"
 )
 
 func seed(db *gorm.DB) {
+	//TODO: Updated at may come before created at
+
 	seedProjects(db, 20)
+	seedApps(db, 100)
+	seedDeployments(db, 500)
 }
 
-func seedProjects(db *gorm.DB, size int) {
-	exists := db.Table("projects").Find(&datamodel.Project{})
-	if exists == nil {
-		var ps []datamodel.Project
-		for i := 0; i < size; i++ {
-			var p datamodel.Project
-			err := faker.FakeData(&p)
-			if err != nil {
-				fmt.Println(err)
-			}
-			ps = append(ps, p)
-		}
-		db.Create(&ps)
-	} else {
-		fmt.Println("Table Project already seeded.")
+func checkSeedExists(db *gorm.DB, tablename string) {
+	var existing int64
+	err := db.Table(tablename).Count(&existing).Error
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else if existing > 0 {
+		fmt.Printf("Table %s already seeded.", tablename)
+		return
 	}
 }
 
-func seedApps(db *gorm.DB) {
+func seedProjects(db *gorm.DB, size int) {
+	checkSeedExists(db, "projects")
 
+	var data []datamodel.Project
+	for i := 0; i < size; i++ {
+		var datum datamodel.Project
+		err := faker.FakeData(&datum)
+		if err != nil {
+			fmt.Println(err)
+		}
+		data = append(data, datum)
+	}
+	db.Create(&data)
 }
 
-func seedDeployments(db *gorm.DB) {
+func seedApps(db *gorm.DB, size int) {
+	checkSeedExists(db, "apps")
+	var keys []string
+	err := db.Table("projects").Select("ID").Scan(&keys).Error
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	var data []datamodel.App
+	for i := 0; i < size; i++ {
+		var datum datamodel.App
+		err := faker.FakeData(&datum)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		setAppForeignKey(&datum, keys)
+
+		data = append(data, datum)
+	}
+	db.Omit("Project").Create(&data)
+}
+
+func setAppForeignKey(datum *datamodel.App, keys []string) {
+	index := rand.Intn(len(keys))
+	datum.ProjectID = keys[index]
+}
+
+func seedDeployments(db *gorm.DB, size int) {
+	checkSeedExists(db, "deployments")
+
+	var keys []string
+	err := db.Table("apps").Select("ID").Scan(&keys).Error
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var data []datamodel.Deployment
+	for i := 0; i < size; i++ {
+		var datum datamodel.Deployment
+		err := faker.FakeData(&datum)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		setDeploymentForeignKey(&datum, keys)
+
+		data = append(data, datum)
+	}
+	db.Omit("App").Create(&data)
+}
+
+func setDeploymentForeignKey(datum *datamodel.Deployment, keys []string) {
+	index := rand.Intn(len(keys))
+	datum.AppID = keys[index]
 }
 
