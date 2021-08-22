49cca590fca2b64d11e87915ccbe7351df22a171
ChanyutJean <chanyut22@hotmail.com>
2021-08-22 12:34:23 +0700

    feat(datastore): postgres connection

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -1,9 +1,53 @@
 package datastore
 
-import "github.com/thecodeisalreadydeployed/model"
+import (
+	"fmt"
+	"github.com/thecodeisalreadydeployed/datamodel"
+	"github.com/thecodeisalreadydeployed/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
-func GetProject(p *model.Project) *model.Project {
-	return new(model.Project)
+func GetDB() *gorm.DB {
+	dsn := "host=localhost user=user password=password dbname=codedeploy port=5432 sslmode=disable TimeZone=Asia/Bangkok"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	createTableIfNotExists(db, &datamodel.Project{})
+	createTableIfNotExists(db, &datamodel.App{})
+	createTableIfNotExists(db, &datamodel.Deployment{})
+
+	return db
+}
+
+func createTableIfNotExists(db *gorm.DB, i interface{}) {
+	if !db.Migrator().HasTable(i) {
+		err := db.Migrator().CreateTable(i)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("hai")
+	}
+}
+
+//func GetAllProjects(p *model.Project) *[]model.Project {
+//
+//}
+//
+//func GetAllAppsFromProject(app *model.App) *[]model.App {
+//
+//}
+//
+//func GetAllDeploymentsFromApp(dpl *model.Deployment) *[]model.Deployment {
+//
+//}
+func GetProject(db *gorm.DB, p *datamodel.Project) *datamodel.Project {
+	GetDB()
+	var result datamodel.Project
+	db.First(&result)
+	return &result
 }
 
 func GetApp(app *model.App) *model.App {
