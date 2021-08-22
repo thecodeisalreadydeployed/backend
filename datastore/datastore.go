124666288db1ed914c05c52d8f6ee4c5b318e69a
trif0lium <gh@fleeingsunlight.dev>
2021-08-22 18:39:40 +0700

    feat(datastore): getDB() -> getInstance()

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -1,1 +1,26 @@
 package datastore
+
+import (
+	"github.com/thecodeisalreadydeployed/datamodel"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var database *gorm.DB
+
+func Init() {
+	dsn := "host=localhost user=user password=password dbname=codedeploy port=5432 sslmode=disable TimeZone=Asia/Bangkok"
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	database.AutoMigrate(&datamodel.Project{})
+	database.AutoMigrate(&datamodel.App{})
+	database.AutoMigrate(&datamodel.Deployment{})
+	seed(database)
+}
+
+func getInstance() *gorm.DB {
+	return database
+}
