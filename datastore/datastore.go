e7bbb915d8829bd90c103a79d03458da6750769b
trif0lium <gh@fleeingsunlight.dev>
2021-08-22 18:12:29 +0700

    refactor(datastore): move GetProjectApps() to datastore/app.go

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -7,24 +7,6 @@ import (
 	"github.com/thecodeisalreadydeployed/logger"
 )
 
-func GetProjectApps(key string) []datamodel.BareApp {
-	var res []datamodel.BareApp
-	err := getDB().Table("apps").Where(datamodel.App{ProjectID: key}).Scan(&res).Error
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	return res
-}
-
-func GetAppByID(app datamodel.App) datamodel.BareApp {
-	var res datamodel.BareApp
-	err := getDB().Table("apps").Where("ID = ?", app.ID).Scan(&res).Error
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	return res
-}
-
 func GetAppDeployments(key string) []datamodel.BareDeployment {
 	var res []datamodel.BareDeployment
 	err := getDB().Table("deployments").Where(datamodel.Deployment{AppID: key}).Scan(&res).Error
