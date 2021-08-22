486f378f7ecfc52f0aa47601865fa0cb3fa7814c
ChanyutJean <chanyut22@hotmail.com>
2021-08-22 13:05:59 +0700

    feat(main): test db connection

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/thecodeisalreadydeployed/apiserver"
+import (
+	"github.com/thecodeisalreadydeployed/apiserver"
+	"github.com/thecodeisalreadydeployed/datastore"
+)
 
 func main() {
+	db := datastore.GetDB()
+	datastore.InitDB(db)
+	//p := datastore.GetProject(db, &datamodel.Project{ID: "11"})
+	//fmt.Println(p)
 	apiserver.APIServer(3000)
 }
