90515ca4c319dc2ea757c79c7c264d1605ce3706
trif0lium <gh@fleeingsunlight.dev>
2021-08-23 19:29:29 +0700

    feat(model/project): add `json` tag

--- model/project.go
+++ model/project.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Project struct {
-	ID        string
-	Name      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func GenerateProjectID() string {
