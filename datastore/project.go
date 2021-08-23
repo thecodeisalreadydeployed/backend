e5da0066b94fb592a73e1750a358b7f8ad02483a
ChanyutJean <chanyut22@hotmail.com>
2021-08-23 16:35:10 +0700

    Revert "refactor(datastore/project): use gorm scan func"
    This reverts commit 764b9e22d85bf0a7ffbedf20160b2ccddad90d91.

--- datastore/project.go
+++ datastore/project.go
@@ -3,6 +3,7 @@ package datastore
 import (
 	"strings"
 
+	"github.com/thecodeisalreadydeployed/datamodel"
 	"github.com/thecodeisalreadydeployed/logger"
 	"github.com/thecodeisalreadydeployed/model"
 )
@@ -12,13 +13,13 @@ func GetProjectByID(projectID string) model.Project {
 		return model.Project{}
 	}
 
-	var data model.Project
-	err := getDB().Table("projects").Where("ID = ?", projectID).Scan(&data).Error
+	var _data datamodel.Project
+	err := getDB().Table("projects").Where("ID = ?", projectID).Scan(&_data).Error
 
 	if err != nil {
 		logger.Warn(err.Error())
 		return model.Project{}
 	}
 
-	return data
+	return _data.ToModel()
 }
