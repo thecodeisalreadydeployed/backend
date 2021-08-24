3efb859629c9bcc7a9a060786aa92bb174b2042e
trif0lium <gh@fleeingsunlight.dev>
2021-08-24 14:23:09 +0700

    feat(test/kanikointeractor): BuildContainerImage()

--- internal/test/kanikointeractor/kanikointeractor_test.go
+++ internal/test/kanikointeractor/kanikointeractor_test.go
@@ -4,8 +4,18 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/thecodeisalreadydeployed/containerregistry"
+	it "github.com/thecodeisalreadydeployed/kanikointeractor"
+	"github.com/thecodeisalreadydeployed/util"
 )
 
 func TestKanikoInteractor_BuildContainerImage(t *testing.T) {
-	assert.True(t, true)
+	interactor := it.KanikoInteractor{
+		Registry:     containerregistry.LOCAL,
+		BuildContext: "https://github.com/thecodeisalreadydeployed/fixture-nest.git",
+		DeploymentID: util.RandomString(5),
+	}
+
+	err := interactor.BuildContainerImage()
+	assert.Nil(t, err)
 }
