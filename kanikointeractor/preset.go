34ba93996291d2a97d9273f782ccc55047cf46cb
trif0lium <gh@fleeingsunlight.dev>
2021-08-24 19:46:17 +0700

    feat(kanikointeractor/preset): use text/template

--- kanikointeractor/preset.go
+++ kanikointeractor/preset.go
@@ -1,22 +1,42 @@
 package kanikointeractor
 
-import "fmt"
+import (
+	"bytes"
+	"text/template"
 
-func PresetNestJS(installCommand string, buildCommand string, outputDirectory string, startCommand string) string {
-	template := `
+	"github.com/spf13/cast"
+)
+
+type BuildOptions struct {
+	InstallCommand  string
+	BuildCommand    string
+	OutputDirectory string
+	StartCommand    string
+}
+
+func PresetNestJS(opts BuildOptions) (string, error) {
+	text := `
 FROM node:14-alpine as build-env
 WORKDIR /app
 ADD . /app
-RUN %s
-RUN %s
+RUN {{.InstallCommand}}
+RUN {{.BuildCommand}}
 
 FROM node:14-alpine
 WORKDIR /app
 COPY --from=build-env /app/package.json /app/yarn.lock ./
 COPY --from=build-env /app/node_modules ./node_modules
-COPY --from=build-env /app/%s ./%s
-CMD %s
+COPY --from=build-env /app/{{.OutputDirectory}} ./{{.OutputDirectory}}
+CMD {{.StartCommand}}
 	`
 
-	return fmt.Sprintf(template, installCommand, buildCommand, outputDirectory, outputDirectory, startCommand)
+	var buffer bytes.Buffer
+	t := template.Must(template.New("Dockerfile").Parse(text))
+	err := t.Execute(&buffer, t)
+
+	if err != nil {
+		return "", err
+	}
+
+	return cast.ToString(buffer), nil
 }
