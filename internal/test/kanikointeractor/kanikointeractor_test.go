74b405bd40e2d34672f384b1e8ef35d5cfcbb885
trif0lium <gh@fleeingsunlight.dev>
2021-08-24 12:41:25 +0700

    feat(test/kanikointeractor): add test for BuildContainerImage()

---
+++ internal/test/kanikointeractor/kanikointeractor_test.go
@@ -0,0 +1,11 @@
+package kanikointeractor
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestKanikoInteractor_BuildContainerImage(t *testing.T) {
+	assert.True(t, true)
+}
