f89aa422e30f74c78d8a803588ed1ab1199d9ba2
trif0lium <gh@fleeingsunlight.dev>
2021-08-25 21:56:50 +0700

    fix(manifestgenerator/json): ApiVersion -> APIVersion

--- manifestgenerator/json/deployment.go
+++ manifestgenerator/json/deployment.go
@@ -1,47 +1,46 @@
 package json
 
 type Deployment struct {
-	ApiVersion       string            `json:"apiVersion"`
-	Kind             string            `json:"kind"`
-	Metadata         Metadata          `json:"metadata"`
-	Spec             Spec              `json:"spec"`
+	APIVersion string   `json:"apiVersion"`
+	Kind       string   `json:"kind"`
+	Metadata   Metadata `json:"metadata"`
+	Spec       Spec     `json:"spec"`
 }
 
 type Metadata struct {
-	Name             string            `json:"name"`
-	Labels           map[string]string `json:"labels"`
+	Name   string            `json:"name"`
+	Labels map[string]string `json:"labels"`
 }
 
 type Spec struct {
-	Replicas         int               `json:"replicas"`
-	Selector         Selector          `json:"selector"`
-	Template         Template          `json:"template"`
+	Replicas int      `json:"replicas"`
+	Selector Selector `json:"selector"`
+	Template Template `json:"template"`
 }
 
 type Selector struct {
-	Labels           map[string]string `json:"matchLabels"`
+	Labels map[string]string `json:"matchLabels"`
 }
 
 type Template struct {
-	TemplateMetadata TemplateMetadata  `json:"metadata"`
-	TemplateSpec     TemplateSpec      `json:"spec"`
+	TemplateMetadata TemplateMetadata `json:"metadata"`
+	TemplateSpec     TemplateSpec     `json:"spec"`
 }
 
 type TemplateMetadata struct {
-	Labels           map[string]string `json:"labels"`
+	Labels map[string]string `json:"labels"`
 }
 
 type TemplateSpec struct {
-	Containers       []Container       `json:"containers"`
+	Containers []Container `json:"containers"`
 }
 
 type Container struct {
-	ContainerName    string            `json:"name"`
-	ContainerImage   string            `json:"image"`
-	ContainerPorts   []Port            `json:"ports"`
+	ContainerName  string `json:"name"`
+	ContainerImage string `json:"image"`
+	ContainerPorts []Port `json:"ports"`
 }
 
 type Port struct {
-	ContainerPort    int               `json:"containerPort"`
+	ContainerPort int `json:"containerPort"`
 }
-
