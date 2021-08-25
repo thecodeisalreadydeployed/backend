801219ef0391a4772f1d1f8f36095e55f11a063a
trif0lium <gh@fleeingsunlight.dev>
2021-08-25 21:20:35 +0700

    fix(internal/test): unused -> deadcode

--- internal/test/kanikointeractor/kanikointeractor_test.go
+++ internal/test/kanikointeractor/kanikointeractor_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/thecodeisalreadydeployed/util"
 )
 
-var kubeconfig = flag.String("kubeconfig", "", "") //nolint:golint,unused
+var kubeconfig = flag.String("kubeconfig", "", "") //nolint:golint,deadcode
 
 func TestKanikoInteractor_BuildContainerImage(t *testing.T) {
 	interactor := it.KanikoInteractor{
