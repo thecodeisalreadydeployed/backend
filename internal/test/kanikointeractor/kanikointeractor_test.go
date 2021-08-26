af2c5da0fc4487039ea8e4dcf1fd70cdc167eb88
trif0lium <gh@fleeingsunlight.dev>
2021-08-26 09:26:00 +0700

    test(kanikointeractor): registry.RegistryFormat()

--- internal/test/kanikointeractor/kanikointeractor_test.go
+++ internal/test/kanikointeractor/kanikointeractor_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/thecodeisalreadydeployed/containerregistry"
+	"github.com/thecodeisalreadydeployed/containerregistry/gcr"
 	it "github.com/thecodeisalreadydeployed/kanikointeractor"
 	"github.com/thecodeisalreadydeployed/util"
 )
@@ -13,13 +14,17 @@ import (
 var kubeconfig = flag.String("kubeconfig", "", "") //nolint
 
 func TestKanikoInteractor_BuildContainerImage(t *testing.T) {
+	registry := gcr.NewGCRGateway("asia.gcr.io", "hu-tao-mains")
+	destination, err := registry.RegistryFormat("fixture-monorepo", "dev")
+	assert.Nil(t, err)
+
 	interactor := it.KanikoInteractor{
-		Registry:     containerregistry.LOCAL,
+		Registry:     containerregistry.GCR,
 		BuildContext: "https://github.com/thecodeisalreadydeployed/fixture-monorepo.git",
 		DeploymentID: util.RandomString(5),
-		Destination:  "fixture-nest:dev",
+		Destination:  destination,
 	}
 
-	err := interactor.BuildContainerImage()
+	err = interactor.BuildContainerImage()
 	assert.Nil(t, err)
 }
