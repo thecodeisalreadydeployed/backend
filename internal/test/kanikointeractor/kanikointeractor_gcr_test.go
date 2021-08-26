88d94b894dbf745f8e98794bb2c077871e735280
trif0lium <gh@fleeingsunlight.dev>
2021-08-26 20:15:16 +0700

    test(kaniko): init kanikointeractor_gcr_test.go

---
+++ internal/test/kanikointeractor/kanikointeractor_gcr_test.go
@@ -0,0 +1,36 @@
+package kanikointeractor
+
+import (
+	"flag"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/thecodeisalreadydeployed/containerregistry"
+	gcr "github.com/thecodeisalreadydeployed/containerregistry/gcr"
+	it "github.com/thecodeisalreadydeployed/kanikointeractor"
+	"github.com/thecodeisalreadydeployed/util"
+)
+
+var kubeconfig = flag.String("kubeconfig", "", "")                 //nolint
+var serviceAccountKey = flag.String("gcp-service-account", "", "") //nolint
+
+func TestKanikoInteractor_BuildContainerImageGCR(t *testing.T) {
+	if os.Getenv("GITHUB_REPOSITORY") == "thecodeisalreadydeployed/backend" && os.Getenv("GITHUB_WORKFLOW") != "kaniko/gcr" {
+		t.Skip()
+	}
+
+	gateway := gcr.NewGCRGateway("asia.gcr.io", "hu-tao-mains", *serviceAccountKey)
+	destination, err := gateway.RegistryFormat("fixture-monorepo", "dev")
+	assert.Nil(t, err)
+
+	interactor := it.KanikoInteractor{
+		Registry:     containerregistry.LOCAL,
+		BuildContext: "https://github.com/thecodeisalreadydeployed/fixture-monorepo.git",
+		DeploymentID: util.RandomString(5),
+		Destination:  destination,
+	}
+
+	err = interactor.BuildContainerImage()
+	assert.Nil(t, err)
+}
