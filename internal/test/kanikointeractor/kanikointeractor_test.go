5f367605891d680772cda174e9aedc48792fc699
trif0lium <gh@fleeingsunlight.dev>
2021-08-26 22:10:47 +0700

    test(kaniko): remove containerregistry.LOCAL tests

--- internal/test/kanikointeractor/kanikointeractor_test.go
+++ internal/test/kanikointeractor/kanikointeractor_test.go
@@ -4,11 +4,6 @@ import (
 	"flag"
 	"os"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/thecodeisalreadydeployed/containerregistry"
-	it "github.com/thecodeisalreadydeployed/kanikointeractor"
-	"github.com/thecodeisalreadydeployed/util"
 )
 
 var kubeconfig = flag.String("kubeconfig", "", "") //nolint
@@ -17,14 +12,4 @@ func TestKanikoInteractor_BuildContainerImage(t *testing.T) {
 	if os.Getenv("GITHUB_REPOSITORY") != "thecodeisalreadydeployed/backend" {
 		t.Skip()
 	}
-
-	interactor := it.KanikoInteractor{
-		Registry:     containerregistry.LOCAL,
-		BuildContext: "https://github.com/thecodeisalreadydeployed/fixture-monorepo.git",
-		DeploymentID: util.RandomString(5),
-		Destination:  "fixture-nest:dev",
-	}
-
-	err := interactor.BuildContainerImage()
-	assert.Nil(t, err)
 }
