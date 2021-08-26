ee63bd916c9a14f1c578a7477629005f5bc05af9
trif0lium <gh@fleeingsunlight.dev>
2021-08-26 09:38:36 +0700

    test(kanikointeractor): BuildContainerImageGCR()

--- internal/test/kanikointeractor/kanikointeractor_test.go
+++ internal/test/kanikointeractor/kanikointeractor_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/thecodeisalreadydeployed/containerregistry"
+	gcr "github.com/thecodeisalreadydeployed/containerregistry/gcr"
 	it "github.com/thecodeisalreadydeployed/kanikointeractor"
 	"github.com/thecodeisalreadydeployed/util"
 )
@@ -21,5 +22,21 @@ func TestKanikoInteractor_BuildContainerImage(t *testing.T) {
 	}
 
 	err := interactor.BuildContainerImage()
+	assert.Nil(t, err)
+}
+
+func TestKanikoInteractor_BuildContainerImageGCR(t *testing.T) {
+	gateway := gcr.NewGCRGateway("asia.gcr.io", "hu-tao-mains")
+	destination, err := gateway.RegistryFormat("fixture-monorepo", "dev")
+	assert.Nil(t, err)
+
+	interactor := it.KanikoInteractor{
+		Registry:     containerregistry.LOCAL,
+		BuildContext: "https://github.com/thecodeisalreadydeployed/fixture-monorepo.git",
+		DeploymentID: util.RandomString(5),
+		Destination:  destination,
+	}
+
+	err = interactor.BuildContainerImage()
 	assert.Nil(t, err)
 }
