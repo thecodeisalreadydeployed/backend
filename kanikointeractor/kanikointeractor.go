981db59d301e215a3f432e5063be5de691f82570
trif0lium <gh@fleeingsunlight.dev>
2021-08-27 20:12:35 +0700

    feat(kaniko): add DeploymentState()

--- kanikointeractor/kanikointeractor.go
+++ kanikointeractor/kanikointeractor.go
@@ -1,9 +1,16 @@
 package kanikointeractor
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/davecgh/go-spew/spew"
 	"github.com/thecodeisalreadydeployed/containerregistry"
 	"github.com/thecodeisalreadydeployed/kubernetesinteractor"
+	"github.com/thecodeisalreadydeployed/model"
+	"go.uber.org/zap"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type KanikoInteractor struct {
@@ -30,4 +37,23 @@ func (it *KanikoInteractor) BuildContainerImage() error {
 	}
 
 	return nil
+}
+
+func DeploymentState(deploymentID string) model.DeploymentState {
+	k8s := kubernetesinteractor.NewKubernetesInteractor()
+	namespace := v1.NamespaceDefault
+	podName := fmt.Sprintf("kaniko-%s", deploymentID)
+	pod, podsErr := k8s.Clientset.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
+
+	if podsErr != nil {
+		zap.L().Sugar().Errorf("Cannot find pod '%s' in namespace '%s'.", podName, namespace)
+		return model.DeploymentStateError
+	}
+
+	if pod.Status.Phase == v1.PodSucceeded {
+		// TODO: Replace with model.DeploymentStateBuildSucceeded
+		return model.DeploymentStateBuilding
+	}
+
+	return model.DeploymentStateError
 }
