da3502e77e3b04ab7175d375ee8fda6e132b8b50
trif0lium <gh@fleeingsunlight.dev>
2021-08-28 18:39:23 +0700

    feat(workloadcontroller): NewDeployment(opts) -> NewDeployment(appID)

--- workloadcontroller/workloadcontroller.go
+++ workloadcontroller/workloadcontroller.go
@@ -2,6 +2,7 @@ package workloadcontroller
 
 import (
 	"github.com/thecodeisalreadydeployed/containerregistry/gcr"
+	"github.com/thecodeisalreadydeployed/datastore"
 	"github.com/thecodeisalreadydeployed/kanikointeractor"
 	manifest "github.com/thecodeisalreadydeployed/manifestgenerator"
 	"github.com/thecodeisalreadydeployed/util"
@@ -26,14 +27,17 @@ func NewApp(opts *NewAppOptions) error {
 	return nil
 }
 
-type NewDeploymentOptions struct {
-	GitRepositoryURL string
-}
+func NewDeployment(appID string) (string, error) {
+	app, getAppErr := datastore.GetAppByID(appID)
+	if getAppErr != nil {
+		return "", getAppErr
+	}
 
-func NewDeployment(opts *NewDeploymentOptions) (string, error) {
+	buildContext := app.GitSource.RepositoryURL
+
 	deploymentID := util.RandomString(5)
 	containerRegistry := gcr.NewGCRGateway("asia.gcr.io", "hu-tao-mains", "")
-	destination, err := containerRegistry.RegistryFormat("fixture-monorepo", "dev")
+	destination, err := containerRegistry.RegistryFormat("fixture-monorepo", deploymentID)
 
 	if err != nil {
 		return "", err
@@ -43,7 +47,7 @@ func NewDeployment(opts *NewDeploymentOptions) (string, error) {
 	kaniko := kanikointeractor.KanikoInteractor{
 		Registry:     containerRegistry,
 		Destination:  destination,
-		BuildContext: opts.GitRepositoryURL,
+		BuildContext: buildContext,
 		DeploymentID: deploymentID,
 	}
 
