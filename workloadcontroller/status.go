a4ce9e97a58dc6174f733640812d65fd225bf3ea
trif0lium <gh@fleeingsunlight.dev>
2021-08-28 13:15:29 +0700

    feat(workloadcontroller/status): CheckDeployment()

--- workloadcontroller/status.go
+++ workloadcontroller/status.go
@@ -1,10 +1,27 @@
 package workloadcontroller
 
 import (
+	"sync"
+
 	"github.com/thecodeisalreadydeployed/kanikointeractor"
 	"github.com/thecodeisalreadydeployed/model"
 )
 
+var deploymentsToCheck = sync.Map{} // [deploymentID]struct{}
+
 func DeploymentState(deploymentID string) model.DeploymentState {
 	return kanikointeractor.DeploymentState(deploymentID)
+}
+
+func CheckDeployments() {
+	for {
+		deploymentsToCheck.Range(func(key, value interface{}) bool {
+			deploymentID := key.(string)
+			state := DeploymentState(deploymentID)
+			if state == model.DeploymentStateBuildSucceeded {
+				deploymentsToCheck.Delete(key)
+			}
+			return true
+		})
+	}
 }
