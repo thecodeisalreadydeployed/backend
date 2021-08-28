03a0e9e3c71635ecce5572cdc05a6f7516db2d50
trif0lium <gh@fleeingsunlight.dev>
2021-08-28 12:04:53 +0700

    fix(workloadcontroller): properly handle error return value

--- workloadcontroller/workloadcontroller.go
+++ workloadcontroller/workloadcontroller.go
@@ -3,17 +3,23 @@ package workloadcontroller
 import (
 	manifest "github.com/thecodeisalreadydeployed/manifestgenerator"
 	"github.com/thecodeisalreadydeployed/util"
+	"go.uber.org/zap"
 )
 
 type NewAppOptions struct{}
 
-func NewApp(opts *NewAppOptions) bool {
-	manifest.GenerateDeploymentYAML(&manifest.GenerateDeploymentOptions{
+func NewApp(opts *NewAppOptions) error {
+	_, generateDeploymentErr := manifest.GenerateDeploymentYAML(&manifest.GenerateDeploymentOptions{
 		Name:           util.RandomString(5),
 		Namespace:      util.RandomString(5),
 		Labels:         map[string]string{},
 		ContainerImage: "k8s.gcr.io/ingress-nginx/controller:v1.0.0",
 	})
 
-	return true
+	if generateDeploymentErr != nil {
+		zap.L().Error(generateDeploymentErr.Error())
+		return generateDeploymentErr
+	}
+
+	return nil
 }
