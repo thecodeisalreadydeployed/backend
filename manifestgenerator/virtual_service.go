daedffaf833c85e5f295bdc11dab4983f8708fc5
trif0lium <gh@fleeingsunlight.dev>
2021-08-30 10:46:41 +0700

    feat(manifestgenerator): GenerateVirtualServerYAML()

--- manifestgenerator/virtual_service.go
+++ manifestgenerator/virtual_service.go
@@ -2,32 +2,31 @@ package manifestgenerator
 
 import (
 	"github.com/ghodss/yaml"
-	apiv1 "k8s.io/api/core/v1"
+	nginx "github.com/nginxinc/kubernetes-ingress/pkg/apis/configuration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type GenerateServiceOptions struct {
-	Name           string
-	Namespace      string
-	Labels         map[string]string
-	ContainerImage string
+type GenerateVirtualServerOptions struct {
+	Name      string
+	Namespace string
+	Labels    map[string]string
 }
 
-func GenerateServiceYAML(opts *GenerateServiceOptions) (string, error) {
-	srv := apiv1.Service{
+func GenerateVirtualServerYAML(opts *GenerateVirtualServerOptions) (string, error) {
+	virtualServer := nginx.VirtualServer{
 		TypeMeta: metav1.TypeMeta{
-			APIVersion: "apps/v1",
-			Kind:       "Service",
+			APIVersion: "k8s.nginx.org/v1",
+			Kind:       "VirtualServer",
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      opts.Name,
 			Namespace: opts.Namespace,
 			Labels:    opts.Labels,
 		},
-		Spec: apiv1.ServiceSpec{},
+		Spec: nginx.VirtualServerSpec{},
 	}
 
-	y, err := yaml.Marshal(srv)
+	y, err := yaml.Marshal(virtualServer)
 
 	return string(y), err
 }
