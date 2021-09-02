52173fe4a99c12ee13bd12c2ec2c40d35b6c62a3
trif0lium <gh@fleeingsunlight.dev>
2021-09-02 12:06:04 +0700

    refactor(repoobserver/cmd): NewGitInteractor() -> NewGitGateway()

--- repositoryobserver/cmd/main.go
+++ repositoryobserver/cmd/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/thecodeisalreadydeployed/gitinteractor"
+
+	"github.com/thecodeisalreadydeployed/gitgateway"
 	"github.com/thecodeisalreadydeployed/repositoryobserver"
 )
 
 func main() {
 	path := "/home/jean/Desktop/gittest"
-	it := gitinteractor.NewGitInteractor(path)
+	it := gitgateway.NewGitGateway(path)
 	repo := repositoryobserver.Repository{
 		SourceCode:    &it,
 		LastCommitSHA: "a7402afdac1147ed8908055e1d511f11418714c7",
