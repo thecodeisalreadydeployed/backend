28cb07a76222026b6228e6cffa59267aae5d0c2f
ChanyutJean <chanyut22@hotmail.com>
2021-09-03 11:35:15 +0700

    feat(manifestgenerator): mvp combine generators

--- manifestgenerator/manifestgenerator.go
+++ manifestgenerator/manifestgenerator.go
@@ -1,1 +1,42 @@
 package manifestgenerator
+
+import "go.uber.org/zap"
+
+func GenerateManifests() ([]string, error) {
+	deploymentYAML, err := GenerateDeploymentYAML(&GenerateDeploymentOptions{
+		Name:           "",
+		Namespace:      "",
+		Labels:         nil,
+		ContainerImage: "",
+	})
+
+	if err != nil {
+		zap.L().Error("Failed to generate deployment YAML.")
+		return nil, err
+	}
+
+	serviceYAML, err := GenerateServiceYAML(&GenerateServiceOptions{
+		Name:           "",
+		Namespace:      "",
+		Labels:         nil,
+		ContainerImage: "",
+	})
+
+	if err != nil {
+		zap.L().Error("Failed to generate service YAML.")
+		return nil, err
+	}
+
+	virtualServerYAML, err := GenerateVirtualServerYAML(&GenerateVirtualServerOptions{
+		Labels:    nil,
+		ProjectID: "",
+		AppID:     "",
+	})
+
+	if err != nil {
+		zap.L().Error("Failed to generate service YAML.")
+		return nil, err
+	}
+
+	return []string{deploymentYAML, serviceYAML, virtualServerYAML}, nil
+}
