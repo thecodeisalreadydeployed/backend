b033ee373d0e05c8e96d7dc91ba4f6892dc739ed
ChanyutJean <chanyut22@hotmail.com>
2021-09-03 11:34:28 +0700

    feat(main): observe every 5 minutes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/thecodeisalreadydeployed/apiserver"
 	"github.com/thecodeisalreadydeployed/datastore"
+	"github.com/thecodeisalreadydeployed/repositoryobserver"
 	"github.com/thecodeisalreadydeployed/workloadcontroller"
 	"go.uber.org/zap"
 )
@@ -20,5 +21,7 @@ func main() {
 
 	datastore.Init()
 	go workloadcontroller.CheckDeployments()
+
+	repositoryobserver.Observe()
 	apiserver.APIServer(3000)
 }
