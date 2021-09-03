febfda2c38758334703cdf6dc4b32a8d643dcd82
trif0lium <gh@fleeingsunlight.dev>
2021-09-03 20:42:33 +0700

    fix!(model/gitsource): remove GitGateway

--- model/git_source.go
+++ model/git_source.go
@@ -1,14 +1,11 @@
 package model
 
-import "github.com/thecodeisalreadydeployed/gitgateway"
-
 type GitSource struct {
-	Provider              string                 `json:"provider"`
-	Organization          string                 `json:"organization"`
-	CommitSHA             string                 `json:"commit_sha"`
-	CommitMessage         string                 `json:"commit_message"`
-	CommitAuthorName      string                 `json:"commit_author_name"`
-	RepositoryURL         string                 `json:"repository_url"`
-	LastObservedCommitSHA string                 `json:"last_observed_commit_sha"`
-	SourceCode            *gitgateway.GitGateway `json:"source_code"`
+	Provider              string `json:"provider"`
+	Organization          string `json:"organization"`
+	CommitSHA             string `json:"commit_sha"`
+	CommitMessage         string `json:"commit_message"`
+	CommitAuthorName      string `json:"commit_author_name"`
+	RepositoryURL         string `json:"repository_url"`
+	LastObservedCommitSHA string `json:"last_observed_commit_sha"`
 }
