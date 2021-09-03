d4918ca617ecc1afc87be60f5724b39c6fd5f941
trif0lium <gh@fleeingsunlight.dev>
2021-09-03 20:23:25 +0700

    feat(repositoryobserver): OnGitSourceUpdate()

--- repositoryobserver/repositoryobserver.go
+++ repositoryobserver/repositoryobserver.go
@@ -1,7 +1,6 @@
 package repositoryobserver
 
 import (
-	"github.com/robfig/cron"
 	"github.com/thecodeisalreadydeployed/datastore"
 	"github.com/thecodeisalreadydeployed/gitgateway"
 	"github.com/thecodeisalreadydeployed/model"
@@ -16,44 +15,37 @@ func hasChanges(gs *model.GitSource) bool {
 }
 
 func Observe() {
-	c := cron.New()
-	err := c.AddFunc("0 */5 * * * *", func() {
-		zap.L().Info("Observing source code...")
+	zap.L().Info("Observing source code...")
 
-		_, err := datastore.GetAllApps()
-		if err != nil {
-			zap.L().Error("Error while observing apps. Cannot access database.")
-			return
-		}
-
-		sc := gitgateway.NewGitGateway("/home/jean/Desktop/gittest")
-
-		gs := model.GitSource{
-			Provider:              "",
-			Organization:          "",
-			CommitSHA:             "",
-			CommitMessage:         "",
-			CommitAuthorName:      "",
-			RepositoryURL:         "",
-			LastObservedCommitSHA: "a1d95e5b2ac18b8e1ad713d39de6e57a2479d4e2",
-			SourceCode:            &sc,
-		}
-
-		if hasChanges(&gs) {
-			zap.L().Info("Source code has changed. Deploying new revision...")
-			workloadcontroller.DeployNewRevision()
-		}
-
-		// TODO: App's source code should be valid.
-		//for _, app := range *apps {
-		//	if hasChanges(&app.GitSource) {
-		//		zap.L().Info("Source code has changed. Deploying new revision...")
-		//		workloadcontroller.DeployNewRevision()
-		//	}
-		//}
-	})
+	_, err := datastore.GetAllApps()
 	if err != nil {
-		panic(err)
+		zap.L().Error("Error while observing apps. Cannot access database.")
+		return
 	}
-	c.Start()
+
+	sc := gitgateway.NewGitGateway("/home/jean/Desktop/gittest")
+
+	gs := model.GitSource{
+		Provider:              "",
+		Organization:          "",
+		CommitSHA:             "",
+		CommitMessage:         "",
+		CommitAuthorName:      "",
+		RepositoryURL:         "",
+		LastObservedCommitSHA: "a1d95e5b2ac18b8e1ad713d39de6e57a2479d4e2",
+		SourceCode:            &sc,
+	}
+
+	if hasChanges(&gs) {
+		zap.L().Info("Source code has changed. Deploying new revision...")
+		workloadcontroller.OnGitSourceUpdate(true)
+	}
+
+	// TODO: App's source code should be valid.
+	//for _, app := range *apps {
+	//	if hasChanges(&app.GitSource) {
+	//		zap.L().Info("Source code has changed. Deploying new revision...")
+	//		workloadcontroller.DeployNewRevision()
+	//	}
+	//}
 }
