86d235da27494b9f3de51169a91a40f208c44306
trif0lium <gh@fleeingsunlight.dev>
2021-09-03 12:10:08 +0700

    chore(repositoryobserver): remove unused cmd/

--- repositoryobserver/cmd/main.go
+++ repositoryobserver/cmd/main.go
@@ -1,18 +1,3 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/thecodeisalreadydeployed/gitgateway"
-	"github.com/thecodeisalreadydeployed/repositoryobserver"
-)
-
-func main() {
-	path := "/home/jean/Desktop/gittest"
-	it := gitgateway.NewGitGateway(path)
-	repo := repositoryobserver.Repository{
-		SourceCode:    &it,
-		LastCommitSHA: "a7402afdac1147ed8908055e1d511f11418714c7",
-	}
-	fmt.Println(repo.HasChanges())
-}
+func main() {}
