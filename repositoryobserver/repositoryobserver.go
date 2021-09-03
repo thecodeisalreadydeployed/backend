c9413daf4f3fd251dd43fc4c32ec6565ab184888
ChanyutJean <chanyut22@hotmail.com>
2021-09-03 11:34:56 +0700

    feat(repositoryobserver): observe all apps every 5 minutes

--- repositoryobserver/repositoryobserver.go
+++ repositoryobserver/repositoryobserver.go
@@ -1,21 +1,59 @@
 package repositoryobserver
 
 import (
+	"github.com/robfig/cron"
+	"github.com/thecodeisalreadydeployed/datastore"
 	"github.com/thecodeisalreadydeployed/gitgateway"
+	"github.com/thecodeisalreadydeployed/model"
+	"github.com/thecodeisalreadydeployed/workloadcontroller"
+	"go.uber.org/zap"
 )
 
-type Repository struct {
-	SourceCode    *gitgateway.GitGateway
-	LastCommitSHA string
-}
-
-func (r *Repository) Init(path string) {
-	it := gitgateway.NewGitGateway(path)
-	r.SourceCode = &it
-}
-
-func (r *Repository) HasChanges() bool {
-	old := r.SourceCode.GetCommit(r.LastCommitSHA)
-	current := r.SourceCode.GetCurrentCommit()
+func hasChanges(gs *model.GitSource) bool {
+	old := gs.SourceCode.GetCommit(gs.LastObservedCommitSHA)
+	current := gs.SourceCode.GetCurrentCommit()
 	return gitgateway.HasProperDiff(old, current)
+}
+
+func Observe() {
+	c := cron.New()
+	err := c.AddFunc("0 */5 * * * *", func() {
+		zap.L().Info("Observing source code...")
+
+		_, err := datastore.GetAllApps()
+		if err != nil {
+			zap.L().Error("Error while observing apps. Cannot access database.")
+			return
+		}
+
+		sc := gitgateway.NewGitGateway("/home/jean/Desktop/gittest")
+
+		gs := model.GitSource{
+			Provider:              "",
+			Organization:          "",
+			CommitSHA:             "",
+			CommitMessage:         "",
+			CommitAuthorName:      "",
+			RepositoryURL:         "",
+			LastObservedCommitSHA: "a1d95e5b2ac18b8e1ad713d39de6e57a2479d4e2",
+			SourceCode:            &sc,
+		}
+
+		if hasChanges(&gs) {
+			zap.L().Info("Source code has changed. Deploying new revision...")
+			workloadcontroller.DeployNewRevision()
+		}
+
+		// TODO: App's source code should be valid.
+		//for _, app := range *apps {
+		//	if hasChanges(&app.GitSource) {
+		//		zap.L().Info("Source code has changed. Deploying new revision...")
+		//		workloadcontroller.DeployNewRevision()
+		//	}
+		//}
+	})
+	if err != nil {
+		panic(err)
+	}
+	c.Start()
 }
