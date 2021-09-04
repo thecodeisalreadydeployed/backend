0ae38e88f0c93c2f85248981c2ce50d6727d35d1
ChanyutJean <chanyut22@hotmail.com>
2021-09-04 11:49:16 +0700

    feat(datastore/project): create project

--- datastore/project.go
+++ datastore/project.go
@@ -1,10 +1,9 @@
 package datastore
 
 import (
-	"strings"
-
 	"github.com/thecodeisalreadydeployed/datamodel"
 	"github.com/thecodeisalreadydeployed/model"
+	"strings"
 )
 
 func GetProjectByID(projectID string) (*model.Project, error) {
@@ -22,4 +21,17 @@ func GetProjectByID(projectID string) (*model.Project, error) {
 
 	ret := _data.ToModel()
 	return &ret, nil
+}
+
+func CreateProject(_p *model.Project) error {
+	if !strings.HasPrefix(_p.ID, "prj_") {
+		return ErrInvalidArgument
+	}
+	p := datamodel.NewProjectFromModel(_p)
+	err := getDB().Create(p).Error
+
+	if err != nil {
+		return ErrCannotCreate
+	}
+	return nil
 }
