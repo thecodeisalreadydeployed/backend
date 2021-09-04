733ddc7866ff74babaa7c8b63baa6ba5b193d35d
ChanyutJean <chanyut22@hotmail.com>
2021-09-04 12:07:22 +0700

    feat(datastore/error): log message

--- datastore/error.go
+++ datastore/error.go
@@ -9,3 +9,9 @@ var (
 	ErrInvalidArgument    = errors.New("ErrInvalidArgument")
 	ErrCannotCreate       = errors.New("ErrCannotCreate")
 )
+
+var (
+	MsgProjectPrefix    = "Project must have a prj_ prefix."
+	MsgAppPrefix        = "App must have an app_ prefix."
+	MsgDeploymentPrefix = "Deployment must have a dpl_ prefix."
+)
