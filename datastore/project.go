d2c0e86f0bfdd1b10a2da74cbba5c991231d6de0
ChanyutJean <chanyut22@hotmail.com>
2021-09-04 12:07:42 +0700

    feat(datastore/project): proper log

--- datastore/project.go
+++ datastore/project.go
@@ -3,19 +3,22 @@ package datastore
 import (
 	"github.com/thecodeisalreadydeployed/datamodel"
 	"github.com/thecodeisalreadydeployed/model"
+	"go.uber.org/zap"
 	"strings"
 )
 
 func GetProjectByID(projectID string) (*model.Project, error) {
 	if !strings.HasPrefix(projectID, "prj_") {
+		zap.L().Error(MsgProjectPrefix)
 		return nil, ErrInvalidArgument
 	}
 
 	var _data datamodel.Project
 
-	result := getDB().First(&_data, "id = ?", projectID)
+	err := getDB().First(&_data, "id = ?", projectID).Error
 
-	if result.Error != nil {
+	if err != nil {
+		zap.L().Error(err.Error())
 		return nil, ErrNotFound
 	}
 
@@ -25,12 +28,14 @@ func GetProjectByID(projectID string) (*model.Project, error) {
 
 func CreateProject(_p *model.Project) error {
 	if !strings.HasPrefix(_p.ID, "prj_") {
+		zap.L().Error(MsgProjectPrefix)
 		return ErrInvalidArgument
 	}
 	p := datamodel.NewProjectFromModel(_p)
 	err := getDB().Create(p).Error
 
 	if err != nil {
+		zap.L().Error(err.Error())
 		return ErrCannotCreate
 	}
 	return nil
