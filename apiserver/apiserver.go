6f25a83ade04b874770f3d663422840a3949d4ab
ChanyutJean <chanyut22@hotmail.com>
2021-09-04 13:03:17 +0700

    feat(apiserver): delete request

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -13,7 +13,7 @@ import (
 func APIServer(port int) {
 	app := fiber.New()
 
-	app.Get("/project/:projectID", func(c *fiber.Ctx) error {
+	app.Get("/project/get/:projectID", func(c *fiber.Ctx) error {
 		projectID := c.Params("projectID")
 		result, err := datastore.GetProjectByID(projectID)
 		if err != nil {
@@ -22,7 +22,7 @@ func APIServer(port int) {
 		return c.JSON(result)
 	})
 
-	app.Get("/project/:projectID/apps", func(c *fiber.Ctx) error {
+	app.Get("/project/get/:projectID/apps", func(c *fiber.Ctx) error {
 		result, err := datastore.GetAppsByProjectID(c.Params("projectID"))
 		if err != nil {
 			return fiber.NewError(mapStatusCode(err))
@@ -30,7 +30,7 @@ func APIServer(port int) {
 		return c.JSON(result)
 	})
 
-	app.Get("/app/:appID", func(c *fiber.Ctx) error {
+	app.Get("/app/get/:appID", func(c *fiber.Ctx) error {
 		appID := c.Params("appID")
 		result, err := datastore.GetAppByID(appID)
 		if err != nil {
@@ -39,7 +39,7 @@ func APIServer(port int) {
 		return c.JSON(result)
 	})
 
-	app.Get("/app/:appID/deployments", func(c *fiber.Ctx) error {
+	app.Get("/app/get/:appID/deployments", func(c *fiber.Ctx) error {
 		result, err := datastore.GetDeploymentsByAppID(c.Params("appID"))
 		if err != nil {
 			return fiber.NewError(mapStatusCode(err))
@@ -47,7 +47,7 @@ func APIServer(port int) {
 		return c.JSON(result)
 	})
 
-	app.Get("/deployment/:deploymentID", func(c *fiber.Ctx) error {
+	app.Get("/deployment/get/:deploymentID", func(c *fiber.Ctx) error {
 		deploymentID := c.Params("deploymentID")
 		result, err := datastore.GetDeploymentByID(deploymentID)
 		if err != nil {
@@ -56,7 +56,7 @@ func APIServer(port int) {
 		return c.JSON(result)
 	})
 
-	app.Get("/deployment/:deploymentID/event", func(c *fiber.Ctx) error {
+	app.Get("/deployment/get/:deploymentID/event", func(c *fiber.Ctx) error {
 		event, err := datastore.GetEventByDeploymentID(c.Params("deploymentID"))
 		if err != nil {
 			return fiber.NewError(mapStatusCode(err))
@@ -64,7 +64,7 @@ func APIServer(port int) {
 		return c.SendString(event)
 	})
 
-	app.Post("/projects/save", func(c *fiber.Ctx) error {
+	app.Post("/project/save", func(c *fiber.Ctx) error {
 		payload := model.Project{}
 		if err := c.BodyParser(&payload); err != nil {
 			return fiber.NewError(fiber.StatusBadRequest)
@@ -75,12 +75,28 @@ func APIServer(port int) {
 		return c.SendStatus(fiber.StatusOK)
 	})
 
-	app.Post("/apps/save", func(c *fiber.Ctx) error {
+	app.Post("/app/save", func(c *fiber.Ctx) error {
 		payload := model.App{}
 		if err := c.BodyParser(&payload); err != nil {
 			return fiber.NewError(fiber.StatusBadRequest)
 		}
 		if err := datastore.SaveApp(&payload); err != nil {
+			return fiber.NewError(mapStatusCode(err))
+		}
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	app.Post("/project/remove/:projectID", func(c *fiber.Ctx) error {
+		projectID := c.Params("projectID")
+		if err := datastore.RemoveProject(projectID); err != nil {
+			return fiber.NewError(mapStatusCode(err))
+		}
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	app.Post("/app/remove/:appID", func(c *fiber.Ctx) error {
+		appID := c.Params("appID")
+		if err := datastore.RemoveApp(appID); err != nil {
 			return fiber.NewError(mapStatusCode(err))
 		}
 		return c.SendStatus(fiber.StatusOK)
