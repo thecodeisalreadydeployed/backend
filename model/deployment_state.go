d738a1d37e60586b4e29413ef5148323cfee1ec5
trif0lium <gh@fleeingsunlight.dev>
2021-09-15 18:40:42 +0700

    feat(model/deploymentstate): DeploymentStateCommited

--- model/deployment_state.go
+++ model/deployment_state.go
@@ -6,6 +6,7 @@ const (
 	DeploymentStateQueueing       DeploymentState = "DeploymentStateQueueing"
 	DeploymentStateBuilding       DeploymentState = "DeploymentStateBuilding"
 	DeploymentStateBuildSucceeded DeploymentState = "DeploymentStateBuildSucceeded"
+	DeploymentStateCommited       DeploymentState = "DeploymentStateCommited"
 	DeploymentStateReady          DeploymentState = "DeploymentStateReady"
 	DeploymentStateError          DeploymentState = "DeploymentStateError"
 )
