20f966160c2043d596b1b428b77b0cf7d85857b1
trif0lium <gh@fleeingsunlight.dev>
2021-09-15 18:20:31 +0700

    feat(model): add additional fields

--- model/deployment.go
+++ model/deployment.go
@@ -8,13 +8,23 @@ import (
 )
 
 type Deployment struct {
-	ID        string          `json:"id"`
-	AppID     string          `json:"app_id"`
-	Name      string          `json:"name"`
-	Creator   Actor           `json:"creator"`
-	Meta      string          `json:"meta"`
-	GitSource GitSource       `json:"git_source"`
-	BuiltAt   time.Time       `json:"builded_at"`
+	ID    string `json:"id"`
+	AppID string `json:"app_id"`
+
+	Creator Actor  `json:"creator"`
+	Meta    string `json:"meta"`
+
+	GitSource GitSource `json:"git_source"`
+
+	// The time that Deployment.State transitioned to DeploymentStateBuildSucceeded.
+	BuiltAt time.Time `json:"built_at"`
+
+	// The time that Deployment.State transitioned to DeploymentStateCommited.
+	CommitedAt time.Time `json:"commited_at"`
+
+	// The time that Deployment.State transitioned to DeploymentStateReady
+	DeployedAt time.Time `json:"deployed_at"`
+
 	CreatedAt time.Time       `json:"created_at"`
 	UpdatedAt time.Time       `json:"updated_at"`
 	State     DeploymentState `json:"state"`
