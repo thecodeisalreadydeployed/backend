bc9df985a8069bcfeb74670ae2b3049f5aa36b10
trif0lium <gh@fleeingsunlight.dev>
2021-09-22 15:38:20 +0700

    feat(datastore): os.Getenv()

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -1,6 +1,9 @@
 package datastore
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/thecodeisalreadydeployed/datamodel"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -9,7 +12,16 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	dsn := "host=localhost user=user password=password dbname=codedeploy port=5432 sslmode=disable TimeZone=Asia/Bangkok"
+	// dsn := "host=localhost user=user password=password dbname=codedeploy port=5432 sslmode=disable TimeZone=Asia/Bangkok"
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Bangkok",
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_USERNAME"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_NAME"),
+		os.Getenv("DATABASE_PORT"),
+	)
+
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
