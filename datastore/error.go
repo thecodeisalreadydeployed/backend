805aca14e4661cd27e3cb5f28e188859c342f033
trif0lium <gh@fleeingsunlight.dev>
2021-10-03 10:51:13 +0700

    chore(error): update error messages

--- datastore/error.go
+++ datastore/error.go
@@ -1,7 +1,7 @@
 package datastore
 
 var (
-	MsgProjectPrefix    = "Project must have a prj_ prefix."
-	MsgAppPrefix        = "App must have an app_ prefix."
-	MsgDeploymentPrefix = "Deployment must have a dpl_ prefix."
+	MsgProjectPrefix    = "Project must have the prj_ prefix."
+	MsgAppPrefix        = "App must have the app_ prefix."
+	MsgDeploymentPrefix = "Deployment must have the dpl_ prefix."
 )
