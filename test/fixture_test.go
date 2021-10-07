2c4c7822cf7e351d46618d9cadfa92c70255587f
trif0lium <gh@fleeingsunlight.dev>
2021-10-07 19:50:52 +0700

    test(fixture): init

---
+++ test/fixture_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/thecodeisalreadydeployed/apiserver/dto"
+)
+
+func testFixture(t *testing.T, fixtureRepo string) {
+	repoURL, urlParseErr := url.Parse(fixtureRepo)
+	assert.Nil(t, urlParseErr)
+
+	repo := strings.TrimLeft(repoURL.Path, "/")
+
+	projectName := "project/" + repo
+	appName := repo
+
+	expect := Setup(t)
+
+	expect.POST("/project").
+		WithForm(dto.CreateProjectRequest{Name: projectName}).
+		Expect().
+		Status(http.StatusOK)
+}
