e5d53dd99ffd557963a3f744dc03e9f4d6a98343
trif0lium <gh@fleeingsunlight.dev>
2021-10-09 11:57:08 +0700

    test(repositoryobserver): remove expectNil

--- repositoryobserver/repositoryobserver_test.go
+++ repositoryobserver/repositoryobserver_test.go
@@ -18,31 +18,24 @@ func TestCheck(t *testing.T) {
 			repoURL:       "https://github.com/thecodeisalreadydeployed/fixture-monorepo",
 			currentCommit: "37e8e4d20d889924780f2373453a246591b6b11a", // feat(nx): init Nx workspace
 			expect:        "5da29979c5ef986dc8ec6aa603e0862310abc96e", // build(dev-deps): @nrwl/next
-			expectNil:     false,
 			branch:        "main",
 		},
 		{
 			repoURL:       "https://github.com/thecodeisalreadydeployed/fixture-monorepo",
 			currentCommit: "5da29979c5ef986dc8ec6aa603e0862310abc96e",
 			expect:        "",
-			expectNil:     true,
 			branch:        "main",
 		},
 		{
 			repoURL:       "https://github.com/thecodeisalreadydeployed/fixture-nest",
 			currentCommit: "62139be31792ab4a43c00eadcc8af6cadd90ee66", // feat: init NestJS project
 			expect:        "14bc77fc515e6d66b8d9c15126ee49ca55faf879", // chore(app): Hello World -> fixture-nest
-			expectNil:     true,
 			branch:        "main",
 		},
 	}
 
 	for _, test := range testSuites {
 		commit := check(test.repoURL, test.branch, test.currentCommit)
-		if test.expectNil {
-			assert.Equal(t, nil, commit)
-		} else {
-			assert.Equal(t, test.expect, *commit)
-		}
+		assert.Equal(t, test.expect, commit)
 	}
 }
