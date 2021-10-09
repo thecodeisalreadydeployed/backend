6c16907c98c7a1b7b661f9cfd4d9125c8f3e52eb
trif0lium <gh@fleeingsunlight.dev>
2021-10-09 16:40:20 +0700

    feat(gitopscontroller): SetupUserspace()

--- gitopscontroller/gitopscontroller.go
+++ gitopscontroller/gitopscontroller.go
@@ -1,8 +1,29 @@
 package gitopscontroller
 
+import (
+	"sync"
+
+	"github.com/thecodeisalreadydeployed/config"
+	"github.com/thecodeisalreadydeployed/gitgateway/v2"
+)
+
 type GitOpsController interface {
 	SetupUserspace()
 	SetupProject(projectID string) error
 	SetupApp(projectID string, appID string) error
 	UpdateContainerImage(appID string, deploymentID string) error
+}
+
+type gitOpsController interface{}
+
+var once sync.Once
+
+func SetupUserspace() {
+	once.Do(func() {
+		path := config.DefaultUserspaceRepository
+		_, err := gitgateway.NewGitRepository(path)
+		if err != nil {
+			panic(err)
+		}
+	})
 }
