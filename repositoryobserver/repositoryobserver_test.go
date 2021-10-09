f7c097cc74cb89ebe9cc0f7ee64d0e9c32f03547
trif0lium <gh@fleeingsunlight.dev>
2021-10-09 14:59:16 +0700

    test(repositoryobserver): add `apps` + `expected`

--- repositoryobserver/repositoryobserver_test.go
+++ repositoryobserver/repositoryobserver_test.go
@@ -49,8 +49,36 @@ func TestCheck(t *testing.T) {
 }
 
 func TestCheckGitSources(t *testing.T) {
-	apps := []model.App{}
-	expected := map[string]string{}
+	apps := []model.App{
+		{
+			ID: "A",
+			GitSource: model.GitSource{
+				RepositoryURL: "https://github.com/thecodeisalreadydeployed/fixture-monorepo",
+				CommitSHA:     "37e8e4d20d889924780f2373453a246591b6b11a",
+				Branch:        "main",
+			},
+		},
+		{
+			ID: "B",
+			GitSource: model.GitSource{
+				RepositoryURL: "https://github.com/thecodeisalreadydeployed/fixture-monorepo",
+				CommitSHA:     "5da29979c5ef986dc8ec6aa603e0862310abc96e",
+				Branch:        "main",
+			},
+		},
+		{
+			ID: "C",
+			GitSource: model.GitSource{
+				RepositoryURL: "https://github.com/thecodeisalreadydeployed/fixture-nest",
+				CommitSHA:     "62139be31792ab4a43c00eadcc8af6cadd90ee66",
+				Branch:        "main",
+			},
+		},
+	}
+	expected := map[string]string{
+		"A": "5da29979c5ef986dc8ec6aa603e0862310abc96e",
+		"C": "bc77fc515e6d66b8d9c15126ee49ca55faf879",
+	}
 	actual := checkGitSources(apps)
 	assert.Equal(t, expected, actual)
 }
