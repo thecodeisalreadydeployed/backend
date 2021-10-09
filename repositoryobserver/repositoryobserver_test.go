16107478df3e7bfbf0e8e456c879918c378a1d1a
trif0lium <gh@fleeingsunlight.dev>
2021-10-09 12:03:26 +0700

    test(repositoryobserver): fix `expectNil`

--- repositoryobserver/repositoryobserver_test.go
+++ repositoryobserver/repositoryobserver_test.go
@@ -18,7 +18,7 @@ func TestCheck(t *testing.T) {
 			repoURL:       "https://github.com/thecodeisalreadydeployed/fixture-monorepo",
 			currentCommit: "37e8e4d20d889924780f2373453a246591b6b11a", // feat(nx): init Nx workspace
 			expect:        "5da29979c5ef986dc8ec6aa603e0862310abc96e", // build(dev-deps): @nrwl/next
-			expectNil:     true,
+			expectNil:     false,
 			branch:        "main",
 		},
 		{
@@ -32,7 +32,7 @@ func TestCheck(t *testing.T) {
 			repoURL:       "https://github.com/thecodeisalreadydeployed/fixture-nest",
 			currentCommit: "62139be31792ab4a43c00eadcc8af6cadd90ee66", // feat: init NestJS project
 			expect:        "14bc77fc515e6d66b8d9c15126ee49ca55faf879", // chore(app): Hello World -> fixture-nest
-			expectNil:     true,
+			expectNil:     false,
 			branch:        "main",
 		},
 	}
