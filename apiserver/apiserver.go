cb615a54216a4bec0a431e30239ffb30883898f9
ChanyutJean <chanyut22@hotmail.com>
2021-10-16 15:29:24 +0700

    feat(apiserver): grouping

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -2,17 +2,13 @@ package apiserver
 
 import (
 	"fmt"
-	"github.com/thecodeisalreadydeployed/preset"
+	"github.com/thecodeisalreadydeployed/apiserver/group"
 	"log"
-
-	"github.com/spf13/cast"
-	"github.com/thecodeisalreadydeployed/apiserver/dto"
-	"github.com/thecodeisalreadydeployed/apiserver/validator"
-	"github.com/thecodeisalreadydeployed/datastore"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/thecodeisalreadydeployed/apiserver/validator"
 )
 
 func APIServer(port int) {
@@ -22,132 +18,22 @@ func APIServer(port int) {
 	app.Use(cors.New())
 	app.Use(logger.New())
 
-	app.Get("/health", func(c *fiber.Ctx) error {
-		ok := datastore.IsReady()
-		return c.JSON(map[string]string{"ok": cast.ToString(ok)})
-	})
+	app.Get("/health", group.Health)
 
-	app.Get("/projects", func(c *fiber.Ctx) error {
-		result, err := datastore.GetAllProjects()
-		if err != nil {
-			return fiber.NewError(mapStatusCode(err))
-		}
-		return c.JSON(result)
-	})
+	app.Get("/projects", group.ProjectList)
+	app.Get("/project/:projectID", group.ProjectID)
+	app.Get("/project/:projectID/apps", group.ProjectApps)
+	app.Get("/app/:appID", group.AppID)
+	app.Get("/app/:appID/deployments", group.AppDeployments)
+	app.Get("/deployment/:deploymentID", group.DeploymentID)
+	app.Get("/deployment/:deploymentID/event", group.DeploymentEvent)
 
-	app.Get("/project/:projectID", func(c *fiber.Ctx) error {
-		projectID := c.Params("projectID")
-		result, err := datastore.GetProjectByID(projectID)
-		if err != nil {
-			return fiber.NewError(mapStatusCode(err))
-		}
-		return c.JSON(result)
-	})
+	app.Post("/project", group.PostProject)
+	app.Post("/app", group.PostApp)
+	app.Delete("/project/:projectID", group.DeleteProject)
+	app.Delete("/app/:appID", group.DeleteApp)
 
-	app.Get("/project/:projectID/apps", func(c *fiber.Ctx) error {
-		result, err := datastore.GetAppsByProjectID(c.Params("projectID"))
-		if err != nil {
-			return fiber.NewError(mapStatusCode(err))
-		}
-		return c.JSON(result)
-	})
-
-	app.Get("/app/:appID", func(c *fiber.Ctx) error {
-		appID := c.Params("appID")
-		result, err := datastore.GetAppByID(appID)
-		if err != nil {
-			return fiber.NewError(mapStatusCode(err))
-		}
-		return c.JSON(result)
-	})
-
-	app.Get("/app/:appID/deployments", func(c *fiber.Ctx) error {
-		result, err := datastore.GetDeploymentsByAppID(c.Params("appID"))
-		if err != nil {
-			return fiber.NewError(mapStatusCode(err))
-		}
-		return c.JSON(result)
-	})
-
-	app.Get("/deployment/:deploymentID", func(c *fiber.Ctx) error {
-		deploymentID := c.Params("deploymentID")
-		result, err := datastore.GetDeploymentByID(deploymentID)
-		if err != nil {
-			return fiber.NewError(mapStatusCode(err))
-		}
-		return c.JSON(result)
-	})
-
-	app.Get("/deployment/:deploymentID/event", func(c *fiber.Ctx) error {
-		event, err := datastore.GetEventByDeploymentID(c.Params("deploymentID"))
-		if err != nil {
-			return fiber.NewError(mapStatusCode(err))
-		}
-		return c.SendString(event)
-	})
-
-	app.Post("/project", func(c *fiber.Ctx) error {
-		request := dto.CreateProjectRequest{}
-		if err := c.BodyParser(&request); err != nil {
-			return fiber.NewError(fiber.StatusBadRequest)
-		}
-
-		if validationErrors := validator.Validate(request); len(validationErrors) > 0 {
-			return c.Status(fiber.StatusBadRequest).JSON(validationErrors)
-		}
-
-		prj := request.ToModel()
-
-		project, projectErr := datastore.SaveProject(&prj)
-
-		if projectErr != nil {
-			return fiber.NewError(mapStatusCode(projectErr))
-		}
-
-		return c.JSON(project)
-	})
-
-	app.Post("/app", func(c *fiber.Ctx) error {
-		request := dto.CreateAppRequest{}
-
-		if err := c.BodyParser(&request); err != nil {
-			return fiber.NewError(fiber.StatusBadRequest)
-		}
-
-		if validationErrors := validator.Validate(request); len(validationErrors) > 0 {
-			return c.Status(fiber.StatusBadRequest).JSON(validationErrors)
-		}
-
-		app := request.ToModel()
-
-		if err := datastore.SaveApp(&app); err != nil {
-			return fiber.NewError(mapStatusCode(err))
-		}
-
-		return c.SendStatus(fiber.StatusOK)
-	})
-
-	app.Delete("/project/:projectID", func(c *fiber.Ctx) error {
-		projectID := c.Params("projectID")
-		if err := datastore.RemoveProject(projectID); err != nil {
-			return fiber.NewError(mapStatusCode(err))
-		}
-		return c.SendStatus(fiber.StatusOK)
-	})
-
-	app.Delete("/app/:appID", func(c *fiber.Ctx) error {
-		appID := c.Params("appID")
-		if err := datastore.RemoveApp(appID); err != nil {
-			return fiber.NewError(mapStatusCode(err))
-		}
-		return c.SendStatus(fiber.StatusOK)
-	})
-
-	app.Get("/preset/:framework", func(c *fiber.Ctx) error {
-		framework := c.Params("framework")
-		text := preset.Text(preset.Framework(framework))
-		return c.SendString(text)
-	})
+	app.Get("/preset/:framework", group.Preset)
 
 	log.Fatal(app.Listen(fmt.Sprintf(":%d", port)))
 }
