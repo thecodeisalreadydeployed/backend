762fe3d1f0ccc41b121150e454d147e30a99e9ed
trif0lium <gh@fleeingsunlight.dev>
2021-10-17 16:16:18 +0700

    feat!(apiserver): NewProjectController()

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -2,8 +2,9 @@ package apiserver
 
 import (
 	"fmt"
-	"github.com/thecodeisalreadydeployed/apiserver/group"
 	"log"
+
+	"github.com/thecodeisalreadydeployed/apiserver/controller"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -18,22 +19,7 @@ func APIServer(port int) {
 	app.Use(cors.New())
 	app.Use(logger.New())
 
-	app.Get("/health", group.Health)
-
-	app.Get("/projects", group.ProjectList)
-	app.Get("/project/:projectID", group.ProjectID)
-	app.Get("/project/:projectID/apps", group.ProjectApps)
-	app.Get("/app/:appID", group.AppID)
-	app.Get("/app/:appID/deployments", group.AppDeployments)
-	app.Get("/deployment/:deploymentID", group.DeploymentID)
-	app.Get("/deployment/:deploymentID/event", group.DeploymentEvent)
-
-	app.Post("/project", group.PostProject)
-	app.Post("/app", group.PostApp)
-	app.Delete("/project/:projectID", group.DeleteProject)
-	app.Delete("/app/:appID", group.DeleteApp)
-
-	app.Get("/preset/:framework", group.Preset)
+	controller.NewProjectController(app.Group("projects"))
 
 	log.Fatal(app.Listen(fmt.Sprintf(":%d", port)))
 }
