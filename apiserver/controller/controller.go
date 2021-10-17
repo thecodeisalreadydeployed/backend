ae508aa8b8fea8a382957e52035c70e597207734
trif0lium <gh@fleeingsunlight.dev>
2021-10-17 09:29:14 +0700

    feat(apiserver/controller): add ModelController interface

--- apiserver/controller/controller.go
+++ apiserver/controller/controller.go
@@ -7,3 +7,26 @@ type Controller interface {
 	Update()
 	Delete()
 }
+
+type ModelController interface {
+	Name() string
+	Controller() Controller
+}
+
+type modelController struct {
+	name       string
+	controller Controller
+}
+
+func NewModelController(name string, controller Controller) ModelController {
+	c := &modelController{name: name, controller: controller}
+	return c
+}
+
+func (c *modelController) Name() string {
+	return c.name
+}
+
+func (c *modelController) Controller() Controller {
+	return c.controller
+}
