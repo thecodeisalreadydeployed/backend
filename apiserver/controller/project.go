354777c887fe22cbddb3d5b1007460d63191794d
trif0lium <gh@fleeingsunlight.dev>
2021-10-17 16:15:01 +0700

    feat(apiserver/controller): init project.go

---
+++ apiserver/controller/project.go
@@ -0,0 +1,19 @@
+package controller
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/thecodeisalreadydeployed/apiserver/errutil"
+	"github.com/thecodeisalreadydeployed/datastore"
+)
+
+func NewProjectController(api fiber.Router) {
+	api.Get("/", listProjects)
+}
+
+func listProjects(ctx *fiber.Ctx) error {
+	result, err := datastore.GetAllProjects()
+	if err != nil {
+		return fiber.NewError(errutil.MapStatusCode(err))
+	}
+	return ctx.JSON(result)
+}
