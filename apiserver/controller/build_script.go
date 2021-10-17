54e04b9ee6b1e20574ff0118422a44ea7a7f15b7
trif0lium <gh@fleeingsunlight.dev>
2021-10-17 17:45:18 +0700

    feat(controller/build_script): ctx.BodyParser()

--- apiserver/controller/build_script.go
+++ apiserver/controller/build_script.go
@@ -3,6 +3,9 @@ package controller
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/cast"
+	"github.com/thecodeisalreadydeployed/apiserver/dto"
+	"github.com/thecodeisalreadydeployed/apiserver/errutil"
+	"github.com/thecodeisalreadydeployed/apiserver/validator"
 )
 
 func NewBuildScriptController(api fiber.Router) {
@@ -10,6 +13,15 @@ func NewBuildScriptController(api fiber.Router) {
 }
 
 func validateBuildScript(ctx *fiber.Ctx) error {
+	input := dto.ValidateBuildScriptRequest{}
+
+	if err := ctx.BodyParser(&input); err != nil {
+		return fiber.NewError(errutil.MapStatusCode(err))
+	}
+
+	if validationErrors := validator.CheckStruct(input); len(validationErrors) > 0 {
+		return ctx.Status(fiber.StatusBadRequest).JSON(validationErrors)
+	}
 	return ctx.JSON(map[string]string{
 		"ok": cast.ToString(true),
 	})
