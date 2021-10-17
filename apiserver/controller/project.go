8f4efbd673537e3b11b1a25fdcd57ee907af388c
trif0lium <gh@fleeingsunlight.dev>
2021-10-17 16:29:22 +0700

    feat(controller/project): add listProjectApps()

--- apiserver/controller/project.go
+++ apiserver/controller/project.go
@@ -2,27 +2,28 @@ package controller
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/thecodeisalreadydeployed/apiserver/errutil"
 	"github.com/thecodeisalreadydeployed/datastore"
 )
 
 func NewProjectController(api fiber.Router) {
 	api.Get("/", listProjects)
+	api.Get("/:projectID", getProject)
+	api.Get("/:projectID/apps", listProjectApps)
 }
 
 func listProjects(ctx *fiber.Ctx) error {
 	result, err := datastore.GetAllProjects()
-	if err != nil {
-		return fiber.NewError(errutil.MapStatusCode(err))
-	}
-	return ctx.JSON(result)
+	return writeResponse(ctx, result, err)
 }
 
 func getProject(ctx *fiber.Ctx) error {
 	projectID := ctx.Params("projectID")
 	result, err := datastore.GetProjectByID(projectID)
-	if err != nil {
-		return fiber.NewError(errutil.MapStatusCode(err))
-	}
-	return ctx.JSON(result)
+	return writeResponse(ctx, result, err)
+}
+
+func listProjectApps(ctx *fiber.Ctx) error {
+	projectID := ctx.Params("projectID")
+	result, err := datastore.GetAppsByProjectID(projectID)
+	return writeResponse(ctx, result, err)
 }
