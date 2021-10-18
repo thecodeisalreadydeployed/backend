5d5562a38a3f562aa91f4df6bb4e97a41799b342
trif0lium <gh@fleeingsunlight.dev>
2021-10-18 20:57:49 +0700

    test(manifestgenerator): TestVirtualServer()

--- manifestgenerator/manifestgenerator_test.go
+++ manifestgenerator/manifestgenerator_test.go
@@ -70,3 +70,78 @@ status:
 	assert.Nil(t, err)
 	assert.YAMLEq(t, expected, actual)
 }
+
+func TestGenerateVirtualServer(t *testing.T) {
+	options := &GenerateVirtualServerOptions{
+		Labels:    map[string]string{"VirtualServerLabelKey": "VirtualServerLabelValue"},
+		ProjectID: "ProjectID",
+		AppID:     "AppID",
+	}
+
+	expected := `
+	kind: VirtualServer
+metadata:
+  creationTimestamp: null
+  labels:
+    VirtualServerLabelKey: VirtualServerLabelValue
+  name: AppID
+  namespace: ProjectID
+spec:
+  host: AppID.
+  http-snippets: ""
+  ingressClassName: ""
+  policies: null
+  routes:
+  - action:
+      pass: AppID
+      proxy: null
+      redirect: null
+      return: null
+    errorPages: null
+    location-snippets: ""
+    matches: null
+    path: /
+    policies: null
+    route: ""
+    splits: null
+  server-snippets: ""
+  tls:
+    redirect: null
+    secret: ""
+  upstreams:
+  - buffer-size: ""
+    buffering: null
+    buffers: null
+    client-max-body-size: ""
+    connect-timeout: ""
+    fail-timeout: ""
+    healthCheck: null
+    keepalive: null
+    lb-method: ""
+    max-conns: null
+    max-fails: null
+    name: AppID
+    next-upstream: ""
+    next-upstream-timeout: ""
+    next-upstream-tries: 0
+    port: 3000
+    queue: null
+    read-timeout: ""
+    send-timeout: ""
+    service: AppID
+    sessionCookie: null
+    slow-start: ""
+    subselector: null
+    tls:
+      enable: false
+    use-cluster-ip: false
+status:
+  message: ""
+  reason: ""
+  state: ""
+	`
+
+	actual, err := GenerateVirtualServerYAML(options)
+	assert.Nil(t, err)
+	assert.YAMLEq(t, expected, actual)
+}
