e3432bf969f0296f43d520ff6175fe242de6275b
trif0lium <gh@fleeingsunlight.dev>
2021-11-01 12:37:57 +0700

    feat(kubernetesinteractor/v2): NewKubernetesInteractor()

--- kubernetesinteractor/v2/interactor.go
+++ kubernetesinteractor/v2/interactor.go
@@ -1,9 +1,16 @@
 package kubernetesinteractor
 
 import (
+	"flag"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
 	"github.com/thecodeisalreadydeployed/errutil"
 	"github.com/thecodeisalreadydeployed/model"
+	"go.uber.org/zap"
 	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 type KubernetesInteractor interface {
@@ -11,10 +18,34 @@ type KubernetesInteractor interface {
 	GetDeploymentState(name string, namespace string) model.DeploymentState
 }
 
-type kubernetesInteractor struct{}
+type kubernetesInteractor struct {
+	client *kubernetes.Clientset
+}
 
-func NewKubernetesInteractor() KubernetesInteractor {
-	return kubernetesInteractor{}
+func NewKubernetesInteractor() (KubernetesInteractor, error) {
+	var kubeconfig *string
+
+	if home, err := homedir.Dir(); err != nil {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+
+	flag.Parse()
+
+	// Use the current context in kubeconfig file.
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		zap.L().Error("error creating rest.Config when using BuildConfigFromFlags()")
+		return nil, errutil.ErrUnknown
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		zap.L().Error("error creating kubernetes.Clientset when using NewForConfig()")
+	}
+
+	return kubernetesInteractor{client: clientset}, nil
 }
 
 func (kubernetesInteractor) CreatePod(pod apiv1.Pod, namespace string) (string, error) {
