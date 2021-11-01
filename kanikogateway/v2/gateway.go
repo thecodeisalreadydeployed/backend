9250f6690fae5da8098a73dcfb0a432e2d65c6ab
trif0lium <gh@fleeingsunlight.dev>
2021-11-01 13:42:38 +0700

    feat(kanikogateway/v2): NewKanikoGateway()

--- kanikogateway/v2/gateway.go
+++ kanikogateway/v2/gateway.go
@@ -1,8 +1,11 @@
 package kanikogateway
 
 import (
+	"github.com/thecodeisalreadydeployed/containerregistry"
 	"github.com/thecodeisalreadydeployed/errutil"
+	"github.com/thecodeisalreadydeployed/kubernetesinteractor/v2"
 	"github.com/thecodeisalreadydeployed/model"
+	"go.uber.org/zap"
 )
 
 type KanikoGateway interface {
@@ -14,10 +17,19 @@ type kanikoGateway struct {
 	repositoryURL      string
 	branch             string
 	buildConfiguration model.BuildConfiguration
+	kubernetes         *kubernetesinteractor.KubernetesInteractor
+	registry           *containerregistry.ContainerRegistry
 }
 
-func NewKanikoGateway(deploymentID string, repositoryURL string, branch string, buildConfiguration model.BuildConfiguration) KanikoGateway {
-	return kanikoGateway{deploymentID: deploymentID, repositoryURL: repositoryURL, branch: branch, buildConfiguration: buildConfiguration}
+func NewKanikoGateway(deploymentID string, repositoryURL string, branch string, buildConfiguration model.BuildConfiguration) (KanikoGateway, error) {
+	it, err := kubernetesinteractor.NewKubernetesInteractor()
+
+	if err != nil {
+		zap.L().Error("could not initialize KubernetesInteractor", zap.String("deploymentID", deploymentID))
+		return nil, errutil.ErrFailedPrecondition
+	}
+
+	return kanikoGateway{deploymentID: deploymentID, repositoryURL: repositoryURL, branch: branch, buildConfiguration: buildConfiguration, kubernetes: &it, registry: nil}, nil
 }
 
 func (kanikoGateway) BuildContainerImage() (string, error) {
