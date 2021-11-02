d4d8445e125270ee9358c32a40ceb3816d6f9ab4
trif0lium <gh@fleeingsunlight.dev>
2021-11-02 15:13:32 +0700

    feat(containerregistry/v2/gcr): NewGCRGateway()

--- containerregistry/v2/gcr.go
+++ containerregistry/v2/gcr.go
@@ -1,10 +1,45 @@
 package containerregistry
 
+import (
+	"context"
+
+	"github.com/thecodeisalreadydeployed/errutil"
+	"go.uber.org/zap"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/cloudresourcemanager/v1"
+)
+
 type gcrGateway struct {
 }
 
-func NewGCRGateway() ContainerRegistry {
-	return gcrGateway{}
+func NewGCRGateway() (ContainerRegistry, error) {
+	c, err := google.DefaultClient(context.TODO(), cloudresourcemanager.CloudPlatformScope)
+	if err != nil {
+		zap.L().Error("cannot initialize google.DefaultClient")
+		return nil, errutil.ErrFailedPrecondition
+	}
+
+	cloudResourceManagerService, err := cloudresourcemanager.New(c)
+	if err != nil {
+		zap.L().Error("cannot initialize Cloud Resource Manager")
+		return nil, errutil.ErrFailedPrecondition
+	}
+
+	resource := ""
+	request := &cloudresourcemanager.TestIamPermissionsRequest{}
+	permissions := []string{}
+
+	resp, err := cloudResourceManagerService.Projects.TestIamPermissions(resource, request).Context(context.TODO()).Do()
+	if err != nil {
+		zap.L().Error("cannot call TestIamPermissions")
+		return nil, errutil.ErrUnavailable
+	}
+
+	if len(resp.Permissions) == len(permissions) {
+		return gcrGateway{}, nil
+	}
+
+	return nil, errutil.ErrUnavailable
 }
 
 func (gateway gcrGateway) ImageURL(name string, tag string) string {
