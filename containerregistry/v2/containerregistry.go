8d4561a8cda50b24544d957dbede680c191ba1b4
trif0lium <gh@fleeingsunlight.dev>
2021-11-02 15:23:40 +0700

    feat(containerregistry): ImageURL() -> ImageName()

--- containerregistry/v2/containerregistry.go
+++ containerregistry/v2/containerregistry.go
@@ -1,7 +1,7 @@
 package containerregistry
 
 type ContainerRegistry interface {
-	ImageURL(name string, tag string) string
+	ImageName(name string, tag string) string
 }
 
 type containerRegistry struct{}
