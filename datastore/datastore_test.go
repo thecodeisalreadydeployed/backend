b51338f24ae4f5049b78f11d7fe14e7c9e9affa2
ChanyutJean <chanyut22@hotmail.com>
2021-12-07 16:51:24 +0700

    feat(datastore): test save project

--- datastore/datastore_test.go
+++ datastore/datastore_test.go
@@ -1,6 +1,7 @@
 package datastore
 
 import (
+	"bou.ke/monkey"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/thecodeisalreadydeployed/datamodel"
 	"github.com/thecodeisalreadydeployed/model"
@@ -81,6 +82,58 @@ func TestGetProjectByID(t *testing.T) {
 	}
 
 	assert.Equal(t, expected, actual)
+
+	err = db.Close()
+	assert.Nil(t, err)
+
+	err = mock.ExpectationsWereMet()
+	assert.Nil(t, err)
+}
+
+func TestSaveProject(t *testing.T) {
+	monkey.Patch(time.Now, func() time.Time {
+		return time.Unix(0, 0)
+	})
+	defer monkey.UnpatchAll()
+
+	db, mock, err := sqlmock.New()
+	assert.Nil(t, err)
+
+	rows := sqlmock.NewRows(datamodel.ProjectStructString()).
+		AddRow("prj_test", "Best Project", time.Unix(0, 0), time.Unix(0, 0))
+
+	mock.ExpectQuery("SELECT VERSION()").WithArgs().WillReturnRows(
+		mock.NewRows([]string{"version"}).FromCSVString("1"),
+	)
+
+	query := "SELECT * FROM `projects` WHERE id = ? ORDER BY `projects`.`id` LIMIT 1"
+	exec := "UPDATE `projects` SET `name`=?,`created_at`=?,`updated_at`=? WHERE `id` = ?"
+
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(exec)).
+		WithArgs("Best Project", time.Unix(0, 0), time.Unix(0, 0), "prj_test").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+	mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs("prj_test").
+		WillReturnRows(rows)
+	mock.ExpectClose()
+
+	gdb, err := gorm.Open(mysql.New(mysql.Config{
+		Conn: db,
+	}), &gorm.Config{})
+	assert.Nil(t, err)
+
+	prj := &model.Project{
+		ID:        "prj_test",
+		Name:      "Best Project",
+		CreatedAt: time.Unix(0, 0),
+		UpdatedAt: time.Unix(0, 0),
+	}
+
+	actual, err := SaveProject(gdb, prj)
+	assert.Nil(t, err)
+	assert.Equal(t, prj, actual)
 
 	err = db.Close()
 	assert.Nil(t, err)
