8c88233f2aa9c857c977dd602330fef16c3efd0b
ChanyutJean <chanyut22@hotmail.com>
2021-12-12 14:19:55 +0700

    test(datastore/app_test): complete all app unit test

--- datastore/app_test.go
+++ datastore/app_test.go
@@ -1,6 +1,7 @@
 package datastore
 
 import (
+	"bou.ke/monkey"
 	"encoding/base64"
 	"encoding/json"
 	"github.com/DATA-DOG/go-sqlmock"
@@ -8,8 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/thecodeisalreadydeployed/datamodel"
 	"github.com/thecodeisalreadydeployed/model"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 	"regexp"
 	"testing"
 	"time"
@@ -18,53 +17,18 @@ import (
 func TestGetAllApps(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	assert.Nil(t, err)
+	expectVersionQuery(mock)
 
-	gitSource, err := json.Marshal(model.GitSource{})
-	if err != nil {
-		panic(err)
-	}
-
-	buildConfiguration, err := json.Marshal(model.BuildConfiguration{})
-	if err != nil {
-		panic(err)
-	}
-
-	rows := sqlmock.NewRows(datamodel.AppStructString()).
-		AddRow(
-			"app_test",
-			"prj_test",
-			"Best App",
-			cast.ToString(gitSource),
-			time.Unix(0, 0),
-			time.Unix(0, 0),
-			base64.StdEncoding.EncodeToString(buildConfiguration),
-			true,
-		)
-
-	mock.ExpectQuery("SELECT VERSION()").WithArgs().WillReturnRows(
-		mock.NewRows([]string{"version"}).FromCSVString("1"),
-	)
-	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `apps`")).WillReturnRows(rows)
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `apps`")).WillReturnRows(getAppRows())
 	mock.ExpectClose()
 
-	gdb, err := gorm.Open(mysql.New(mysql.Config{
-		Conn: db,
-	}), &gorm.Config{})
+	gdb, err := openGormDB(db)
 	assert.Nil(t, err)
 
 	actual, err := GetAllApps(gdb)
 	assert.Nil(t, err)
 
-	expected := &[]model.App{{
-		ID:                 "app_test",
-		ProjectID:          "prj_test",
-		Name:               "Best App",
-		GitSource:          model.GitSource{},
-		CreatedAt:          time.Unix(0, 0),
-		UpdatedAt:          time.Unix(0, 0),
-		BuildConfiguration: model.BuildConfiguration{},
-		Observable:         true,
-	}}
+	expected := &[]model.App{*getExpectedApp()}
 	assert.Equal(t, expected, actual)
 
 	err = db.Close()
@@ -74,178 +38,237 @@ func TestGetAllApps(t *testing.T) {
 	assert.Nil(t, err)
 }
 
-//func TestGetProjectByID(t *testing.T) {
-//	db, mock, err := sqlmock.New()
-//	assert.Nil(t, err)
-//
-//	rows := sqlmock.NewRows(datamodel.ProjectStructString()).
-//		AddRow("prj_test", "Best Project", time.Unix(0, 0), time.Unix(0, 0))
-//
-//	mock.ExpectQuery("SELECT VERSION()").WithArgs().WillReturnRows(
-//		mock.NewRows([]string{"version"}).FromCSVString("1"),
-//	)
-//
-//	query := "SELECT * FROM `projects` WHERE id = ? ORDER BY `projects`.`id` LIMIT 1"
-//	mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs("prj_test").WillReturnRows(rows)
-//	mock.ExpectClose()
-//
-//	gdb, err := gorm.Open(mysql.New(mysql.Config{
-//		Conn: db,
-//	}), &gorm.Config{})
-//	assert.Nil(t, err)
-//
-//	actual, err := GetProjectByID(gdb, "prj_test")
-//	assert.Nil(t, err)
-//
-//	expected := &model.Project{
-//		ID:        "prj_test",
-//		Name:      "Best Project",
-//		CreatedAt: time.Unix(0, 0),
-//		UpdatedAt: time.Unix(0, 0),
-//	}
-//
-//	assert.Equal(t, expected, actual)
-//
-//	err = db.Close()
-//	assert.Nil(t, err)
-//
-//	err = mock.ExpectationsWereMet()
-//	assert.Nil(t, err)
-//}
-//
-//func TestSaveProject(t *testing.T) {
-//	monkey.Patch(time.Now, func() time.Time {
-//		return time.Unix(0, 0)
-//	})
-//	defer monkey.UnpatchAll()
-//
-//	db, mock, err := sqlmock.New()
-//	assert.Nil(t, err)
-//
-//	rows := sqlmock.NewRows(datamodel.ProjectStructString()).
-//		AddRow("prj_test", "Best Project", time.Unix(0, 0), time.Unix(0, 0))
-//
-//	mock.ExpectQuery("SELECT VERSION()").WithArgs().WillReturnRows(
-//		mock.NewRows([]string{"version"}).FromCSVString("1"),
-//	)
-//
-//	query := "SELECT * FROM `projects` WHERE id = ? ORDER BY `projects`.`id` LIMIT 1"
-//	exec := "UPDATE `projects` SET `name`=?,`created_at`=?,`updated_at`=? WHERE `id` = ?"
-//
-//	mock.ExpectBegin()
-//	mock.ExpectExec(regexp.QuoteMeta(exec)).
-//		WithArgs("Best Project", time.Unix(0, 0), time.Unix(0, 0), "prj_test").
-//		WillReturnResult(sqlmock.NewResult(1, 1))
-//	mock.ExpectCommit()
-//	mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs("prj_test").
-//		WillReturnRows(rows)
-//	mock.ExpectClose()
-//
-//	gdb, err := gorm.Open(mysql.New(mysql.Config{
-//		Conn: db,
-//	}), &gorm.Config{})
-//	assert.Nil(t, err)
-//
-//	prj := &model.Project{
-//		ID:        "prj_test",
-//		Name:      "Best Project",
-//		CreatedAt: time.Unix(0, 0),
-//		UpdatedAt: time.Unix(0, 0),
-//	}
-//
-//	actual, err := SaveProject(gdb, prj)
-//	assert.Nil(t, err)
-//	assert.Equal(t, prj, actual)
-//
-//	err = db.Close()
-//	assert.Nil(t, err)
-//
-//	err = mock.ExpectationsWereMet()
-//	assert.Nil(t, err)
-//}
-//
-//func TestRemoveProject(t *testing.T) {
-//	monkey.Patch(time.Now, func() time.Time {
-//		return time.Unix(0, 0)
-//	})
-//	defer monkey.UnpatchAll()
-//
-//	db, mock, err := sqlmock.New()
-//	assert.Nil(t, err)
-//
-//	rows := sqlmock.NewRows(datamodel.ProjectStructString()).
-//		AddRow("prj_test", "Best Project", time.Unix(0, 0), time.Unix(0, 0))
-//
-//	mock.ExpectQuery("SELECT VERSION()").WithArgs().WillReturnRows(
-//		mock.NewRows([]string{"version"}).FromCSVString("1"),
-//	)
-//
-//	query := "SELECT * FROM `projects` WHERE `projects`.`id` = ? ORDER BY `projects`.`id` LIMIT 1"
-//	exec := "DELETE FROM `projects` WHERE `projects`.`id` = ?"
-//
-//	mock.ExpectQuery(regexp.QuoteMeta(query)).
-//		WithArgs("prj_test").
-//		WillReturnRows(rows)
-//
-//	mock.ExpectBegin()
-//	mock.ExpectExec(regexp.QuoteMeta(exec)).
-//		WithArgs("prj_test").
-//		WillReturnResult(sqlmock.NewResult(1, 1))
-//	mock.ExpectCommit()
-//
-//	mock.ExpectClose()
-//
-//	gdb, err := gorm.Open(mysql.New(mysql.Config{
-//		Conn: db,
-//	}), &gorm.Config{})
-//	assert.Nil(t, err)
-//
-//	err = RemoveProject(gdb, "prj_test")
-//	assert.Nil(t, err)
-//
-//	err = db.Close()
-//	assert.Nil(t, err)
-//
-//	err = mock.ExpectationsWereMet()
-//	assert.Nil(t, err)
-//}
-//
-//func TestGetProjectByName(t *testing.T) {
-//	db, mock, err := sqlmock.New()
-//	assert.Nil(t, err)
-//
-//	rows := sqlmock.NewRows(datamodel.ProjectStructString()).
-//		AddRow("prj_test", "Best Project", time.Unix(0, 0), time.Unix(0, 0))
-//
-//	mock.ExpectQuery("SELECT VERSION()").WithArgs().WillReturnRows(
-//		mock.NewRows([]string{"version"}).FromCSVString("1"),
-//	)
-//	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `projects` WHERE `projects`.`name` = ?")).
-//		WithArgs("Best Project").
-//		WillReturnRows(rows)
-//	mock.ExpectClose()
-//
-//	gdb, err := gorm.Open(mysql.New(mysql.Config{
-//		Conn: db,
-//	}), &gorm.Config{})
-//	assert.Nil(t, err)
-//
-//	actual, err := GetProjectsByName(gdb, "Best Project")
-//	assert.Nil(t, err)
-//
-//	expected := &[]model.Project{{
-//		ID:        "prj_test",
-//		Name:      "Best Project",
-//		CreatedAt: time.Unix(0, 0),
-//		UpdatedAt: time.Unix(0, 0),
-//	}}
-//
-//	assert.Equal(t, expected, actual)
-//
-//	err = db.Close()
-//	assert.Nil(t, err)
-//
-//	err = mock.ExpectationsWereMet()
-//	assert.Nil(t, err)
-//}
+func TestGetObservableApps(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.Nil(t, err)
+	expectVersionQuery(mock)
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `apps` WHERE observable = ?")).
+		WithArgs(true).
+		WillReturnRows(getAppRows())
+	mock.ExpectClose()
+
+	gdb, err := openGormDB(db)
+	assert.Nil(t, err)
+
+	actual, err := GetObservableApps(gdb)
+	assert.Nil(t, err)
+
+	expected := &[]model.App{*getExpectedApp()}
+	assert.Equal(t, expected, actual)
+
+	err = db.Close()
+	assert.Nil(t, err)
+
+	err = mock.ExpectationsWereMet()
+	assert.Nil(t, err)
+}
+
+func TestGetAppByProjectID(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.Nil(t, err)
+	expectVersionQuery(mock)
+
+	query := "SELECT * FROM `apps` WHERE `apps`.`project_id` = ?"
+	mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs("prj_test").
+		WillReturnRows(getAppRows())
+	mock.ExpectClose()
+
+	gdb, err := openGormDB(db)
+	assert.Nil(t, err)
+
+	actual, err := GetAppsByProjectID(gdb, "prj_test")
+	assert.Nil(t, err)
+
+	expected := &[]model.App{*getExpectedApp()}
+	assert.Equal(t, expected, actual)
+
+	err = db.Close()
+	assert.Nil(t, err)
+
+	err = mock.ExpectationsWereMet()
+	assert.Nil(t, err)
+}
+
+func TestGetAppByID(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.Nil(t, err)
+	expectVersionQuery(mock)
+
+	query := "SELECT * FROM `apps` WHERE id = ? ORDER BY `apps`.`id` LIMIT 1"
+	mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs("app_test").
+		WillReturnRows(getAppRows())
+	mock.ExpectClose()
+
+	gdb, err := openGormDB(db)
+	assert.Nil(t, err)
+
+	actual, err := GetAppByID(gdb, "app_test")
+	assert.Nil(t, err)
+
+	expected := getExpectedApp()
+	assert.Equal(t, expected, actual)
+
+	err = db.Close()
+	assert.Nil(t, err)
+
+	err = mock.ExpectationsWereMet()
+	assert.Nil(t, err)
+}
+
+func TestSaveApp(t *testing.T) {
+	monkey.Patch(time.Now, func() time.Time {
+		return time.Unix(0, 0)
+	})
+	defer monkey.UnpatchAll()
+
+	db, mock, err := sqlmock.New()
+	assert.Nil(t, err)
+	expectVersionQuery(mock)
+
+	query := "SELECT * FROM `apps` WHERE id = ? ORDER BY `apps`.`id` LIMIT 1"
+	exec := "UPDATE `apps` SET `project_id`=?,`name`=?,`git_source`=?,`created_at`=?,`updated_at`=?,`build_configuration`=?,`observable`=? WHERE `id` = ?"
+
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(exec)).
+		WithArgs(
+			"prj_test",
+			"Best App",
+			getGitSourceString(),
+			time.Unix(0, 0),
+			time.Unix(0, 0),
+			getBuildConfigurationString(),
+			true,
+			"app_test").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+	mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs("app_test").
+		WillReturnRows(getAppRows())
+	mock.ExpectClose()
+
+	gdb, err := openGormDB(db)
+	assert.Nil(t, err)
+
+	expected := getExpectedApp()
+
+	actual, err := SaveApp(gdb, expected)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, actual)
+
+	err = db.Close()
+	assert.Nil(t, err)
+
+	err = mock.ExpectationsWereMet()
+	assert.Nil(t, err)
+}
+
+func TestRemoveApp(t *testing.T) {
+	monkey.Patch(time.Now, func() time.Time {
+		return time.Unix(0, 0)
+	})
+	defer monkey.UnpatchAll()
+
+	db, mock, err := sqlmock.New()
+	assert.Nil(t, err)
+	expectVersionQuery(mock)
+
+	query := "SELECT * FROM `apps` WHERE `apps`.`id` = ? ORDER BY `apps`.`id` LIMIT 1"
+	exec := "DELETE FROM `apps` WHERE `apps`.`id` = ?"
+
+	mock.ExpectQuery(regexp.QuoteMeta(query)).
+		WithArgs("app_test").
+		WillReturnRows(getAppRows())
+
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(exec)).
+		WithArgs("app_test").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	mock.ExpectClose()
+
+	gdb, err := openGormDB(db)
+	assert.Nil(t, err)
+
+	err = RemoveApp(gdb, "app_test")
+	assert.Nil(t, err)
+
+	err = db.Close()
+	assert.Nil(t, err)
+
+	err = mock.ExpectationsWereMet()
+	assert.Nil(t, err)
+}
+
+func TestGetAppByName(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.Nil(t, err)
+	expectVersionQuery(mock)
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `apps` WHERE `apps`.`name` = ?")).
+		WithArgs("Best App").
+		WillReturnRows(getAppRows())
+	mock.ExpectClose()
+
+	gdb, err := openGormDB(db)
+	assert.Nil(t, err)
+
+	actual, err := GetAppsByName(gdb, "Best App")
+	assert.Nil(t, err)
+
+	expected := &[]model.App{*getExpectedApp()}
+
+	assert.Equal(t, expected, actual)
+
+	err = db.Close()
+	assert.Nil(t, err)
+
+	err = mock.ExpectationsWereMet()
+	assert.Nil(t, err)
+}
+
+func getGitSourceString() string {
+	gitSource, err := json.Marshal(model.GitSource{})
+	if err != nil {
+		panic(err)
+	}
+	return cast.ToString(gitSource)
+}
+
+func getBuildConfigurationString() string {
+	buildConfiguration, err := json.Marshal(model.BuildConfiguration{})
+	if err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(buildConfiguration)
+}
+
+func getAppRows() *sqlmock.Rows {
+	return sqlmock.NewRows(datamodel.AppStructString()).
+		AddRow(
+			"app_test",
+			"prj_test",
+			"Best App",
+			getGitSourceString(),
+			time.Unix(0, 0),
+			time.Unix(0, 0),
+			getBuildConfigurationString(),
+			true,
+		)
+}
+
+func getExpectedApp() *model.App {
+	return &model.App{
+		ID:                 "app_test",
+		ProjectID:          "prj_test",
+		Name:               "Best App",
+		GitSource:          model.GitSource{},
+		CreatedAt:          time.Unix(0, 0),
+		UpdatedAt:          time.Unix(0, 0),
+		BuildConfiguration: model.BuildConfiguration{},
+		Observable:         true,
+	}
+}
