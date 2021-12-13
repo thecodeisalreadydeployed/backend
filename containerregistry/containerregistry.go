aa51627b7bd28f04adcfcee64657fdea06f254e8
trif0lium <gh@fleeingsunlight.dev>
2021-12-14 00:01:04 +0700

    feat(containerregistry): AuthenticationMethod

--- containerregistry/containerregistry.go
+++ containerregistry/containerregistry.go
@@ -1,6 +1,7 @@
 package containerregistry
 
 type ContainerRegistryType string
+type AuthenticationMethod string
 
 const (
 	// Local
@@ -19,8 +20,14 @@ const (
 	ECR ContainerRegistryType = "ECR"
 )
 
+const (
+	KubernetesServiceAccount AuthenticationMethod = "KubernetesServiceAccount"
+	Secret                   AuthenticationMethod = "Secret"
+)
+
 type ContainerRegistry interface {
 	RegistryFormat(repository string, tag string) (string, error)
 	Type() ContainerRegistryType
 	Secret() string
+	AuthenticationMethod() AuthenticationMethod
 }
