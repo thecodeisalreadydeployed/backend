fd04eec21ed636e5f7bb40c0c01b7f61e6e05920
trif0lium <gh@fleeingsunlight.dev>
2021-12-13 23:28:41 +0700

    feat(logexporter): add export() function

--- logexporter/logexporter.go
+++ logexporter/logexporter.go
@@ -2,14 +2,35 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strings"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetEnvPrefix("CODEDEPLOY")
+	viper.BindEnv("API_URL")
+	viper.BindEnv("DEPLOYMENT_ID")
+	viper.BindEnv("DEPLOYMENT_GIT_SOURCE")
+	viper.BindEnv("DEPLOYMENT_BUILD_CONFIGURATION")
+
+	deploymentID := viper.GetString("CODEDEPLOY_DEPLOYMENT_ID")
+	apiURL := fmt.Sprintf("%s/internal/%s/events", strings.TrimSuffix(viper.GetString("CODEDEPLOY_API_URL"), "/"), deploymentID)
+
 	scanner := bufio.NewScanner(os.Stdin)
 	queue := NewQueue()
+	isExporting := false
+
 	for scanner.Scan() {
 		text := scanner.Text()
 		queue.Enqueue(text)
+
+		if !isExporting {
+			go export(apiURL, queue)
+		}
 	}
 }
+
+func export(apiURL string, queue Queue) {}
