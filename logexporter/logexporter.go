708c90ea98fe9224bccdddceddf8b9f949402f62
trif0lium <gh@fleeingsunlight.dev>
2021-12-13 21:56:01 +0700

    feat(logexporter): add main()

--- logexporter/logexporter.go
+++ logexporter/logexporter.go
@@ -1,1 +1,16 @@
 package logexporter
+
+import (
+	"bufio"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	queue := NewQueue()
+	for {
+		scanner.Scan()
+		text := scanner.Text()
+		queue.Enqueue(text)
+	}
+}
