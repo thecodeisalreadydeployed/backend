edadb7dfe2ce862d78043732ca697ec9436d821d
trif0lium <gh@fleeingsunlight.dev>
2021-12-14 15:55:49 +0700

    feat(apiserver/deployment): check X-CodeDeploy-Internal-Request and X-Forwarded-For

--- apiserver/controller/deployment.go
+++ apiserver/controller/deployment.go
@@ -7,7 +7,8 @@ import (
 
 func NewDeploymentController(api fiber.Router) {
 	api.Get("/:deploymentID", getDeployment)
-	api.Get("/:deploymentID/event", getDeploymentEvent)
+	api.Get("/:deploymentID/events", getDeploymentEvents)
+	api.Post("/:deploymentID/events", createDeploymentEvents)
 }
 
 func getDeployment(ctx *fiber.Ctx) error {
@@ -16,6 +17,16 @@ func getDeployment(ctx *fiber.Ctx) error {
 	return writeResponse(ctx, result, err)
 }
 
-func getDeploymentEvent(ctx *fiber.Ctx) error {
+func getDeploymentEvents(ctx *fiber.Ctx) error {
+	return fiber.NewError(fiber.StatusNotImplemented)
+}
+
+func createDeploymentEvents(ctx *fiber.Ctx) error {
+	isInternalRequest := string(ctx.Request().Header.Peek("X-CodeDeploy-Internal-Request")) == "True" && len(ctx.Request().Header.Peek("X-Forwarded-For")) == 0
+	if !isInternalRequest {
+		return fiber.NewError(fiber.StatusNotFound)
+	}
+
+	_ = ctx.Params("deploymentID")
 	return fiber.NewError(fiber.StatusNotImplemented)
 }
