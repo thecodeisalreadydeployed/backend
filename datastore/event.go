5c0fd9687a9d1d46e28abd079c723d51731de13c
trif0lium <gh@fleeingsunlight.dev>
2021-12-14 15:04:12 +0700

    feat(datastore/event): SaveEvent() + IsValidKSUID()

--- datastore/event.go
+++ datastore/event.go
@@ -1,7 +1,39 @@
 package datastore
 
-import "gorm.io/gorm"
+import (
+	"regexp"
+
+	"github.com/thecodeisalreadydeployed/datamodel"
+	"github.com/thecodeisalreadydeployed/errutil"
+	"github.com/thecodeisalreadydeployed/model"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
 
 func GetEventByDeploymentID(DB *gorm.DB, deploymentID string) (string, error) {
 	return "", nil
+}
+
+func IsValidKSUID(str string) bool {
+	re := regexp.MustCompile("^[a-zA-Z0-9]{27}$")
+	return re.MatchString(str)
+}
+
+func SaveEvent(DB *gorm.DB, event *model.Event) error {
+	if event.ID != "" {
+		if !IsValidKSUID(event.ID) {
+			return errutil.ErrInvalidArgument
+		}
+	} else {
+		event.ID = model.GenerateEventID()
+	}
+	e := datamodel.NewEventFromModel(event)
+	err := DB.Save(e).Error
+
+	if err != nil {
+		zap.L().Error(err.Error())
+		return errutil.ErrUnknown
+	}
+
+	return nil
 }
