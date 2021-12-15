a0cbeac80d528faf9ed00459562a71ea485bf30b
trif0lium <gh@fleeingsunlight.dev>
2021-12-15 14:48:53 +0700

    feat(imagebuilder): run.sh

---
+++ imagebuilder/run.sh
@@ -0,0 +1,5 @@
+docker run \
+  -v $(pwd)/testdata/fixture-nest.Dockerfile:/kaniko/deploys-dev/Dockerfile \
+  --env CODEDEPLOY_API_URL=http://host.docker.internal:3000 \
+  --env CODEDEPLOY_DEPLOYMENT_ID=testdata \
+  ghcr.io/thecodeisalreadydeployed/imagebuilder:latest
