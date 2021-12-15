e6b019154f620b05bd51d5ebc18791f71c6f7545
trif0lium <gh@fleeingsunlight.dev>
2021-12-15 15:15:20 +0700

    fix(imagebuilder): add --pull to run.sh

--- imagebuilder/run.sh
+++ imagebuilder/run.sh
@@ -1,4 +1,5 @@
 docker run \
+  --pull always \
   -v $(pwd)/testdata/fixture-nest.Dockerfile:/kaniko/deploys-dev/Dockerfile \
   --env CODEDEPLOY_API_URL=http://host.docker.internal:3000 \
   --env CODEDEPLOY_DEPLOYMENT_ID=testdata \
