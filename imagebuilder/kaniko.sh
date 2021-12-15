f8bbc7b49e107130c8f361648bc15513f08a0b5e
trif0lium <gh@fleeingsunlight.dev>
2021-12-15 16:20:57 +0700

    feat(imagebuilder/kaniko.sh): --context

--- imagebuilder/kaniko.sh
+++ imagebuilder/kaniko.sh
@@ -3,4 +3,4 @@
   --log-format=json \
   --verbosity=debug \
   --dockerfile=/kaniko/deploys-dev/Dockerfile \
-  --context=git://github.com/thecodeisalreadydeployed/fixture-nest.git#refs/tags/v2 2>&1 | tee /kaniko/deploys-dev/kaniko.log | /kaniko/deploys-dev/logexporter && echo "DONE" && cat /kaniko/deploys-dev/kaniko.log
+  --context=git://github.com/thecodeisalreadydeployed/fixture-nest.git#refs/tags/v2#14bc77fc515e6d66b8d9c15126ee49ca55faf879 2>&1 | tee /kaniko/deploys-dev/kaniko.log | /kaniko/deploys-dev/logexporter && echo "DONE" && cat /kaniko/deploys-dev/kaniko.log
