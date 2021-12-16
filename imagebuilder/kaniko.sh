ac62673d315e4a6581506230f18cfee873a7ae8c
trif0lium <gh@fleeingsunlight.dev>
2021-12-16 13:26:24 +0700

    feat(imagebuilder): CODEDEPLOY_KANIKO_*

--- imagebuilder/kaniko.sh
+++ imagebuilder/kaniko.sh
@@ -1,6 +1,12 @@
+[[ -z $CODEDEPLOY_KANIKO_LOG_VERBOSITY ]] && CODEDEPLOY_KANIKO_LOG_VERBOSITY="debug"
+[[ -z $CODEDEPLOY_KANIKO_LOG_FILE ]] && CODEDEPLOY_KANIKO_LOG_FILE="/kaniko/deploys-dev/kaniko.log"
+[[ -z $CODEDEPLOY_KANIKO_CONTEXT ]] && CODEDEPLOY_KANIKO_CONTEXT="dir:///workspace"
+[[ -z $CODEDEPLOY_KANIKO_DOCKERFILE ]] && CODEDEPLOY_KANIKO_DOCKERFILE="/kaniko/deploys-dev/Dockerfile"
+
+
 /kaniko/executor \
   --no-push \
   --log-format=json \
-  --verbosity=debug \
-  --dockerfile=/kaniko/deploys-dev/Dockerfile \
-  --context=dir:///workspace 2>&1 | tee /kaniko/deploys-dev/kaniko.log | /kaniko/deploys-dev/logexporter && echo "DONE" && cat /kaniko/deploys-dev/kaniko.log
+  --verbosity=$CODEDEPLOY_KANIKO_LOG_VERBOSITY \
+  --dockerfile=$CODEDEPLOY_KANIKO_DOCKERFILE \
+  --context=$CODEDEPLOY_KANIKO_CONTEXT 2>&1 | tee $CODEDEPLOY_KANIKO_LOG_FILE | /kaniko/deploys-dev/logexporter && echo "DONE" && cat $CODEDEPLOY_KANIKO_LOG_FILE
