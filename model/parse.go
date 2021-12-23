ae4b12b00a3922b5d37bf23752beb3853f90fb43
ChanyutJean <chanyut22@hotmail.com>
2021-12-23 10:36:49 +0700

    refactor(model): parse string to object method

--- model/parse.go
+++ model/parse.go
@@ -30,3 +30,36 @@ func GetBuildConfigurationString(bc BuildConfiguration) string {
 	}
 	return base64.StdEncoding.EncodeToString(buildConfiguration)
 }
+
+func GetCreatorObject(c string) Actor {
+	creator := Actor{}
+	err := json.Unmarshal([]byte(c), &creator)
+	if err != nil {
+		zap.L().Error(err.Error())
+	}
+	return creator
+}
+
+func GetGitSourceObject(gs string) GitSource {
+	gitSource := GitSource{}
+	err := json.Unmarshal([]byte(gs), &gitSource)
+	if err != nil {
+		zap.L().Error(err.Error())
+	}
+	return gitSource
+}
+
+func GetBuildConfigurationObject(bc string) BuildConfiguration {
+	buildConfiguration := BuildConfiguration{}
+	_buildConfiguration, err := base64.StdEncoding.DecodeString(bc)
+	if err != nil {
+		zap.L().Error(err.Error())
+	}
+
+	err = json.Unmarshal(_buildConfiguration, &buildConfiguration)
+	if err != nil {
+		zap.L().Error(err.Error())
+	}
+
+	return buildConfiguration
+}
