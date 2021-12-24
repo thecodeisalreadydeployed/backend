83fce232b191a39c740bf337415395d6d51a9ae1
ChanyutJean <chanyut22@hotmail.com>
2021-12-24 15:01:28 +0700

    test(repoobserver): done test PHEW

--- repositoryobserver/repositoryobserver_test.go
+++ repositoryobserver/repositoryobserver_test.go
@@ -1,6 +1,12 @@
 package repositoryobserver
 
 import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/thecodeisalreadydeployed/config"
+	"github.com/thecodeisalreadydeployed/datastore"
+	"github.com/thecodeisalreadydeployed/gitgateway/v2"
+	"github.com/thecodeisalreadydeployed/model"
+	"regexp"
 	"testing"
 	"time"
 
@@ -8,7 +14,6 @@ import (
 )
 
 func TestCheckChanges(t *testing.T) {
-
 	changeString, duration := checkChanges(
 		"https://github.com/thecodeisalreadydeployed/fixture-monorepo",
 		"main",
@@ -71,4 +76,43 @@ func TestCheckObservableApps(t *testing.T) {
 
 	err = mock.ExpectationsWereMet()
 	assert.Nil(t, err)
+}
+
+func TestCheckGitSource(t *testing.T) {
+	path, clean := gitgateway.InitRepository()
+	defer clean()
+
+	git, err := gitgateway.NewGitGatewayLocal(path)
+	assert.Nil(t, err)
+
+	err = git.WriteFile(".thecodeisalreadydeployed", "data")
+	assert.Nil(t, err)
+
+	hash, err := git.Commit([]string{".thecodeisalreadydeployed"}, "This is a commit.")
+	assert.Nil(t, err)
+
+	app := model.App{
+		ID:        "app_test",
+		ProjectID: "prj_test",
+		Name:      "BestApp",
+		GitSource: model.GitSource{
+			CommitSHA:        hash,
+			CommitMessage:    "This is a commit.",
+			CommitAuthorName: config.DefaultGitSignature().Name,
+			RepositoryURL:    path,
+			Branch:           "main",
+		},
+		CreatedAt:          time.Unix(0, 0),
+		UpdatedAt:          time.Unix(0, 0),
+		BuildConfiguration: model.BuildConfiguration{},
+		Observable:         true,
+	}
+
+	err = git.WriteFile(".thecodeisalreadydeployed", "new data")
+	assert.Nil(t, err)
+
+	_, err = git.Commit([]string{".thecodeisalreadydeployed"}, "This is another commit.")
+	assert.Nil(t, err)
+
+	checkGitSource(app, 0, false)
 }
