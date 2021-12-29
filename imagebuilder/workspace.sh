bc47d429f16c0008b3bf0a732db39b75493cd1ee
trif0lium <gh@fleeingsunlight.dev>
2021-12-29 15:07:00 +0700

    build(imagebuilder): /workspace/CODEDEPLOY_DEPLOYMENT_ID

--- imagebuilder/workspace.sh
+++ imagebuilder/workspace.sh
@@ -1,9 +1,10 @@
+[[ -z $CODEDEPLOY_DEPLOYMENT_ID ]] && exit 1
 [[ -z $CODEDEPLOY_GIT_REPOSITORY ]] && exit 1
 [[ -z $CODEDEPLOY_GIT_REFERENCE ]] && exit 1
 
-rm -rf /workspace/* && git clone $CODEDEPLOY_GIT_REPOSITORY \
+ls -a /workspace && rm -rf /workspace/* && git clone $CODEDEPLOY_GIT_REPOSITORY \
   --branch $CODEDEPLOY_GIT_REFERENCE \
   --single-branch \
   --depth 1 \
   --no-tags \
-  /workspace
+  /workspace/$CODEDEPLOY_DEPLOYMENT_ID
