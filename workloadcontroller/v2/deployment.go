be2bc1fd81b0d98b000a848b482737ea4a5d909f
trif0lium <gh@fleeingsunlight.dev>
2021-12-30 14:02:51 +0700

    feat(workloadcontroller): datastore.SaveDeployment()

--- workloadcontroller/v2/deployment.go
+++ workloadcontroller/v2/deployment.go
@@ -1,9 +1,26 @@
 package workloadcontroller
 
-import "go.uber.org/zap"
+import (
+	"github.com/thecodeisalreadydeployed/datastore"
+	"github.com/thecodeisalreadydeployed/model"
+	"go.uber.org/zap"
+)
 
 func NewDeployment(appID string) error {
 	logger := zap.L().Sugar().With("appID", appID)
 	_ = logger
+
+	app, err := datastore.GetAppByID(datastore.GetDB(), appID)
+	if err != nil {
+		return err
+	}
+
+	deployment, err := datastore.SaveDeployment(datastore.GetDB(), &model.Deployment{
+		AppID:     appID,
+		GitSource: app.GitSource,
+	})
+
+	_ = deployment
+
 	return nil
 }
