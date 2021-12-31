4a9a89ce2a124279535819ab156806f89445b8da
ChanyutJean <chanyut22@hotmail.com>
2021-12-31 15:10:16 +0700

    feat(datastore/preset): preset datastore function completed

--- datastore/preset.go
+++ datastore/preset.go
@@ -1,26 +1,113 @@
 package datastore
 
 import (
+	"errors"
+	"github.com/thecodeisalreadydeployed/datamodel"
+	"github.com/thecodeisalreadydeployed/errutil"
 	"github.com/thecodeisalreadydeployed/model"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
+	"strings"
 )
 
 func GetAllPresets(DB *gorm.DB) (*[]model.Preset, error) {
-	return nil, nil
+	var _data []datamodel.Preset
+	err := DB.Table("presets").Scan(&_data).Error
+	if err != nil {
+		zap.L().Error(err.Error())
+		return nil, errutil.ErrNotFound
+	}
+
+	var _ret []model.Preset
+	for _, data := range _data {
+		m := data.ToModel()
+		_ret = append(_ret, m)
+	}
+
+	ret := &_ret
+	return ret, nil
 }
 
 func GetPresetByID(DB *gorm.DB, id string) (*model.Preset, error) {
-	return nil, nil
+	if !strings.HasPrefix(id, "pst-") {
+		zap.L().Error(MsgPresetPrefix)
+		return nil, errutil.ErrInvalidArgument
+	}
+
+	var _data datamodel.Preset
+	err := DB.First(&_data, "id = ?", id).Error
+
+	if err != nil {
+		zap.L().Error(err.Error())
+		return nil, errutil.ErrNotFound
+	}
+
+	ret := _data.ToModel()
+	return &ret, nil
 }
 
-func GetPresetByName(DB *gorm.DB, name string) (*model.Preset, error) {
-	return nil, nil
+func GetPresetsByName(DB *gorm.DB, name string) (*[]model.Preset, error) {
+	var _data []datamodel.Preset
+
+	err := DB.Table("presets").Where(datamodel.Preset{Name: name}).Scan(&_data).Error
+
+	if err != nil {
+		zap.L().Error(err.Error())
+		return nil, errutil.ErrNotFound
+	}
+
+	var _ret []model.Preset
+	for _, data := range _data {
+		m := data.ToModel()
+		_ret = append(_ret, m)
+	}
+
+	ret := &_ret
+	return ret, nil
 }
 
 func SavePreset(DB *gorm.DB, preset *model.Preset) (*model.Preset, error) {
-	return nil, nil
+	if preset.ID != "" {
+		preset.ID = model.GeneratePresetID()
+	}
+	if !IsValidKSUID(preset.ID) {
+		zap.L().Error(MsgPresetPrefix)
+		return nil, errutil.ErrInvalidArgument
+	}
+
+	a := datamodel.NewPresetFromModel(preset)
+	err := DB.Save(a).Error
+
+	if err != nil {
+		zap.L().Error(err.Error())
+
+		if errors.Is(err, gorm.ErrInvalidField) || errors.Is(err, gorm.ErrInvalidData) {
+			return nil, errutil.ErrInvalidArgument
+		}
+
+		if errors.Is(err, gorm.ErrMissingWhereClause) {
+			return nil, errutil.ErrFailedPrecondition
+		}
+
+		return nil, errutil.ErrUnknown
+	}
+	return GetPresetByID(DB, preset.ID)
 }
 
 func RemovePreset(DB *gorm.DB, id string) error {
+	if !IsValidKSUID(id) {
+		zap.L().Error(MsgPresetPrefix)
+		return errutil.ErrInvalidArgument
+	}
+	var a datamodel.Preset
+	err := DB.Table("presets").Where(datamodel.Preset{ID: id}).First(&a).Error
+	if err != nil {
+		zap.L().Error(err.Error())
+		return errutil.ErrNotFound
+	}
+	if err := DB.Delete(&a).Error; err != nil {
+		zap.L().Error(err.Error())
+		return errutil.ErrUnknown
+	}
 	return nil
 }
