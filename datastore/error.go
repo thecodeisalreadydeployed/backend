4f8139064224e844129f5ce2f9b555063c9d53fe
ChanyutJean <chanyut22@hotmail.com>
2021-12-31 14:37:36 +0700

    feat(datastore/error): event prefix error message

--- datastore/error.go
+++ datastore/error.go
@@ -4,4 +4,5 @@ var (
 	MsgProjectPrefix    = "Project must have the prj_ prefix."
 	MsgAppPrefix        = "App must have the app_ prefix."
 	MsgDeploymentPrefix = "Deployment must have the dpl_ prefix."
+	MsgEventPrefix      = "Event must have a proper prefix."
 )
