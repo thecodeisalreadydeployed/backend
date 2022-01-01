8d9a36c7c6182a70bccdb0a55b667f82d99a9d26
trif0lium <gh@fleeingsunlight.dev>
2022-01-01 13:10:49 +0700

    test(gitopscontroller): temporalDir()

--- gitopscontroller/gitopscontroller_test.go
+++ gitopscontroller/gitopscontroller_test.go
@@ -4,14 +4,33 @@ import (
 	"os"
 	"testing"
 
+	"github.com/go-git/go-billy/v5/osfs"
+	"github.com/go-git/go-billy/v5/util"
 	"github.com/stretchr/testify/assert"
 	"github.com/thecodeisalreadydeployed/gitopscontroller"
 )
 
+func temporalDir() (path string, clean func()) {
+	fs := osfs.New(os.TempDir())
+	path, err := util.TempDir(fs, "", "")
+	if err != nil {
+		panic(err)
+	}
+	return fs.Join(fs.Root(), path), func() {
+		err := util.RemoveAll(fs, path)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
 func TestGitOpsController(t *testing.T) {
 	if os.Getenv("CI") == "true" && os.Getenv("GITHUB_WORKFLOW") == "test: unit" {
-		controller := gitopscontroller.NewGitOpsController()
+		dir, clean := temporalDir()
+		gitopscontroller.SetupUserspace(dir)
+		controller := gitopscontroller.NewGitOpsController(dir)
 		err := controller.SetupApp("prj-test", "app-test")
 		assert.NoError(t, err)
+		clean()
 	}
 }
