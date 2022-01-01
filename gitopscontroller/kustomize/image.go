2d1cd48e51c362f5d7dcb5703c86f5cfd8140a0e
trif0lium <gh@fleeingsunlight.dev>
2022-01-01 17:11:03 +0700

    feat(kustomize/image): parse() + parseOverwrite()

--- gitopscontroller/kustomize/image.go
+++ gitopscontroller/kustomize/image.go
@@ -1,6 +1,8 @@
 package kustomize
 
 import (
+	"strings"
+
 	"sigs.k8s.io/kustomize/api/types"
 )
 
@@ -37,4 +39,56 @@ func replaceDigest(image types.Image, digest string) types.Image {
 		NewTag:  image.NewTag,
 		Digest:  digest,
 	}
+}
+
+func parse(arg string) (types.Image, error) {
+
+	// matches if there is an image name to overwrite
+	// <image>=<new-image><:|@><new-tag>
+	if s := strings.Split(arg, separator); len(s) == 2 {
+		p, err := parseOverwrite(s[1], true)
+		return types.Image{
+			Name:    s[0],
+			NewName: p.name,
+			NewTag:  p.tag,
+			Digest:  p.digest,
+		}, err
+	}
+
+	// matches only for <tag|digest> overwrites
+	// <image><:|@><new-tag>
+	p, err := parseOverwrite(arg, false)
+	return types.Image{
+		Name:   p.name,
+		NewTag: p.tag,
+		Digest: p.digest,
+	}, err
+}
+
+// parseOverwrite parses the overwrite parameters
+// from the given arg into a struct
+func parseOverwrite(arg string, overwriteImage bool) (overwrite, error) {
+	// match <image>@<digest>
+	if d := strings.Split(arg, "@"); len(d) > 1 {
+		return overwrite{
+			name:   d[0],
+			digest: d[1],
+		}, nil
+	}
+
+	// match <image>:<tag>
+	if t := pattern.FindStringSubmatch(arg); len(t) == 3 {
+		return overwrite{
+			name: t[1],
+			tag:  t[2],
+		}, nil
+	}
+
+	// match <image>
+	if len(arg) > 0 && overwriteImage {
+		return overwrite{
+			name: arg,
+		}, nil
+	}
+	return overwrite{}, errImageInvalidArgs
 }
