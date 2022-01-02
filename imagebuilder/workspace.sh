b718afaedf6eeebcbfe52095803908174c54aee5
trif0lium <gh@fleeingsunlight.dev>
2022-01-02 20:35:12 +0700

    feat(imagebuilder-workspace): git remote add origin

--- imagebuilder/workspace.sh
+++ imagebuilder/workspace.sh
@@ -2,9 +2,15 @@
 [[ -z $CODEDEPLOY_GIT_REPOSITORY ]] && exit 1
 [[ -z $CODEDEPLOY_GIT_REFERENCE ]] && exit 1
 
-ls -a /workspace && rm -rf /workspace/* && git clone $CODEDEPLOY_GIT_REPOSITORY \
-  --branch $CODEDEPLOY_GIT_REFERENCE \
-  --single-branch \
-  --depth 1 \
-  --no-tags \
-  /workspace/$CODEDEPLOY_DEPLOYMENT_ID
+# ls -a /workspace && rm -rf /workspace/* && git clone $CODEDEPLOY_GIT_REPOSITORY \
+#   --branch $CODEDEPLOY_GIT_REFERENCE \
+#   --single-branch \
+#   --depth 1 \
+#   --no-tags \
+#   /workspace/$CODEDEPLOY_DEPLOYMENT_ID
+
+rm -rf ./workspace/* && mkdir -p /workspace/$CODEDEPLOY_DEPLOYMENT_ID && cd /workspace/$CODEDEPLOY_DEPLOYMENT_ID && \
+  git init && \
+  git remote add origin $CODEDEPLOY_GIT_REPOSITORY && \
+  git fetch --depth 1 origin $CODEDEPLOY_GIT_REFERENCE && \
+  git checkout FETCH_HEAD
