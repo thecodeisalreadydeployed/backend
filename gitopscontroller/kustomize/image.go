e0c918029a50db9f5eb3b5570c96d6379bc6d02d
trif0lium <gh@fleeingsunlight.dev>
2022-01-02 18:56:59 +0700

    fix(gitopscontroller/kustomize): errImageNoArgs -> errImageInvalidArgs

--- gitopscontroller/kustomize/image.go
+++ gitopscontroller/kustomize/image.go
@@ -15,7 +15,7 @@ const separator = "="
 var pattern = regexp.MustCompile(`^(.*):([a-zA-Z0-9._-]*|\*)$`)
 var preserveSeparator = "*"
 var (
-	errImageNoArgs = errors.New("no image specified")
+	errImageInvalidArgs = errors.New("invalid format of image")
 )
 
 type overwrite struct {
