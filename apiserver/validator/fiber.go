ea11aef005e839de832bbb2ff66834f0fb48dfc0
trif0lium <gh@fleeingsunlight.dev>
2022-01-03 14:03:55 +0700

    feat(apiserver/validator): ParseBodyAndValidate()

---
+++ apiserver/validator/fiber.go
@@ -0,0 +1,23 @@
+package validator
+
+import "github.com/gofiber/fiber/v2"
+
+func ParseBody(ctx *fiber.Ctx, body interface{}) *fiber.Error {
+	if err := ctx.BodyParser(body); err != nil {
+		return fiber.ErrBadRequest
+	}
+
+	return nil
+}
+
+func ParseBodyAndValidate(ctx *fiber.Ctx, body interface{}) *fiber.Error {
+	if err := ParseBody(ctx, body); err != nil {
+		return err
+	}
+
+	if validationErrors := CheckStruct(body); len(validationErrors) > 0 {
+		return ctx.Status(fiber.StatusBadRequest).JSON(validationErrors)
+	}
+
+	return nil
+}
