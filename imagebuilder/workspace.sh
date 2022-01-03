cb20a1059334376ecfd4d397b88c83d67e09f7bd
trif0lium <gh@fleeingsunlight.dev>
2022-01-03 12:21:50 +0700

    feat(imagebuilder/workspace.sh): /deploys-dev/logexporter

--- imagebuilder/workspace.sh
+++ imagebuilder/workspace.sh
@@ -2,8 +2,8 @@
 [[ -z $CODEDEPLOY_GIT_REPOSITORY ]] && exit 1
 [[ -z $CODEDEPLOY_GIT_REFERENCE ]] && exit 1
 
-rm -rf ./workspace/* && mkdir -p /workspace/$CODEDEPLOY_DEPLOYMENT_ID && cd /workspace/$CODEDEPLOY_DEPLOYMENT_ID && \
-  git init && \
-  git remote add origin $CODEDEPLOY_GIT_REPOSITORY && \
-  git fetch --depth 1 --no-tags --prune --progress origin $CODEDEPLOY_GIT_REFERENCE && \
-  git checkout FETCH_HEAD 2>&1 | /deploys-dev/logexporter
+rm -rf ./workspace/* && mkdir -p /workspace/$CODEDEPLOY_DEPLOYMENT_ID && cd /workspace/$CODEDEPLOY_DEPLOYMENT_ID
+git init 2>&1 | /deploys-dev/logexporter
+git remote add origin $CODEDEPLOY_GIT_REPOSITORY 2>&1 | /deploys-dev/logexporter
+git fetch --depth 1 --no-tags --prune --progress origin $CODEDEPLOY_GIT_REFERENCE 2>&1 | /deploys-dev/logexporter
+git checkout FETCH_HEAD 2>&1 | /deploys-dev/logexporter
