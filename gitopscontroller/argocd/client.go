54f1fcde734972da0ed60759e53f41a2596f751f
trif0lium <gh@fleeingsunlight.dev>
2022-01-03 23:25:59 +0700

    feat(gitopscontroller/argocd): http.NewRequest()

--- gitopscontroller/argocd/client.go
+++ gitopscontroller/argocd/client.go
@@ -1,13 +1,30 @@
 package argocd
 
-import "net/http"
+import (
+	"crypto/tls"
+	"net/http"
+	"time"
+)
+
+var transport = &http.Transport{
+	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+}
+
+var httpClient = &http.Client{
+	Timeout:   2 * time.Second,
+	Transport: transport,
+}
 
 func NewArgoCDClient() error {
 	apiURL := "http://argocd-server.argocd.svc.cluster.local/api/v1/application?name=" + "codedeploy" + "&refresh=true"
-	req, err := http.Get(apiURL)
+	req, err := http.NewRequest("GET", apiURL, nil)
 	if err != nil {
 		return err
 	}
-	_ = req
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	_ = resp
 	return nil
 }
