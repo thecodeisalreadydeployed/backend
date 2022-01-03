db05f8c61bef2e0b99a425b907ed38de08092bac
trif0lium <gh@fleeingsunlight.dev>
2022-01-03 18:07:04 +0700

    feat(deploy): kubectl apply -k argocd

--- deploy/1-kubectl-apply-argocd.sh
+++ deploy/1-kubectl-apply-argocd.sh
@@ -1,4 +1,3 @@
 cd "$(dirname "$0")" || exit
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.2.2/manifests/core-install.yaml
-kubectl apply -f .argocd.yaml
+kubectl apply -k argocd
