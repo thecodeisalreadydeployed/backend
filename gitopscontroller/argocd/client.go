65cf3d649daf63a999e39419664282ede3e3e76d
trif0lium <gh@fleeingsunlight.dev>
2022-01-03 19:12:38 +0700

    Revert "feat(gitopscontroller/argocd): NewClient()"
    This reverts commit 0290842d6b2332e579abdde2a6716aa1809f3198.

--- gitopscontroller/argocd/client.go
+++ gitopscontroller/argocd/client.go
@@ -1,24 +1,5 @@
 package argocd
 
-import (
-	"github.com/argoproj/argo-cd/v2/pkg/apiclient"
-)
-
 func NewArgoCDClient() error {
-	client, err := apiclient.NewClient(&apiclient.ClientOptions{
-		// TODO(trif0lium): use environment variable
-		ServerAddr: "argocd-server.argocd.svc.cluster.local",
-		PlainText:  true,
-		Insecure:   true,
-	})
-
-	if err != nil {
-		return err
-	}
-	closer, sessionClient, err := client.NewSessionClient()
-
-	_ = closer
-	_ = sessionClient
-
-	return err
+	return nil
 }
