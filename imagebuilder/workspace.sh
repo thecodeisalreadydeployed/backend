4c1f7bead4558cec764ec1dcf4c4cc218c425c0f
trif0lium <gh@fleeingsunlight.dev>
2022-01-03 12:11:24 +0700

    feat(imagebuilder/workspace.sh): --progress

--- imagebuilder/workspace.sh
+++ imagebuilder/workspace.sh
@@ -5,5 +5,5 @@
 rm -rf ./workspace/* && mkdir -p /workspace/$CODEDEPLOY_DEPLOYMENT_ID && cd /workspace/$CODEDEPLOY_DEPLOYMENT_ID && \
   git init && \
   git remote add origin $CODEDEPLOY_GIT_REPOSITORY && \
-  git fetch --depth 1 origin $CODEDEPLOY_GIT_REFERENCE && \
+  git fetch --depth 1 --no-tags --prune --progress origin $CODEDEPLOY_GIT_REFERENCE && \
   git checkout FETCH_HEAD 2>&1 | /deploys-dev/logexporter
