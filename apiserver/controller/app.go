2b0f16f317f34926f8a26d93de643b27c84d4ad0
ChanyutJean <chanyut22@hotmail.com>
2022-01-03 15:28:44 +0700

    lint: no strconv used

--- apiserver/controller/app.go
+++ apiserver/controller/app.go
@@ -6,7 +6,6 @@ import (
 	"github.com/thecodeisalreadydeployed/apiserver/errutil"
 	"github.com/thecodeisalreadydeployed/apiserver/validator"
 	"github.com/thecodeisalreadydeployed/datastore"
-	"strconv"
 )
 
 func NewAppController(api fiber.Router) {
