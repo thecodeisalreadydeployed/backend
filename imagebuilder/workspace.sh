b118c5be031f56194610d7b072c3d748d9743ccc
trif0lium <gh@fleeingsunlight.dev>
2022-01-03 11:57:39 +0700

    chore(imagebuilder/workspace.sh): clean up

--- imagebuilder/workspace.sh
+++ imagebuilder/workspace.sh
@@ -2,13 +2,6 @@
 [[ -z $CODEDEPLOY_GIT_REPOSITORY ]] && exit 1
 [[ -z $CODEDEPLOY_GIT_REFERENCE ]] && exit 1
 
-# ls -a /workspace && rm -rf /workspace/* && git clone $CODEDEPLOY_GIT_REPOSITORY \
-#   --branch $CODEDEPLOY_GIT_REFERENCE \
-#   --single-branch \
-#   --depth 1 \
-#   --no-tags \
-#   /workspace/$CODEDEPLOY_DEPLOYMENT_ID
-
 rm -rf ./workspace/* && mkdir -p /workspace/$CODEDEPLOY_DEPLOYMENT_ID && cd /workspace/$CODEDEPLOY_DEPLOYMENT_ID && \
   git init && \
   git remote add origin $CODEDEPLOY_GIT_REPOSITORY && \
