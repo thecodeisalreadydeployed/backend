028d0529d07979ed1534e33ecd2e8de1f05c4054
trif0lium <gh@fleeingsunlight.dev>
2022-01-03 22:36:06 +0700

    feat(gitopscontroller/argocd): http.Get(apiURL)

--- gitopscontroller/argocd/client.go
+++ gitopscontroller/argocd/client.go
@@ -1,5 +1,13 @@
 package argocd
 
+import "net/http"
+
 func NewArgoCDClient() error {
+	apiURL := "http://argocd-server.argocd.svc.cluster.local/api/v1/application?name=" + "codedeploy" + "&refresh=true"
+	req, err := http.Get(apiURL)
+	if err != nil {
+		return err
+	}
+	_ = req
 	return nil
 }
