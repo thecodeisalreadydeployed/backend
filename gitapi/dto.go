3d73dd3150638955816c723bdefb6043884b2d94
ChanyutJean <chanyut22@hotmail.com>
2022-02-05 14:36:52 +0700

    feat(gitapi/dto): actual response body

---
+++ gitapi/dto.go
@@ -0,0 +1,26 @@
+package gitapi
+
+type Branch struct {
+	Name      string `json:"name"`
+	Protected bool   `json:"protected"`
+	Commit    Commit
+}
+
+type Commit struct {
+	SHA string `json:"sha"`
+	URL string `json:"url"`
+}
+
+type File struct {
+	SHA  string `json:"sha"`
+	URL  string `json:"url"`
+	Tree []Tree
+}
+
+type Tree struct {
+	Path string `json:"path"`
+	Mode string `json:"mode"`
+	Type string `json:"type"`
+	SHA  string `json:"sha"`
+	URL  string `json:"url"`
+}
