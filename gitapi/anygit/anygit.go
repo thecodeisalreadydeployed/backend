8780754dfab789fedf61c649f3eb472244e02db5
trif0lium <gh@fleeingsunlight.dev>
2022-02-08 17:31:10 +0700

    feat(gitapi/anygit): NewAnyGitAPI()

--- gitapi/anygit/anygit.go
+++ gitapi/anygit/anygit.go
@@ -1,1 +1,19 @@
 package anygit
+
+import (
+	"github.com/thecodeisalreadydeployed/gitapi/provider"
+	"go.uber.org/zap"
+)
+
+type anyGitAPI struct {
+	logger  *zap.Logger
+	repoURL string
+}
+
+func NewAnyGitAPI(logger *zap.Logger, repoURL string) provider.GitProvider {
+	return &anyGitAPI{logger: logger, repoURL: repoURL}
+}
+
+func (api *anyGitAPI) GetBranches() ([]string, error) {
+	return []string{}, nil
+}
