f0df35e6970ab89fd1bb03ede443e189b75dc162
trif0lium <gh@fleeingsunlight.dev>
2022-02-08 14:26:47 +0700

    feat(gitapi): GitAPIBackend + gitAPIBackend

--- gitapi/api.go
+++ gitapi/api.go
@@ -5,3 +5,15 @@ type GitProvider interface {
 	GetFiles(owner string, repo string, branch string) ([]string, error)
 	GetRaw(owner string, repo string, branch string, path string) (string, error)
 }
+
+type GitAPIBackend interface {
+	GetBranches(repoURL string) ([]string, error)
+	GetFiles(repoURL string, branch string) ([]string, error)
+	GetRaw(repoURL string, branch string, path string) (string, error)
+}
+
+type gitAPIBackend struct{}
+
+func NewGitAPIBackend() GitAPIBackend {
+	return &gitAPIBackend{}
+}
