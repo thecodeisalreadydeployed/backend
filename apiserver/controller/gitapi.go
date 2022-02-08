fe4f61d138e8c001d666cab39139d3c6b1ae6fe8
trif0lium <gh@fleeingsunlight.dev>
2022-02-08 16:45:51 +0700

    feat(controller/gitapi): getRaw(gitAPIBackend)

--- apiserver/controller/gitapi.go
+++ apiserver/controller/gitapi.go
@@ -1,20 +1,17 @@
 package controller
 
 import (
-	"strings"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/thecodeisalreadydeployed/apiserver/dto"
 	"github.com/thecodeisalreadydeployed/apiserver/errutil"
 	"github.com/thecodeisalreadydeployed/apiserver/validator"
 	"github.com/thecodeisalreadydeployed/gitapi"
-	"github.com/thecodeisalreadydeployed/gitgateway/v2"
 )
 
 func NewGitAPIController(api fiber.Router, gitAPIBackend gitapi.GitAPIBackend) {
 	api.Post("/branches", getBranches(gitAPIBackend))
 	api.Post("/files", getFiles(gitAPIBackend))
-	api.Post("/raw", getRaw)
+	api.Post("/raw", getRaw(gitAPIBackend))
 }
 
 func getBranches(gitAPIBackend gitapi.GitAPIBackend) fiber.Handler {
@@ -42,32 +39,18 @@ func getFiles(gitAPIBackend gitapi.GitAPIBackend) fiber.Handler {
 	}
 }
 
-func getRaw(ctx *fiber.Ctx) error {
-	input := dto.GetRawRequest{}
+func getRaw(gitAPIBackend gitapi.GitAPIBackend) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		input := dto.GetRawRequest{}
 
-	if err := validator.ParseBodyAndValidate(ctx, &input); err != nil {
-		return err
-	}
-
-	var raw string
-	var err error
-	if isGitHubURL(input.Url) {
-		raw, err = gitapi.GetRaw(input.Url, input.Branch, input.Path)
-	} else {
-		git, gitErr := gitgateway.NewGitGatewayRemote(input.Url)
-		if gitErr != nil {
-			return fiber.ErrBadRequest
+		if err := validator.ParseBodyAndValidate(c, &input); err != nil {
+			return err
 		}
-		raw, err = git.GetRaw(input.Branch, input.Path)
-	}
 
-	if err != nil {
-		return fiber.NewError(errutil.MapStatusCode(err))
+		raw, err := gitAPIBackend.GetRaw(input.URL, input.Branch, input.Path)
+		if err != nil {
+			return fiber.NewError(errutil.MapStatusCode(err))
+		}
+		return c.SendString(raw)
 	}
-	return ctx.SendString(raw)
-}
-
-func isGitHubURL(url string) bool {
-	slices := strings.Split(url, "/")
-	return len(slices) == 5 && slices[2] == "github.com"
 }
