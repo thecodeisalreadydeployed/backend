ee74bc6ce9e06f46da262c1b2027bebad6bd4131
trif0lium <gh@fleeingsunlight.dev>
2022-02-08 14:42:13 +0700

    feat(gitapi/provider): init

--- gitapi/api.go
+++ gitapi/api.go
@@ -1,11 +1,5 @@
 package gitapi
 
-type GitProvider interface {
-	GetBranches(owner string, repo string) ([]string, error)
-	GetFiles(owner string, repo string, branch string) ([]string, error)
-	GetRaw(owner string, repo string, branch string, path string) (string, error)
-}
-
 type GitAPIBackend interface {
 	GetBranches(repoURL string) ([]string, error)
 	GetFiles(repoURL string, branch string) ([]string, error)
