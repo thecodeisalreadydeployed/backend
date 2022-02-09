c5bed85bf7376255cb2181566837c096c2e2e5d5
trif0lium <gh@fleeingsunlight.dev>
2022-02-09 22:02:40 +0700

    feat(main): NewGitOpsController()

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/thecodeisalreadydeployed/apiserver"
 	"github.com/thecodeisalreadydeployed/config"
 	"github.com/thecodeisalreadydeployed/datastore"
+	"github.com/thecodeisalreadydeployed/gitopscontroller"
+	"github.com/thecodeisalreadydeployed/gitopscontroller/argocd"
 	"github.com/thecodeisalreadydeployed/repositoryobserver"
 	"github.com/thecodeisalreadydeployed/util"
 	"github.com/thecodeisalreadydeployed/workloadcontroller/v2"
@@ -32,6 +34,8 @@ func main() {
 
 	config.SetDefault()
 	datastore.Migrate()
+	argoCDClient := argocd.NewArgoCDClient()
+	gitOpsController := gitopscontroller.NewGitOpsController(zap.L(), argoCDClient)
 	workloadController := workloadcontroller.NewWorkloadController()
 	repositoryObserver := repositoryobserver.NewRepositoryObserver(zap.L(), datastore.GetDB(), workloadController)
 	go repositoryObserver.ObserveGitSources()
