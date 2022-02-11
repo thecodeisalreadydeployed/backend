c1e14b08704f43d78bad76eb6a1b81adbc7d5d3d
trif0lium <gh@fleeingsunlight.dev>
2022-02-11 11:12:49 +0700

    feat(apiserver/auth): return nil in SetupFirebase() in dev environment

--- apiserver/auth/firebase.go
+++ apiserver/auth/firebase.go
@@ -6,11 +6,16 @@ import (
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
 	"github.com/thecodeisalreadydeployed/config"
+	"github.com/thecodeisalreadydeployed/util"
 	"go.uber.org/zap"
 	"google.golang.org/api/option"
 )
 
 func SetupFirebase() *auth.Client {
+	if util.IsDevEnvironment() || util.IsTestEnvironment() {
+		return nil
+	}
+
 	opt := option.WithCredentialsJSON([]byte(config.FirebaseServiceAccountKey()))
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
