75dca71036bdf8825427c88413f6550814f030bf
trif0lium <gh@fleeingsunlight.dev>
2022-02-11 14:47:56 +0700

    feat(manifestgenerator): GenerateNamespaceYAML()

---
+++ manifestgenerator/namespace.go
@@ -0,0 +1,22 @@
+package manifestgenerator
+
+import (
+	"github.com/ghodss/yaml"
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func GenerateNamespaceYAML(name string) (string, error) {
+	namespace := apiv1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Namespace",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
+	y, err := yaml.Marshal(namespace)
+
+	return string(y), err
+}
