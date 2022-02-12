ff51b12d007dc8c45a2ef2a74a677357227a5563
trif0lium <gh@fleeingsunlight.dev>
2022-02-12 15:25:06 +0700

    feat(clusterbackend): add ClusterBackend interface

--- clusterbackend/cluster.go
+++ clusterbackend/cluster.go
@@ -1,1 +1,30 @@
 package clusterbackend
+
+import (
+	"go.uber.org/zap"
+	apiv1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+type ClusterBackend interface {
+	CreatePod(pod apiv1.Pod, namespace string) (string, error)
+	CreateConfigMap(configMap apiv1.ConfigMap, namespace string) (string, error)
+}
+
+type clusterBackend struct {
+	logger           *zap.Logger
+	kubernetesClient *kubernetes.Clientset
+}
+
+func NewClusterBackend() ClusterBackend {
+	backend := &clusterBackend{}
+	return backend
+}
+
+func (backend *clusterBackend) CreatePod(pod apiv1.Pod, namespace string) (string, error) {
+	return "", nil
+}
+
+func (backend *clusterBackend) CreateConfigMap(configMap apiv1.ConfigMap, namespace string) (string, error) {
+	return "", nil
+}
