aef67d4b9402c06c4f79572e59c26e02175f8994
trif0lium <gh@fleeingsunlight.dev>
2022-02-12 17:31:14 +0700

    feat(workloadcontroller): ctrl.clusterBackend.Pods()

--- workloadcontroller/v2/workload.go
+++ workloadcontroller/v2/workload.go
@@ -1,4 +1,33 @@
 package workloadcontroller
 
+import (
+	"time"
+
+	"github.com/thecodeisalreadydeployed/datastore"
+	"github.com/thecodeisalreadydeployed/model"
+)
+
 func (ctrl *workloadController) ObserveWorkloads() {
+	for {
+		pendingDeployments, err := datastore.GetPendingDeployments(datastore.GetDB())
+		if err != nil {
+			ctrl.logger.Error(err.Error())
+			continue
+		}
+
+		for _, deployment := range *pendingDeployments {
+			if deployment.State == model.DeploymentStateBuilding {
+				pods, err := ctrl.clusterBackend.Pods("codedeploy-internal", map[string]string{
+					"beta.deploys.dev/deployment-id": deployment.ID,
+					"beta.deploys.dev/component":     "imagebuilder",
+				})
+				if err != nil {
+					ctrl.logger.Error(err.Error())
+					continue
+				}
+				_ = pods
+			}
+		}
+		time.Sleep(3 * time.Second)
+	}
 }
