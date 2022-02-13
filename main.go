95bf4c0b31792da6d99ac66e45976cac1509bdd2
trif0lium <gh@fleeingsunlight.dev>
2022-02-13 20:44:58 +0700

    feat(main): add containerRegistry

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/thecodeisalreadydeployed/apiserver"
 	"github.com/thecodeisalreadydeployed/clusterbackend"
 	"github.com/thecodeisalreadydeployed/config"
+	"github.com/thecodeisalreadydeployed/containerregistry"
 	"github.com/thecodeisalreadydeployed/datastore"
 	"github.com/thecodeisalreadydeployed/gitopscontroller"
 	"github.com/thecodeisalreadydeployed/repositoryobserver"
@@ -34,9 +35,10 @@ func main() {
 
 	config.BindEnv()
 	datastore.Migrate()
+	containerRegistry := containerregistry.NewContainerRegistry(config.DefaultContainerRegistryConfiguration())
 	clusterBackend := clusterbackend.NewClusterBackend(zap.L())
 	gitOpsController := gitopscontroller.NewGitOpsController(zap.L())
-	workloadController := workloadcontroller.NewWorkloadController(zap.L(), gitOpsController, clusterBackend)
+	workloadController := workloadcontroller.NewWorkloadController(zap.L(), gitOpsController, clusterBackend, containerRegistry)
 	repositoryObserver := repositoryobserver.NewRepositoryObserver(zap.L(), datastore.GetDB(), workloadController)
 	go repositoryObserver.ObserveGitSources()
 	go workloadController.ObserveWorkloads()
