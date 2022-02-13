70731d74f053bbacfae14207838b5ffb2fb8a157
trif0lium <gh@fleeingsunlight.dev>
2022-02-13 16:54:05 +0700

    test(manifestgenerator): testGenerateDeployment() + testGenerateService()

--- manifestgenerator/manifestgenerator_test.go
+++ manifestgenerator/manifestgenerator_test.go
@@ -6,70 +6,70 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func testGenerateDeployment(t *testing.T) {
-	options := &GenerateDeploymentOptions{
-		Name:           "DeploymentName",
-		Namespace:      "DeploymentNamespace",
-		Labels:         map[string]string{"DeploymentLabelKey": "DeploymentLabelValue"},
-		ContainerImage: "DeploymentContainerImage",
-	}
+// func testGenerateDeployment(t *testing.T) {
+// 	options := &GenerateDeploymentOptions{
+// 		Name:           "DeploymentName",
+// 		Namespace:      "DeploymentNamespace",
+// 		Labels:         map[string]string{"DeploymentLabelKey": "DeploymentLabelValue"},
+// 		ContainerImage: "DeploymentContainerImage",
+// 	}
 
-	expected := `apiVersion: apps/v1
-kind: Deployment
-metadata:
-  creationTimestamp: null
-  labels:
-    DeploymentLabelKey: DeploymentLabelValue
-  name: DeploymentName
-  namespace: DeploymentNamespace
-spec:
-  selector: null
-  strategy: {}
-  template:
-    metadata:
-      creationTimestamp: null
-      labels:
-        DeploymentLabelKey: DeploymentLabelValue
-    spec:
-      containers:
-      - env:
-        - name: PORT
-          value: "3000"
-        image: DeploymentContainerImage
-        imagePullPolicy: IfNotPresent
-        name: DeploymentName
-        resources: {}
-status: {}`
+// 	expected := `apiVersion: apps/v1
+// kind: Deployment
+// metadata:
+//   creationTimestamp: null
+//   labels:
+//     DeploymentLabelKey: DeploymentLabelValue
+//   name: DeploymentName
+//   namespace: DeploymentNamespace
+// spec:
+//   selector: null
+//   strategy: {}
+//   template:
+//     metadata:
+//       creationTimestamp: null
+//       labels:
+//         DeploymentLabelKey: DeploymentLabelValue
+//     spec:
+//       containers:
+//       - env:
+//         - name: PORT
+//           value: "3000"
+//         image: DeploymentContainerImage
+//         imagePullPolicy: IfNotPresent
+//         name: DeploymentName
+//         resources: {}
+// status: {}`
 
-	actual, err := GenerateDeploymentYAML(options)
+// 	actual, err := GenerateDeploymentYAML(options)
 
-	assert.Nil(t, err)
-	assert.YAMLEq(t, expected, actual)
-}
+// 	assert.Nil(t, err)
+// 	assert.YAMLEq(t, expected, actual)
+// }
 
-func testGenerateService(t *testing.T) {
-	options := &GenerateServiceOptions{
-		Name:      "ServiceName",
-		Namespace: "ServiceNamespace",
-		Labels:    map[string]string{"ServiceLabelKey": "ServiceLabelValue"},
-	}
+// func testGenerateService(t *testing.T) {
+// 	options := &GenerateServiceOptions{
+// 		Name:      "ServiceName",
+// 		Namespace: "ServiceNamespace",
+// 		Labels:    map[string]string{"ServiceLabelKey": "ServiceLabelValue"},
+// 	}
 
-	expected := `apiVersion: apps/v1
-kind: Service
-metadata:
-  creationTimestamp: null
-  labels:
-    ServiceLabelKey: ServiceLabelValue
-  name: ServiceName
-  namespace: ServiceNamespace
-spec: {}
-status:
-  loadBalancer: {}`
+// 	expected := `apiVersion: apps/v1
+// kind: Service
+// metadata:
+//   creationTimestamp: null
+//   labels:
+//     ServiceLabelKey: ServiceLabelValue
+//   name: ServiceName
+//   namespace: ServiceNamespace
+// spec: {}
+// status:
+//   loadBalancer: {}`
 
-	actual, err := GenerateServiceYAML(options)
-	assert.Nil(t, err)
-	assert.YAMLEq(t, expected, actual)
-}
+// 	actual, err := GenerateServiceYAML(options)
+// 	assert.Nil(t, err)
+// 	assert.YAMLEq(t, expected, actual)
+// }
 
 func TestGenerateVirtualServer(t *testing.T) {
 	options := &GenerateVirtualServerOptions{
