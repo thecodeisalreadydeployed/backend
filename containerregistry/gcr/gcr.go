3107333035b7b629f408531c2912e238b341f514
trif0lium <gh@fleeingsunlight.dev>
2022-02-13 17:27:43 +0700

    chore(containerregistry/gcr): AuthenticationMethod()

--- containerregistry/gcr/gcr.go
+++ containerregistry/gcr/gcr.go
@@ -6,24 +6,34 @@ import (
 	"github.com/thecodeisalreadydeployed/containerregistry"
 )
 
-func NewGCRGateway(hostname string, projectID string, serviceAccountKey string) containerregistry.ContainerRegistry {
-	return &gcrGateway{hostname: hostname, projectID: projectID, serviceAccountKey: serviceAccountKey}
+func NewGCRGateway(hostname string, projectID string, authenticationMethod containerregistry.AuthenticationMethod, serviceAccountKey string, kubernetesServiceAccount string) containerregistry.ContainerRegistry {
+	return &gcrGateway{hostname: hostname, projectID: projectID, authenticationMethod: authenticationMethod, serviceAccountKey: serviceAccountKey, kubernetesServiceAccount: kubernetesServiceAccount}
 }
 
 type gcrGateway struct {
-	hostname          string
-	projectID         string
-	serviceAccountKey string
+	hostname                 string
+	projectID                string
+	authenticationMethod     containerregistry.AuthenticationMethod
+	serviceAccountKey        string
+	kubernetesServiceAccount string
 }
 
-func (g *gcrGateway) RegistryFormat(repository string, tag string) string {
-	return fmt.Sprintf("%s/%s/%s:%s", g.hostname, g.projectID, repository, tag)
+func (gcr *gcrGateway) RegistryFormat(repository string, tag string) string {
+	return fmt.Sprintf("%s/%s/%s:%s", gcr.hostname, gcr.projectID, repository, tag)
 }
 
-func (g *gcrGateway) Type() containerregistry.ContainerRegistryType {
+func (gcr *gcrGateway) Type() containerregistry.ContainerRegistryType {
 	return containerregistry.GCR
 }
 
-func (g *gcrGateway) Secret() string {
-	return g.serviceAccountKey
+func (gcr *gcrGateway) Secret() string {
+	return gcr.serviceAccountKey
+}
+
+func (gcr *gcrGateway) AuthenticationMethod() containerregistry.AuthenticationMethod {
+	return gcr.authenticationMethod
+}
+
+func (gcr *gcrGateway) KubernetesServiceAccount() string {
+	return gcr.kubernetesServiceAccount
 }
