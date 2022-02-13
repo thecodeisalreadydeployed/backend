0d7e981494c423a8837abe8ddf1c618b7b9d49ee
trif0lium <gh@fleeingsunlight.dev>
2022-02-13 17:30:32 +0700

    chore(imagebuilder/kaniko.sh): check CODEDEPLOY_KANIKO_DESTINATION

--- imagebuilder/kaniko.sh
+++ imagebuilder/kaniko.sh
@@ -4,11 +4,20 @@
 [[ -z $CODEDEPLOY_KANIKO_CONTEXT ]] && CODEDEPLOY_KANIKO_CONTEXT="dir:///workspace"
 [[ -z $CODEDEPLOY_KANIKO_DOCKERFILE ]] && CODEDEPLOY_KANIKO_DOCKERFILE="/kaniko/deploys-dev/Dockerfile"
 
-
-/kaniko/executor \
-  --no-push \
-  --force \
-  --log-format=json \
-  --verbosity=$CODEDEPLOY_KANIKO_LOG_VERBOSITY \
-  --dockerfile=$CODEDEPLOY_KANIKO_DOCKERFILE \
-  --context=$CODEDEPLOY_KANIKO_CONTEXT 2>&1 | tee $CODEDEPLOY_KANIKO_LOG_FILE | /kaniko/deploys-dev/logexporter && echo "DONE" && cat $CODEDEPLOY_KANIKO_LOG_FILE
+if [[ -z $CODEDEPLOY_KANIKO_DESTINATION ]]; then
+  /kaniko/executor \
+    --no-push \
+    --force \
+    --log-format=json \
+    --verbosity=$CODEDEPLOY_KANIKO_LOG_VERBOSITY \
+    --dockerfile=$CODEDEPLOY_KANIKO_DOCKERFILE \
+    --context=$CODEDEPLOY_KANIKO_CONTEXT 2>&1 | tee $CODEDEPLOY_KANIKO_LOG_FILE | /kaniko/deploys-dev/logexporter && echo "DONE" && cat $CODEDEPLOY_KANIKO_LOG_FILE
+else
+  /kaniko/executor \
+    --destination=$CODEDEPLOY_KANIKO_DESTINATION \
+    --force \
+    --log-format=json \
+    --verbosity=$CODEDEPLOY_KANIKO_LOG_VERBOSITY \
+    --dockerfile=$CODEDEPLOY_KANIKO_DOCKERFILE \
+    --context=$CODEDEPLOY_KANIKO_CONTEXT 2>&1 | tee $CODEDEPLOY_KANIKO_LOG_FILE | /kaniko/deploys-dev/logexporter && echo "DONE" && cat $CODEDEPLOY_KANIKO_LOG_FILE
+fi
