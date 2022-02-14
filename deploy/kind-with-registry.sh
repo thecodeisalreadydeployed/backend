7abe2aecbbf1a009cb39036b8eaa9c14f338669f
trif0lium <gh@fleeingsunlight.dev>
2022-02-14 17:54:56 +0700

    build(deploy): document the local registry

--- deploy/kind-with-registry.sh
+++ deploy/kind-with-registry.sh
@@ -45,3 +45,17 @@ EOF
 if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${registryName}")" = 'null' ]; then
   docker network connect "kind" "${registryName}"
 fi
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${registryPort}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
