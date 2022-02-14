8353193eb69b3ef785c18a2d04b75238b88f8cb0
trif0lium <gh@fleeingsunlight.dev>
2022-02-14 21:01:19 +0700

    feat(containerregistry): add `repository` to gcrGateway

--- containerregistry/gcr/gcr.go
+++ containerregistry/gcr/gcr.go
@@ -6,19 +6,20 @@ import (
 	"github.com/thecodeisalreadydeployed/containerregistry/types"
 )
 
-func NewGCRGateway(hostname string, projectID string, authenticationMethod types.AuthenticationMethod, secret string) types.ContainerRegistry {
-	return &gcrGateway{hostname: hostname, projectID: projectID, authenticationMethod: authenticationMethod, secret: secret}
+func NewGCRGateway(hostname string, projectID string, repository string, authenticationMethod types.AuthenticationMethod, secret string) types.ContainerRegistry {
+	return &gcrGateway{hostname: hostname, projectID: projectID, repository: repository, authenticationMethod: authenticationMethod, secret: secret}
 }
 
 type gcrGateway struct {
 	hostname             string
 	projectID            string
+	repository           string
 	authenticationMethod types.AuthenticationMethod
 	secret               string
 }
 
-func (gcr *gcrGateway) RegistryFormat(repository string, tag string) string {
-	return fmt.Sprintf("%s/%s/codedeploy/%s:%s", gcr.hostname, gcr.projectID, repository, tag)
+func (gcr *gcrGateway) RegistryFormat(image string, tag string) string {
+	return fmt.Sprintf("%s/%s/%s/%s:%s", gcr.hostname, gcr.projectID, gcr.repository, image, tag)
 }
 
 func (gcr *gcrGateway) Type() types.ContainerRegistryType {
