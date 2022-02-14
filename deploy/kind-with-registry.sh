25efbdefa4cfc3b9134f0cf3f967f54f09732b05
trif0lium <gh@fleeingsunlight.dev>
2022-02-14 17:54:07 +0700

    build(deploy): connect registry to the cluster network

--- deploy/kind-with-registry.sh
+++ deploy/kind-with-registry.sh
@@ -9,3 +9,39 @@ if [ "$(docker inspect -f '{{.State.Running}}' "${registryName}" 2>/dev/null || 
     -d --restart=always -p "127.0.0.1:${registryPort}:5000" --name "${registryName}" \
     registry:2
 fi
+
+# Create a cluster with the local registry enabled in containerd
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+  - role: control-plane
+    image: kindest/node:v1.19.11@sha256:07db187ae84b4b7de440a73886f008cf903fcf5764ba8106a9fd5243d6f32729
+  - role: worker
+    image: kindest/node:v1.19.11@sha256:07db187ae84b4b7de440a73886f008cf903fcf5764ba8106a9fd5243d6f32729
+    extraMounts:
+      - hostPath: ./__w
+        containerPath: /__w
+    kubeadmConfigPatches:
+      - |
+        kind: InitConfiguration
+        nodeRegistration:
+          kubeletExtraArgs:
+            node-labels: "ingress-ready=true"
+    extraPortMappings:
+      - containerPort: 80
+        hostPort: 80
+        protocol: TCP
+      - containerPort: 443
+        hostPort: 443
+        protocol: TCP
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${registryPort}"]
+    endpoint = ["http://${registryName}:5000"]
+EOF
+
+# Connect the registry to the cluster network if not already connected
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${registryName}")" = 'null' ]; then
+  docker network connect "kind" "${registryName}"
+fi
