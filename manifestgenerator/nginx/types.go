aa04efa37d0d711b8da0bd5ebbac09fd06f8829e
trif0lium <gh@fleeingsunlight.dev>
2022-02-15 21:57:34 +0700

    chore(manifestgenerator): nginx.Metadata + nginx.Spec

--- manifestgenerator/nginx/types.go
+++ manifestgenerator/nginx/types.go
@@ -1,30 +1,44 @@
 package types
 
+type Metadata struct {
+	Name      string
+	Namespace string
+}
+
+type TLSRedirect struct {
+	Enable bool
+}
+
+type TLS struct {
+	Secret   string
+	Redirect TLSRedirect
+}
+
+type Upstream struct {
+	Name    string
+	Service string
+	Port    int
+}
+
+type RouteAction struct {
+	Pass string
+}
+
+type Route struct {
+	Path   string
+	Action RouteAction
+}
+
+type Spec struct {
+	Host      string
+	TLS       TLS
+	Upstreams []Upstream
+	Routes    []Route
+}
+
 type VirtualServer struct {
 	APIVersion string
 	Kind       string
-	Metadata   struct {
-		Name      string
-		Namespace string
-	}
-	Spec struct {
-		Host string
-		TLS  struct {
-			Secret   string
-			Redirect struct {
-				Enable bool
-			}
-		}
-		Upstreams []struct {
-			Name    string
-			Service string
-			Port    int
-		}
-		Routes []struct {
-			Path   string
-			Action struct {
-				Pass string
-			}
-		}
-	}
+	Metadata   Metadata
+	Spec       Spec
 }
