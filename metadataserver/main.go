8ec5cccb1c0c4e4720a914cbafd67e689da65d3b
trif0lium <gh@fleeingsunlight.dev>
2022-02-15 23:17:26 +0700

    feat(metadataserver): w.Write()

--- metadataserver/main.go
+++ metadataserver/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"os"
+
+	types "github.com/thecodeisalreadydeployed/metadataserver/types"
 )
 
 type metadataHandler struct{}
 
-func (handler *metadataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
-
-func main() {
+func (handler *metadataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	nodeName := os.Getenv("NODE_NAME")
 	podName := os.Getenv("POD_NAME")
 	podNamespace := os.Getenv("POD_NAMESPACE")
@@ -17,6 +18,21 @@ func main() {
 	appID := os.Getenv("APP_ID")
 	deploymentID := os.Getenv("DEPLOYMENT_ID")
 
+	metadataBytes, _ := json.Marshal(types.Metadata{
+		NodeName:     nodeName,
+		PodName:      podName,
+		PodNamespace: podNamespace,
+		ProjectID:    projectID,
+		AppID:        appID,
+		DeploymentID: deploymentID,
+	})
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(metadataBytes)
+
+}
+
+func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", &metadataHandler{})
 	http.ListenAndServe(":5000", mux)
