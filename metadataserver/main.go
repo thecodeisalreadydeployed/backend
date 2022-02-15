fbfc86989c962431db7f8d58f49224b91ee617b3
trif0lium <gh@fleeingsunlight.dev>
2022-02-15 23:09:45 +0700

    feat(metadataserver): os.Getenv()

--- metadataserver/main.go
+++ metadataserver/main.go
@@ -1,12 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 type metadataHandler struct{}
 
 func (handler *metadataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
 
 func main() {
+	nodeName := os.Getenv("NODE_NAME")
+	podName := os.Getenv("POD_NAME")
+	podNamespace := os.Getenv("POD_NAMESPACE")
+	projectID := os.Getenv("PROJECT_ID")
+	appID := os.Getenv("APP_ID")
+	deploymentID := os.Getenv("DEPLOYMENT_ID")
+
 	mux := http.NewServeMux()
 	mux.Handle("/", &metadataHandler{})
 	http.ListenAndServe(":5000", mux)
