2560cf652566eab34f99bdb7121ad55c69c4f530
trif0lium <gh@fleeingsunlight.dev>
2022-02-15 22:58:09 +0700

    feat(metadataserver): metadataHandler

--- metadataserver/main.go
+++ metadataserver/main.go
@@ -1,1 +1,13 @@
 package main
+
+import "net/http"
+
+type metadataHandler struct{}
+
+func (handler *metadataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", &metadataHandler{})
+	http.ListenAndServe(":5000", mux)
+}
