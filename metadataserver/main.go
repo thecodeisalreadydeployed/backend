db6b28e3408a167731aa8bb1b34e46c5066c18ee
trif0lium <gh@fleeingsunlight.dev>
2022-02-16 14:29:03 +0700

    feat(metadataserver): add /healthz

--- metadataserver/main.go
+++ metadataserver/main.go
@@ -8,32 +8,42 @@ import (
 	types "github.com/thecodeisalreadydeployed/metadataserver/types"
 )
 
-type metadataHandler struct{}
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		nodeName := os.Getenv("NODE_NAME")
+		podName := os.Getenv("POD_NAME")
+		podNamespace := os.Getenv("POD_NAMESPACE")
+		projectID := os.Getenv("PROJECT_ID")
+		appID := os.Getenv("APP_ID")
+		deploymentID := os.Getenv("DEPLOYMENT_ID")
 
-func (handler *metadataHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	nodeName := os.Getenv("NODE_NAME")
-	podName := os.Getenv("POD_NAME")
-	podNamespace := os.Getenv("POD_NAMESPACE")
-	projectID := os.Getenv("PROJECT_ID")
-	appID := os.Getenv("APP_ID")
-	deploymentID := os.Getenv("DEPLOYMENT_ID")
+		metadataBytes, _ := json.Marshal(types.Metadata{
+			NodeName:     nodeName,
+			PodName:      podName,
+			PodNamespace: podNamespace,
+			ProjectID:    projectID,
+			AppID:        appID,
+			DeploymentID: deploymentID,
+		})
 
-	metadataBytes, _ := json.Marshal(types.Metadata{
-		NodeName:     nodeName,
-		PodName:      podName,
-		PodNamespace: podNamespace,
-		ProjectID:    projectID,
-		AppID:        appID,
-		DeploymentID: deploymentID,
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write(metadataBytes)
 	})
 
-	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write(metadataBytes)
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(`{"ok": true}`))
+	})
 
-}
+	http.HandleFunc("/livez", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(`{"ok": true}`))
+	})
 
-func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", &metadataHandler{})
-	http.ListenAndServe(":5000", mux)
+	http.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(`{"ok": true}`))
+	})
+
+	http.ListenAndServe(":5000", nil)
 }
