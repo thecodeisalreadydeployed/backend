dee4b31aee348e482ecc24e8fb10c23660f72c02
trif0lium <gh@fleeingsunlight.dev>
2022-02-16 16:30:49 +0700

    feat(statusapi): GetActiveDeploymentID()

--- statusapi/api.go
+++ statusapi/api.go
@@ -2,10 +2,24 @@ package statusapi
 
 import (
 	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/google/uuid"
+	"github.com/thecodeisalreadydeployed/datastore"
 	"go.uber.org/zap"
 )
+
+type Metadata struct {
+	NodeName     string
+	PodName      string
+	PodNamespace string
+	ProjectID    string
+	AppID        string
+	DeploymentID string
+}
 
 type StatusAPIBackend interface {
 	GetActiveDeploymentID(appID string) (string, error)
@@ -25,5 +39,42 @@ func NewStatusAPIBackend(logger *zap.Logger) StatusAPIBackend {
 }
 
 func (backend *statusAPIBackend) GetActiveDeploymentID(appID string) (string, error) {
-	return "", nil
+	requestID := uuid.NewString()
+	logger := backend.logger.With(zap.String("appID", appID), zap.String("requestID", requestID))
+	_ = logger
+
+	app, err := datastore.GetAppByID(datastore.GetDB(), appID)
+	if err != nil {
+		logger.Error(err.Error())
+		return "", err
+	}
+
+	metadataAPI := fmt.Sprintf("http://%s.%s.svc.cluster.local:5000", app.ID, app.ProjectID)
+	req, err := http.NewRequest("GET", metadataAPI, nil)
+	if err != nil {
+		logger.Error(err.Error())
+		return "", err
+	}
+
+	resp, err := backend.httpClient.Do(req)
+	if err != nil {
+		logger.Error(err.Error())
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger.Error(err.Error())
+		return "", err
+	}
+
+	var metadata Metadata
+	err = json.Unmarshal(responseBody, &metadata)
+	if err != nil {
+		logger.Error(err.Error())
+		return "", err
+	}
+
+	return metadata.DeploymentID, nil
 }
