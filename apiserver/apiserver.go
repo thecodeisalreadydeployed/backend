74907e009673a50153d7d850750c802f089321aa
trif0lium <gh@fleeingsunlight.dev>
2022-02-16 20:35:40 +0700

    chore(apiserver): do not log API requests

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -15,7 +15,6 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/thecodeisalreadydeployed/apiserver/validator"
 )
 
@@ -27,7 +26,6 @@ func APIServer(port int, workloadController workloadcontroller.WorkloadControlle
 	app := fiber.New()
 
 	app.Use(cors.New())
-	app.Use(logger.New())
 
 	controller.NewProjectController(app.Group("projects", auth.EnsureAuthenticated(firebaseAuth)), workloadController)
 	controller.NewAppController(app.Group("apps", auth.EnsureAuthenticated(firebaseAuth)), workloadController, observer, statusAPIBackend)
