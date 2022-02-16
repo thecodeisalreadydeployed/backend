dc01e4690e545a7c5c0ecdb616a5fd652201cf4c
trif0lium <gh@fleeingsunlight.dev>
2022-02-16 16:21:21 +0700

    feat(statusapi): add StatusAPIBackend interface

--- statusapi/api.go
+++ statusapi/api.go
@@ -1,1 +1,29 @@
 package statusapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+type StatusAPIBackend interface {
+	GetActiveDeploymentID(appID string) (string, error)
+}
+
+type statusAPIBackend struct {
+	logger     *zap.Logger
+	httpClient *http.Client
+}
+
+var HTTPTransport http.RoundTripper = &http.Transport{
+	TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+}
+
+func NewStatusAPIBackend(logger *zap.Logger) StatusAPIBackend {
+	return &statusAPIBackend{logger: logger, httpClient: &http.Client{Transport: HTTPTransport}}
+}
+
+func (backend *statusAPIBackend) GetActiveDeploymentID(appID string) (string, error) {
+	return "", nil
+}
