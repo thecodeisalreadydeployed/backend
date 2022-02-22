2d2b0ff30e411b2ac406e79a583c4ca615c77c57
ChanyutJean <chanyut22@hotmail.com>
2022-02-22 15:12:22 +0700

    feat(gitapi/provider): add fillgitsource method

--- gitapi/provider/provider.go
+++ gitapi/provider/provider.go
@@ -1,7 +1,10 @@
 package provider
 
+import "github.com/thecodeisalreadydeployed/model"
+
 type GitProvider interface {
 	GetBranches() ([]string, error)
 	GetFiles(branch string) ([]string, error)
 	GetRaw(branch string, path string) (string, error)
+	FillGitSource(gs model.GitSource) (model.GitSource, error)
 }
